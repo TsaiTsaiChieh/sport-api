82d956e3fd120362c8498cbcb5a90f94523536cf
Rex <rex@gets-info.com>
2019-10-21 18:06:54 +0800

    auth : phone

--- functions/auth.js
+++ functions/auth.js
@@ -1,10 +1,6 @@
 const functions = require('firebase-functions');
 
 
-// import * as express from "express";
-// import * as admin from "firebase-admin";
-// import * as functions from "firebase-functions";
-
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -12,6 +8,7 @@ const functions = require('firebase-functions');
 //  res.send("Hello from Firebase!");
 // });
 const express = require("express");
+const {google} = require('googleapis');
 
 const admin = require("firebase-admin");
 const serviceAccount = require("./auth/sport19y0715-d23e597f8c95.json");
@@ -43,7 +40,7 @@ app.use(express.json());
 app.post('/api', (req, res) => {
     console.log("api....");
     res.json({
-        message: 'Hello API'
+        message: 'Hello API2'
     });
     // return res.status(200);
     // res.status(200).send("hello");
@@ -71,25 +68,28 @@ app.post('/emailRegister', (req, res) => {
         // var displayName = req.body.displayName;
 
         if (!registerEmail) return res.status(400).json({
-            error: 'missing email'
+            success: false,
+            message: 'missing email'
         });
         if (!registerPassword) return res.status(400).json({
-            error: 'missing password'
+            success: false,
+            message: 'missing password'
         });
         if (!confirmPassword) return res.status(400).json({
-            error: 'missing password'
+            success: false,
+            message: 'missing confirm password'
         });
         if (registerPassword !== confirmPassword) return res.status(400).json({
-            error: 'password not same'
+            success: false,
+            message: 'password confirm failed'
         });
 
         //參考https://firebase.google.com/docs/auth/admin/manage-users?authuser=0#create_a_user
         admin.auth().createUser({
-                email: registerEmail,
-                password: registerPassword,
-                // displayName:displayName
-                rawId: "testID"
-            })
+            email: registerEmail,
+            password: registerPassword,
+            // displayName:displayName
+        })
             .then(function (userRecord) {
                 // See the UserRecord reference doc for the contents of userRecord.
                 // var uid = userRecord.uid;
@@ -124,10 +124,12 @@ app.post('/email', (req, res) => {
     const loginPassword = req.body.loginPassword;
 
     if (!loginEmail) return res.status(400).json({
-        error: 'missing email'
+        success: false,
+        message: 'missing email'
     });
     if (!loginPassword) return res.status(400).json({
-        error: 'missing password'
+        success: false,
+        message: 'missing password'
     });
     firebase.auth().signInWithEmailAndPassword(loginEmail, loginPassword)
         .then(function (user) {
@@ -135,8 +137,8 @@ app.post('/email', (req, res) => {
                 .then(function (idToken) {
                     const expiresIn = 60 * 60 * 24 * 7 * 1000;
                     admin.auth().createSessionCookie(idToken, {
-                            expiresIn
-                        })
+                        expiresIn
+                    })
                         .then(function (sessionCookie) {
                             const options = {
                                 maxAge: expiresIn,
@@ -154,8 +156,7 @@ app.post('/email', (req, res) => {
                             res.set('Cache-Control', 'no-cache, no-store, must-revalidate');
                             res.json({
                                 success: false,
-                                error: error,
-                                a: 'createSessionCookie'
+                                message: 'createSessionCookie'
                             });
                             console.log('Error login user : createSessionCookie\n\t', error);
                         });
@@ -164,8 +165,7 @@ app.post('/email', (req, res) => {
                     res.set('Cache-Control', 'no-cache, no-store, must-revalidate');
                     res.json({
                         success: false,
-                        error: error,
-                        a: 'idToken'
+                        message: 'idToken'
                     });
                     console.log('Error login user: idToken\n\t', error);
                 });
@@ -180,6 +180,168 @@ app.post('/email', (req, res) => {
         });
 });
 
+app.post('/phoneRegister', (req, res) => {
+    const registerPhone = req.body.registerPhone;
+    const registerPassword = req.body.registerPassword;
+    const confirmPassword = req.body.confirmPassword;
+    console.log(registerPhone)
+
+    if (!registerPhone) return res.status(400).json({
+        success: false,
+        message: 'missing phone number'
+    });
+    if (!registerPassword) return res.status(400).json({
+        success: false,
+        message: 'missing password'
+    });
+    if (!confirmPassword) return res.status(400).json({
+        success: false,
+        message: 'missing confirm password'
+    });
+    if (registerPassword !== confirmPassword) return res.status(400).json({
+        success: false,
+        message: 'password confirm failed'
+    });
+    //參考https://firebase.google.com/docs/auth/admin/manage-users?authuser=0#create_a_user
+    admin.auth().createUser({
+        phoneNumber: registerPhone
+    })
+        .then(function (userRecord) {
+            // See the UserRecord reference doc for the contents of userRecord.
+            const uid = userRecord.uid;
+            res.set('Cache-Control', 'no-cache, no-store, must-revalidate');
+            res.json({
+                success: true,
+                message: userRecord
+            });
+            console.log('Successfully created new user:', uid);
+        })
+        .catch(function (error) {
+            res.set('Cache-Control', 'no-cache, no-store, must-revalidate');
+            res.json({
+                success: false,
+                message: error
+            });
+            console.log('Error creating new user:', error);
+        });
+});
+
+
+// https://stackoverflow.com/questions/46893766/firebase-auth-signinwithphonenumber-express-on-node-js
+app.post('/phoneToken', (req, res) => {
+    firebase.auth().settings.appVerificationDisabledForTesting = false;
+
+    const loginPhone = req.body.loginPhone;
+    const testVerificationCode = "123456";
+    // const loginPassword = req.body.loginPassword;
+
+    if (!loginPhone) return res.status(400).json({
+        success: false,
+        message: 'missing phone number'
+    });
+    // if (!loginPassword) return res.status(400).json({
+    //     success: false,
+    //     message: 'missing password'
+    // });
+    const appVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container');
+    firebase.auth().signInWithPhoneNumber(loginPhone, appVerifier)
+        .then(function (user) {
+            user.user.getIdToken()
+                .then(function (idToken) {
+                    const expiresIn = 60 * 60 * 24 * 7 * 1000;
+                    admin.auth().createSessionCookie(idToken, {
+                        expiresIn
+                    })
+                        .then(function (sessionCookie) {
+                            const options = {
+                                maxAge: expiresIn,
+                                httpOnly: true,
+                                secure: true
+                            };
+                            res.cookie('__session', sessionCookie, options);
+                            res.json({
+                                success: true,
+                                message: '登入成功'
+                            });
+                            console.log('Successfully login user : ', user.user.uid);
+                        })
+                        .catch(function (error) {
+                            res.set('Cache-Control', 'no-cache, no-store, must-revalidate');
+                            res.json({
+                                success: false,
+                                message: 'createSessionCookie'
+                            });
+                            console.log('Error login user : createSessionCookie\n\t', error);
+                        });
+                })
+                .catch(function (error) {
+                    res.set('Cache-Control', 'no-cache, no-store, must-revalidate');
+                    res.json({
+                        success: false,
+                        message: 'idToken'
+                    });
+                    console.log('Error login user: idToken\n\t', error);
+                });
+        })
+        .catch(function (error) {
+            res.set('Cache-Control', 'no-cache, no-store, must-revalidate');
+            res.json({
+                success: false,
+                error: error
+            });
+            console.log('Error login user: ', error);
+        });
+});
+app.post('/phone', (req, res) => {
+
+    var token = req.body.token;
+    if (!token) return res.status(400).json({error: 'missing email'});
+    console.log(token);
+
+    var expiresIn = 60 * 60 * 24 * 7 * 1000;
+    admin.auth().createSessionCookie(token, {expiresIn})
+        .then(function (sessionCookie) {
+            const options = {maxAge: expiresIn, httpOnly: true, secure: true};
+            res.cookie('__session', sessionCookie, options);
+            res.json({
+                success: true,
+                message: '登入成功'
+            })
+        })
+        .catch(function (error) {
+            res.set('Cache-Control', 'no-cache, no-store, must-revalidate');
+            res.json({
+                success: false,
+                error: error,
+                a: 'createSessionCookie'
+            })
+        });
+});
+
+app.post('/sendSMS', function (req, res) {
+    const { phoneNumber, recaptchaToken } = req.body;
+    console.log(phoneNumber,recaptchaToken);
+    const identityToolkit = google.identitytoolkit({
+        auth: 'AIzaSyB31V6WewUi-iY12231Ixahquf68uGaoCo',
+        version: 'v3',
+    });
+    const response = identityToolkit.relyingparty.sendVerificationCode({
+        phoneNumber,
+        recaptchaToken: recaptchaToken
+        // recaptchaToken: recaptcha,
+    });
+
+    // save sessionInfo into db. You will need this to verify the SMS code
+    // const sessionInfo = response.data.sessionInfo;
+    console.log(response);
+    res.json({
+        "test":"test"
+    })
+
+});
+
 // app.listen(5000,()=> console.log('Server started on port 5000'));
+
+// module.exports = functions.https.onRequest(app);
 
 module.exports = functions.https.onRequest(app);
