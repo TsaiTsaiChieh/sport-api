5f80cca941f38ca6ac7d205163d2c2052abaf8f4
Rex <rex@gets-info.com>
2019-11-20 15:07:46 +0800

    remove auth.js useless code

--- functions/auth.js
+++ functions/auth.js
@@ -121,7 +121,6 @@ app.post('/emailRegister', (req, res) => {
 });
 
 app.post('/email', (req, res) => {
-
     const loginEmail = req.body.loginEmail;
     const loginPassword = req.body.loginPassword;
 
@@ -431,68 +430,6 @@ const verifySessionCookie = async (sessionCookie) => {
             return false;
         });
 };
-// async function verifySessionCookie(sessionCookie) {
-//     console.log("......verify")
-//     await admin.auth().verifySessionCookie(
-//         sessionCookie, true)
-//         .then((decodedClaims) => {
-//             console.log("......verify2")
-//             console.log('Auth - verifySessionCookie success : ', decodedClaims);
-//             return true;
-//         })
-//         .catch(error => {
-//             console.log("......verify3")
-//             console.log('Auth - verifySessionCookie false : ', error);
-//             return false;
-//         });
-//     console.log("......verify4")
-// }
-
-/**
- * Line login , reference from :
- * https://firebase.googleblog.com/2016/11/authenticate-your-firebase-users-with-line-login.html
- */
-// app.post('/verifyToken', (req, res) => {
-//     if (!req.body.token) {
-//         return res.status(400).send('Access Token not found');
-//     }
-//     const reqToken = req.body.token;
-//
-//     // Send request to LINE server for access token verification
-//     const options = {
-//         url: 'https://api.line.me/v1/oauth/verify',
-//         headers: {
-//             'Authorization': `Bearer ${reqToken}`
-//         }
-//     };
-//     request(options, (error, response, body) => {
-//         if (!error && response.statusCode === 200) {
-//             const lineObj = JSON.parse(body);
-//             // Don't forget to verify the token's channelId to prevent spoof attack
-//             if ((typeof lineObj.mid !== 'undefined')
-//                 && (lineObj.channelId === myLINEChannelId)) {
-//                 // Access Token Validation succeed with LINE server
-//                 // Generate Firebase token and return to device
-//                 const firebaseToken = generateFirebaseToken(lineObj.mid);
-//
-//                 // Update Firebase user profile with LINE profile
-//                 firebase.auth().signInWithCredential()
-//                 admin.updateUserProfile(reqToken, firebaseToken, lineObj.mid, () => {
-//                     const ret = {
-//                         firebase_token: firebaseToken
-//                     };
-//                     return res.status(200).send(ret);
-//                 });
-//             }
-//         }
-//
-//         const ret = {
-//             error_message: 'Authentication error: Cannot verify access token.'
-//         };
-//         return res.status(403).send(ret);
-//
-//     });
-// });
 
 // Generate a Request option to access LINE APIs
 function generateLineApiRequest(apiEndpoint, lineAccessToken) {
@@ -503,116 +440,6 @@ function generateLineApiRequest(apiEndpoint, lineAccessToken) {
     };
 }
 
-
-/**
- * Look up Firebase user based on LINE's mid. If the Firebase user does not exist,
- * fetch LINE profile and create a new Firebase user with it.
- * reference from https://github.com/firebase/functions-samples/blob/master/line-auth/functions/index.js
- * @returns {Promise<UserRecord>} The Firebase user record in a promise.
- */
-// async function getFirebaseUser(lineMid, lineAccessToken) {
-//     // Generate Firebase user's uid based on LINE's mid
-//     const firebaseUid = `line:${lineMid}`;
-//
-//     // LINE's get user profile API endpoint
-//     const getProfileOptions = generateLineApiRequest(envValues.lineConfig.profileURL, lineAccessToken);
-//
-//     try {
-//         const response = await admin.auth().getUser(lineMid);
-//         // Parse user profile from LINE's get user profile API response
-//         const displayName = response.displayName;
-//         const photoURL = response.pictureUrl;
-//         const email = response.email;
-//
-//         console.log('Create new Firebase user for LINE user mid = "', lineMid, '"');
-//         // Create a new Firebase user with LINE profile and return it
-//         return admin.auth().createUser({
-//             uid: lineMid,
-//             displayName: displayName,
-//             photoURL: photoURL,
-//             email: email
-//         });
-//     } catch (error) {
-//         // If user does not exist, fetch LINE profile and create a Firebase new user with it
-//         if (error.code === 'auth/user-not-found') {
-//             return admin.auth().createUser({
-//                 uid: lineMid,
-//                 displayName: displayName,
-//                 photoURL: photoURL,
-//                 email: email
-//             });
-//             // return rp(getProfileOptions);
-//         }
-//         // If error other than auth/user-not-found occurred, fail the whole login process
-//         throw error;
-//     }
-// }
-
-/**
- * Verify LINE access token and return a custom auth token allowing signing-in
- * the corresponding Firebase account.
- *
- * Here are the steps involved:
- *  1. Verify with LINE server that a LINE access token is valid
- *  2. Check if a Firebase user corresponding to the LINE user already existed.
- *  If not, fetch user profile from LINE and generate a corresponding Firebase user.
- *  3. Return a custom auth token allowing signing-in the Firebase account.
- *
- * @returns {Promise<string>} The Firebase custom auth token in a promise.
- */
-async function verifyLineToken(lineAccessToken) {
-    // Send request to LINE server for access token verification
-    const verifyTokenOptions = generateLineApiRequest(envValues.lineConfig.verifyURL, lineAccessToken);
-    // const verifyTokenOptions = generateLineApiRequest('https://api.line.me/v1/oauth/verify', lineAccessToken);
-
-    // STEP 1: Verify with LINE server that a LINE access token is valid
-    const response = await rp(verifyTokenOptions);
-    // Verify the token’s channelId match with my channelId to prevent spoof attack
-    // <IMPORTANT> As LINE's Get user profiles API response doesn't include channelID,
-    // you must not skip this step to make sure that the LINE access token is indeed
-    // issued for your channel.
-    // TODO: consider !== here
-    // if (response.channelId !== functions.config().line.channelid) {
-    //     throw new Error('LINE channel ID mismatched');
-    // }
-
-    // STEP 2: Access token validation succeeded, so look up the corresponding Firebase user
-    const lineMid = response.mid;
-    const userRecord = await getFirebaseUser(lineMid, lineAccessToken);
-    // STEP 3: Generate Firebase Custom Auth Token
-    const token = await admin.auth().createCustomToken(userRecord.uid);
-    console.log('Created Custom token for UID "', userRecord.uid, '" Token:', token);
-    return token;
-}
-
-// Verify LINE token and exchange for Firebase Custom Auth token
-app.post('/verifyToken', async (req, res) => {
-    // exports.verifyToken = functions.https.onRequest(async (req, res) => {
-    if (req.body.token === undefined) {
-        const ret = {
-            error_message: 'Access Token not found',
-        };
-        return res.status(400).send(ret);
-    }
-
-    const reqToken = req.body.token;
-
-    try {
-        // Verify LINE access token with LINE server then generate Firebase Custom Auth token
-        const customAuthToken = await verifyLineToken(reqToken);
-        const ret = {
-            firebase_token: customAuthToken,
-        };
-        return res.status(200).send(ret);
-    } catch (err) {
-        // If LINE access token verification failed, return error response to client
-        const ret = {
-            error_message: 'Authentication error: Cannot verify access token.',
-        };
-        console.error('LINE token verification failed: ', err);
-        return res.status(403).send(ret);
-    }
-});
 
 app.post('/getUserInfo', async (req, res) => {
     let returnJson = {
@@ -734,13 +561,6 @@ app.post('/modifyUserProfile', async function (req, res) {
         if (verifySessionCookie(sessionCookie)) {
             let uid = req.body.uid;
             if (!uid) res.status(400).json({success: false, message: 'missing uid'});
-            // get updateTime,createTime from firestore document original function
-            // doc.createTime,doc.updateTime
-            // {
-            //       "_seconds": 1573540134,
-            //       "_nanoseconds": 604604000
-            // }
-            // let nowTimestamp = ShortcutFunction.timestampUTCmsInt();
             let firestoreUser = await getUserProfile(uid);
             let data = {};
             console.log(firestoreUser.userStats);
@@ -748,8 +568,6 @@ app.post('/modifyUserProfile', async function (req, res) {
                 case 0: //新會員
                     if (!req.body.displayName || !req.body.name || !req.body.phone || !req.body.email || !req.body.birthday)
                         res.status(400).json({success: false, message: 'missing info'});
-                    // data.createTime = nowTimestamp;
-                    // data.updateTime = nowTimestamp;
                     data.displayName = req.body.displayName;    //only new user can set displayName, none changeable value
                     data.name = req.body.name;                  //only new user can set name(real name), none changeable value
                     data.phone = req.body.phone;
@@ -803,104 +621,8 @@ app.post('/modifyUserProfile', async function (req, res) {
 async function uploadAvatar() {
     console.log("compress avatar to 100*100?");
     return "https://uploaded.firestorage.avatar.jpg";
-    // let returnJson = {
-    //     success: false,
-    //     uid: userId
-    // };
-    // try {
-    //     // let userIdStr = userId.toString().trim();
-    //     let userIdStr = userId;
-    //     console.log("get firestore user : ", userIdStr);
-    //
-    //     if (userIdStr.length < 1) {
-    //         console.warn('firebaseGetUserData no userId : ', userId);
-    //         returnJson.userStats = 0;
-    //         returnJson.uid = userId;
-    //         returnJson.success = false;
-    //         return returnJson;
-    //     }
-    //
-    //     let firestore = ShortcutFunction.lazyFirebaseAdmin().firestore();
-    //     let doc = await firestore.collection('users').doc(userIdStr).get();
-    //     if (!doc.exists) {
-    //         console.log('No such document!');
-    //         returnJson.userStats = 0;
-    //         returnJson.success = true;
-    //     } else {
-    //         returnJson.data = doc.data();
-    //         returnJson.userStats = doc.data().stats;
-    //         returnJson.success = true;
-    //     }
-    //     console.log('getFirestoreUser : ', userIdStr, '\n', (JSON.stringify(returnJson, null, '\t')));
-    //     return await returnJson;
-    // } catch (error) {
-    //     console.warn('firebaseGetUserData', error);
-    //     returnJson.success = false;
-    // }
 }
 
-// app.all('/lineLoginHandler', (req, res) => {
-//     // req.checkBody('access_token').notEmpty();
-//
-//     const lineAccessToken = req.query.code;
-//     const lineState = req.query.state;
-//     console.log(lineAccessToken);
-//
-//     // }).then((firebaseCustomToken) => {
-//     //     const result = {
-//     //         firebase_token: firebaseCustomToken,
-//     //     };
-//     //     return res.status(200).json(result);
-//     return res.status(200).json({success: true, access_token: lineAccessToken, state: lineState});
-//
-//     // req.getValidationResult().then(result => {
-//     //     console.info('validating requested data.');
-//     //
-//     //     if (!result.isEmpty()) {
-//     //         return res.status(400).json({
-//     //             error: result.array()
-//     //         });
-//     //     }
-//     // }).then((error) => {
-//     //     if (error) {
-//     //         console.error(error);
-//     //         throw new Error('validating request Error');
-//     //     }
-//     //     console.info('validated. (result OK)');
-//     //     return Promise.resolve();
-//     //
-//     // }).then(() => {
-//     //     // const line_login_util = require('./lineLoginUtil');
-//     //     // return line_login_util.createFirebaseCustomToken(lineAccessToken);
-//     //     console.log(lineAccessToken);
-//     //
-//     //     // }).then((firebaseCustomToken) => {
-//     //     //     const result = {
-//     //     //         firebase_token: firebaseCustomToken,
-//     //     //     };
-//     //     //     return res.status(200).json(result);
-//     //     return res.status(400).json({success: true, access_token: lineAccessToken, state: lineState});
-//     // }).catch(error => {
-//     //     console.error('Error: ', error);
-//     //     return res.status(500).json({error: error});
-//     // });
-// });
-// app.use('/lineLoginHandler', lineLogin.callback(
-//     (req, res, next, token_response) => {
-//         // Success callback
-//         const lineAccessToken = req.query.code;
-//         const lineState = req.query.state;
-//         console.log("lineLoginHandler.....",lineAccessToken);
-//         res.json({access: lineAccessToken, state: lineState, test:"123",response: token_response});
-//         return next()
-//     },
-//     (req, res, next, error) => {
-//         // Failure callback
-//         console.log("lineLoginHandler.....error",req.query.code);
-//         res.status(400).json(error);
-//         return next()
-//     }
-// ));
 
 function getFirebaseUser(accessToken) {
     // const firebaseUid = `line:${body.id}`;
@@ -921,13 +643,9 @@ function getFirebaseUser(accessToken) {
     });
 }
 
-app.all('/lineLoginHandler', (req, res) => {
+app.get('/lineLoginHandler', (req, res) => {
         const lineAccessToken = req.query.code;
-        const lineState = req.query.state;
-
-        if (!secure_compare(req.session.line_login_state, lineState)) {
-            res.status(500).send({error: 'login failed!'});
-        }
+        // const lineState = req.query.state;
 
         // https://api.line.me/oauth2/v2.1/token
         lineLogin.issue_access_token(lineAccessToken).then((token_response) => {
@@ -943,11 +661,12 @@ app.all('/lineLoginHandler', (req, res) => {
                     }
                 );
                 console.log("id token verification succeeded.");
+                console.log("test state", JSON.stringify(token_response));
                 token_response.id_token = decoded_id_token;
 
-                if (!secure_compare(decoded_id_token.nonce, req.session.line_login_nonce)) {
-                    res.status(500).send({error: 'login failed!'});
-                }
+                // if (!secure_compare(decoded_id_token.nonce, req.session.line_login_nonce)) {
+                //     res.status(500).send({error: 'login failed! nonce error'});
+                // }
 
                 lineLogin.verify_access_token(token_response.access_token).then((verify_response) => {
                     if (verify_response.client_id !== envValues.lineConfig.channelID) {
