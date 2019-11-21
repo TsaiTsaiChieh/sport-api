dd5c3df12f4136cabdba8ddb9efa4d0fc9523b37
godzhit <ina2588@gets-info.com>
2019-11-21 15:15:03 +0800

    追加sipHash可以讀取buffer
    將buffer轉base64並將空白轉+號後產生hash

--- functions/shortcut_hash.js
+++ functions/shortcut_hash.js
@@ -2,7 +2,7 @@
 //const longsingShortcutFunction = require( './myfunc' );
 const longsingShortcutFunction = require( './shortcut_function' );
 //const envValues = require( '././env_values' );
-
+const Buffer = require( "buffer" );
 //"@enonic/fnv-plus": "^1.3.0",
 
 
@@ -12,22 +12,45 @@ const siphash = require( 'siphash' );
 
 const crypto = require( 'crypto' );
 
-exports.farmHashToInt = function ( str = '' ) {
+exports.farmHashToInt = function ( stringOrBuffer ) {
 	//, seedInt
 	//if (Number.isInteger(seedInt)) {
 	//	farmhash.hash32WithSeed(str.toString(), seedInt);
 	//}
 
-	return farmhash.fingerprint32( str.toString() );
+	return farmhash.fingerprint32( stringOrBuffer );
 };
 
-exports.sipHashToInt = function ( str = '', key ) {
-	if ( longsingShortcutFunction.haveValue( key ) ) {
-		return siphash.hash_uint( siphash.string16_to_key( key.toString() ), str.toString() );
-	}
 
-	return siphash.hash_uint( siphash.string16_to_key( new Date().getTime().toString() ), str.toString() );
-};
+exports.BufferToSipHashHex =
+	/**
+	 * @param {{ toString: (arg0: string) => { replace: (arg0: RegExp, arg1: string) => void; }; }} buffer
+	 */
+	function ( buffer, intKey1 = 0, intKey2 = 0, intKey3 = 0, intKey4 = 0 ) {
+		//replace( / /g, "+" ); //避免存入資料庫的問題;
+		return this.sipHashToHex( buffer.toString( 'base64' ).replace( / /g, "+" ) );
+
+		/*
+			buffer.toString();
+			if ( longsingShortcutFunction.haveValue( key ) ) {
+				return siphash.hash_hex( siphash.string16_to_key( key.toString() ), str.toString() );
+			}
+
+			return siphash.hash_uint( siphash.string16_to_key( new Date().getTime().toString() ), str.toString() );
+			*/
+	};
+
+exports.sipHashToInt =
+	/**
+	 * @param {{ toString: () => void; }} key
+	 */
+	function ( str = '', key ) {
+		if ( longsingShortcutFunction.haveValue( key ) ) {
+			return siphash.hash_uint( siphash.string16_to_key( key.toString() ), str.toString() );
+		}
+
+		return siphash.hash_uint( siphash.string16_to_key( new Date().getTime().toString() ), str.toString() );
+	};
 
 exports.sipHashToHex = function ( str = '', intKey1 = 0, intKey2 = 0, intKey3 = 0, intKey4 = 0 ) {
 	if ( longsingShortcutFunction.haveValue( intKey1 ) ) {
