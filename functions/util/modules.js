6c8103e143d471c20da8fb69c29ecf3effb0f1b7
蔡采潔 <tsaitsaichieh@caicaijies-MacBook-Pro.local>
2019-12-04 17:15:12 +0800

    add getDoc function for update action

--- functions/util/modules.js
+++ functions/util/modules.js
@@ -8,10 +8,13 @@ function getSnapshot(collection, id) {
     .doc(id)
     .get();
 }
+function getDoc(collection, id) {
+  return firestore.collection(collection).doc(id);
+}
 function createError(code, error) {
   const err = {};
   err.code = code;
   err.error = error;
   return err;
 }
-module.exports = { firebaseAdmin, firestore, getSnapshot, createError };
+module.exports = { firebaseAdmin, firestore, getSnapshot, createError, getDoc };
