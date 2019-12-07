897a045324a51be98beb570e8a3627cec8ae19e4
蔡采潔 <d0177381@gmail.com>
2019-12-07 16:48:17 +0800

    add ajv json validation module

--- functions/util/modules.js
+++ functions/util/modules.js
@@ -1,29 +1,39 @@
 const envValues = require('../config/env_values');
 const firebaseAdmin = require('firebase-admin');
+const Ajv = require('ajv');
+const ajv = new Ajv({ allErrors: true });
+
 firebaseAdmin.initializeApp({
-    credential: firebaseAdmin.credential.cert(envValues.cert),
-    databaseURL: envValues.firebaseConfig.databaseURL
+  credential: firebaseAdmin.credential.cert(envValues.cert),
+  databaseURL: envValues.firebaseConfig.databaseURL
 });
 
 const firestore = firebaseAdmin.firestore();
 
 function getSnapshot(collection, id) {
-    // console.log(collection, id);
-    return firestore
-        .collection(collection)
-        .doc(id)
-        .get();
+  // console.log(collection, id);
+  return firestore
+    .collection(collection)
+    .doc(id)
+    .get();
 }
 
 function getDoc(collection, id) {
-    return firestore.collection(collection).doc(id);
+  return firestore.collection(collection).doc(id);
 }
 
 function createError(code, error) {
-    const err = {};
-    err.code = code;
-    err.error = error;
-    return err;
+  const err = {};
+  err.code = code;
+  err.error = error;
+  return err;
 }
 
-module.exports = {firebaseAdmin, firestore, getSnapshot, createError, getDoc};
+module.exports = {
+  firebaseAdmin,
+  firestore,
+  getSnapshot,
+  createError,
+  getDoc,
+  ajv
+};
