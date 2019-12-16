bd5f8b362bf6021d6f486789ee0f12d716982766
蔡采潔 <d0177381@gmail.com>
2019-12-16 23:04:10 +0800

    add delete message apiDoc

--- public/apidoc/api_project.js
+++ public/apidoc/api_project.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-define({  "name": "GSI sport API",  "version": "0.1.0",  "description": "API Document",  "title": "API Document",  "url": "https://doinfo.cc",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2019-12-16T14:43:04.891Z",    "url": "http://apidocjs.com",    "version": "0.19.0"  }});
-=======
-define({  "name": "GSI sport API",  "version": "0.1.0",  "description": "API Document",  "title": "API Document",  "url": "https://chat-api.doinfo.cc",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2019-12-16T10:05:57.207Z",    "url": "http://apidocjs.com",    "version": "0.19.0"  }});
->>>>>>> master
+define({  "name": "GSI sport API",  "version": "0.1.0",  "description": "API Document",  "title": "API Document",  "url": "https://chat-api.doinfo.cc",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2019-12-16T15:02:25.999Z",    "url": "http://apidocjs.com",    "version": "0.19.0"  }});
