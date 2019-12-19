f591eea346b8470841c5944a61dd37edf0a0564d
蔡采潔 <d0177381@gmail.com>
2019-12-19 10:08:04 +0800

    temp

--- functions/model/admin/giveTitleModel.js
+++ functions/model/admin/giveTitleModel.js
@@ -12,9 +12,10 @@ function giveTitleModel(args) {
       }
       const user = userSnapshot.data();
       let titles = user.titles ? user.titles : [];
-      /* step 2: check if titles duplication */
-
+      /* step 2: insert if titles.length === 0 */
       if (titles.length === 0) {
+        console.log('null');
+
         titles.push({
           rank: args.rank,
           league: args.league,
@@ -26,38 +27,79 @@ function giveTitleModel(args) {
         });
         return;
       }
+      /* step 3: check if titles duplication */
 
-      /* step 3: update rank */
-      titles.forEach(function(ele, idx) {
+      // if ele.rank === 1, insert new title
+      // if ele.rank === 2, update new rank
+      let duplicatedFlag = false;
+      for (let i = 0; i < titles.length; i++) {
+        ele = titles[i];
         if (
           ele.sport === args.sport &&
           ele.league === args.league &&
           ele.rank === args.rank
         ) {
-          // console.log(`..0..`);
+          duplicatedFlag = true;
+          console.log(`${i}, ${titles[i]}, ..0..`);
+          break;
+        }
+        if (duplicatedFlag) {
           reject({
             code: 403,
             error: 'forbidden, this user had the same title'
           });
-          // do nothing
-        } else if (
-          ele.sport === args.sport &&
-          ele.league === args.league &&
-          ele.rank !== args.rank &&
-          ele.rank !== 1
-        ) {
-          // console.log(`..1..`);
-          ele.rank = args.rank;
-        } else {
-          // console.log(`..2..`);
-          titles.push({
-            rank: args.rank,
-            league: args.league,
-            sport: args.sport
-          });
+          return;
         }
-      });
-      userDoc.set({ titles }, { merge: true });
+        /* step 3: update rank */
+        // else if (
+        //   ele.sport === args.sport &&
+        //   ele.league === args.league &&
+        //   ele.rank !== args.rank &&
+        //   ele.rank !== 1
+        // ) {
+        //   console.log(`${i}, ${ele}, ..1..`);
+        //   ele.rank = args.rank;
+        // } else {
+        //   console.log(`${i}, ${ele}, ..2..`);
+        //   titles.push({
+        //     rank: args.rank,
+        //     league: args.league,
+        //     sport: args.sport
+        //   });
+        // }
+      }
+
+      // titles.forEach(function(ele, idx) {
+      //   if (
+      //     ele.sport === args.sport &&
+      //     ele.league === args.league &&
+      //     ele.rank === args.rank
+      //   ) {
+      //     console.log(`..0..`);
+      //     reject({
+      //       code: 403,
+      //       error: 'forbidden, this user had the same title'
+      //     });
+      //     break;
+      //     // do nothing
+      //   } else if (
+      //     ele.sport === args.sport &&
+      //     ele.league === args.league &&
+      //     ele.rank !== args.rank &&
+      //     ele.rank !== 1
+      //   ) {
+      //     console.log(`..1..`);
+      //     ele.rank = args.rank;
+      //   } else {
+      //     console.log(`..2..`);
+      //     titles.push({
+      //       rank: args.rank,
+      //       league: args.league,
+      //       sport: args.sport
+      //     });
+      //   }
+      // });
+      // userDoc.set({ titles }, { merge: true });
       resolve({
         data: `Given user: ${args.uid} a title: [${args.rank} ${args.sport} ${args.league}] successful`
       });
