62f9e7b888ca24681960543cef695ab21c2470d3
Rex <rex@gets-info.com>
2019-12-19 18:09:50 +0800

    temp

--- functions/controller/user/accuseUser.js
+++ functions/controller/user/accuseUser.js
@@ -38,15 +38,22 @@ const modules = require('../../util/modules');
  */// Unique collections: uniqueName,uniqueEmail,uniquePhone
 async function accuseUser(req, res) {
     try {
-        //對象
+        console.log("accuse...")
+        console.log(req.token)
+        const accuser = await modules.getSnapshot('user', req.token.uid);
+        //對象 defendant
+        const defendant = req.body.defendant;
         //原因[打廣告，內容不實，灌水洗版，人身攻擊，其它]
+        const reason = req.body.reason;
         //照片
-        let collection = req.body.type;
-        let value = req.body.value;
-        if (!collection || !value) return res.status(400).json({success: false});
-        const collections = ['uniqueName', 'uniqueEmail', 'uniquePhone'];
-        if (collections.indexOf(collection) < 0) return res.status(400).json({success: false});
-        return res.json(await userUtils.checkUniqueCollection(collection, value));
+        const evidenceImg = req.body.evidence;
+        return res.status(200).json({success: true, accuser});
+        // let collection = req.body.type;
+        // let value = req.body.value;
+        // if (!collection || !value) return res.status(400).json({success: false});
+        // const collections = ['uniqueName', 'uniqueEmail', 'uniquePhone'];
+        // if (collections.indexOf(collection) < 0) return res.status(400).json({success: false});
+        // return res.json(await userUtils.checkUniqueCollection(collection, value));
     } catch (e) {
         console.log(e);
         return res.status(500).json({success: false});
