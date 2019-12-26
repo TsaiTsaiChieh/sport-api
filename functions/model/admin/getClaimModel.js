264d9ccbbbc91c3ff89cf72868e7f09fbe85b057
蔡采潔 <d0177381@gmail.com>
2019-12-26 16:53:29 +0800

    add get claim API in admin router

---
+++ functions/model/admin/getClaimModel.js
@@ -0,0 +1,21 @@
+const modules = require('../../util/modules');
+
+function getClaim(args) {
+  return new Promise(async function(resolve, reject) {
+    /* step 1: check user if exists */
+    try {
+      const userSnapshot = await modules.getSnapshot('users', args.uid);
+      if (!userSnapshot.exists) {
+        reject({ code: 404, error: 'user not found' });
+        return;
+      }
+      const claim = await modules.firebaseAdmin.auth().getUser(args.uid);
+      resolve(claim.customClaims);
+    } catch (err) {
+      console.log('error happened...', err);
+      reject({ code: 500, error: err });
+    }
+  });
+}
+
+module.exports = getClaim;
