1a21652a68bfa669b750c2c178b5996f4110261d
蔡采潔 <d0177381@gmail.com>
2020-01-08 17:40:36 +0800

    firebase serve could not emulate pubsub function, so place the router URL: eventTest for testing

--- functions/router/sport.js
+++ functions/router/sport.js
@@ -4,4 +4,5 @@ const router = express.Router();
 router.get('/', require('../controller/sport/getSports'));
 // ?day=2016-12-01&page=0&sport_id=1
 router.get('/events', require('../controller/sport/getEventsController'));
+router.get('/eventTest', require('../pubsub/updateUpcomingEvent'));
 module.exports = router;
