fee991c560891dd250e0117f88119c022ce97553
蔡采潔 <d0177381@gmail.com>
2020-01-09 15:36:03 +0800

    for testing pubsub URL

--- functions/router/sport.js
+++ functions/router/sport.js
@@ -4,5 +4,6 @@ const router = express.Router();
 router.get('/', require('../controller/sport/getSports'));
 // ?day=2016-12-01&page=0&sport_id=1
 router.get('/events', require('../controller/sport/getEventsController'));
-router.get('/eventTest', require('../pubsub/updateUpcomingEvent'));
+// for testing pubsub
+router.get('/upcoming', require('../pubsub/getUpcomingEvent'));
 module.exports = router;
