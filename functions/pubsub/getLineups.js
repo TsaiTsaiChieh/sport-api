85afd05ded60f2215862b4fb200e09790ac989fb
蔡采潔 <d0177381@gmail.com>
2020-01-10 23:51:40 +0800

    get all event id which status is upcoming(2) and test dummy data

--- functions/pubsub/getLineups.js
+++ functions/pubsub/getLineups.js
@@ -1,29 +1,30 @@
 const modules = require('../util/modules');
 const data = require('./lineup.json');
+const lineupsURL = 'https://api.betsapi.com/v1/event/lineup';
 async function getLineups(req, res) {
-  // step 1: get all event id
-  let sportSnapshot = await modules.firestore
-    .collection(modules.db.sport_events)
-    .doc('18')
-    .collection('1146')
-    .doc('1982486')
+  // step 1: get all event id which status is upcoming(2)
+  let upcomingEvents = [];
+  const upcomingEventsSnapshot = await modules.firestore
+    .collection(modules.db.sport_18)
     .get();
-  // sportSnapshot.docs.forEach(function(doc) {
-  //   console.log('test', doc.data());
-  // });
-  // console.log(sportSnapshot.data());
-  let a = await modules.firestore
-    .collection(`${modules.db.sport_events}/18/1146/1982486`)
-    .get();
-  console.log(a);
+  console.log(modules.betsToken);
 
-  // let a = await modules
-  //   .getSnapshot('sport_test', '18')
-  //   .doc('10769')
-  //   .get();
-  // console.log(a.data());
+  upcomingEventsSnapshot.docs.forEach(async function(doc) {
+    let ele = doc.data();
+    // if (ele.status === 2) {
+    //   const { data } = await modules.axios(
+    //     `${lineupsURL}?token=${modules.betsToken}&event_id=${ele.id}`
+    //   );
+    // }
+    // if (ele.status === 2 && ele.id === '1706128') {
+    //   modules
+    //     .getDoc(modules.db.sport_18, ele.id)
+    //     .set({ home: data.results.home, away: data.results.away });
+    // }
+  });
+  console.log(upcomingEvents.length);
 
-  res.json(sportSnapshot);
+  res.json(upcomingEvents);
   // return data;
 }
 module.exports = getLineups;
