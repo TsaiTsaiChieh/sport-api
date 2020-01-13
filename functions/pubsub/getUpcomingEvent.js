fa630f1c3ef285a7cf45b2f0ca5f7e2fe4303c8c
蔡采潔 <d0177381@gmail.com>
2020-01-13 14:47:19 +0800

    finish getting upcoming events regularly at 12 A.M.

--- functions/pubsub/getUpcomingEvent.js
+++ functions/pubsub/getUpcomingEvent.js
@@ -1,107 +1,115 @@
 /* eslint-disable no-await-in-loop */
 const modules = require('../util/modules');
 // dummy data
-const data = require('./upcoming_baseketball.json');
+// const data = require('./upcoming_baseketball.json');
 const upcomingURL = 'https://api.betsapi.com/v2/events/upcoming';
 const oddsURL = 'https://api.betsapi.com/v2/event/odds';
-const token = '35388-8IqMa0NK19LJVY';
-async function getUpcomingEvent(req, res) {
-  // const sport_ids = [];
-  let result = await getUpcomingSportEvent();
-  // console.log(result);
-  res.json(result);
+const token = modules.betsToken;
+// async function getUpcomingEvent(req, res) {
+async function getUpcomingEvent() {
+  const sport_id = 18;
+  const baseketball_leagues = [
+    2274, // NBA
+    8251, // SBL
+    244, // WNBA
+    1714, // NBL
+    2319, // CBA
+    2148, // KBL
+    1298, // JBL
+    1543,
+    2630
+  ];
+  let result = await getUpcomingSportEvent(sport_id, baseketball_leagues);
+  console.log(result);
+  return result;
+  // res.json(result);
 }
 
-async function getUpcomingSportEvent(sport_ids) {
+async function getUpcomingSportEvent(sport_id, league_ids) {
   try {
     // tomorrow
     let date = new Date(Date.now() + 24 * 60 * 60 * 1000)
       .toISOString()
       .slice(0, 10)
       .replace(/-/g, '');
-
-    let sport_id = 18;
-    // NBA(2274), SBL(8251), WNBA(244), NBL(1714), CBA(2319), KBL(2148), JBL
-    let league_id = [2274, 8251, 244, 1714, 2319, 2148, 1298, 1543, 2630];
-    // const { data } = await modules.axios(
-    //   `${upcomingURL}?token=${token}&sport_id=${sport_id}&league_id=${league_id}&day=${date}&page=1`
-    // );
     let events = [];
     // for real data
     // league loop
-    // for (let i = 0; i < league_id.length; i++) {
-    //   let { data } = await modules.axios(
-    //     `${upcomingURL}?token=${token}&sport_id=${sport_id}&league_id=${league_id[i]}&day=${date}&page=1`
-    //   );
-    //   events.push(data.results);
-    //   // page loop
-    //   totalPage = Math.ceil(data.pager.total / data.pager.per_page);
-    //   if (totalPage > 1)
-    //     for (let j = 2; j <= totalPage; j++) {
-    //       data = await modules.axios(
-    //         `${upcomingURL}?token=${token}&sport_id=${sport_id}&league_id=${league_id[i]}&day=${date}&page=${j}`
-    //       );
-    //       events.push(data.results);
-    //     }
-    // }
-
-    // events = repackage(events);
-    // for (let i = 0; i < events.length; i++) {
-    // let ele = events[i];
-    // ele.time = modules.firebaseAdmin.firestore.Timestamp.fromDate(
-    //   new Date(Number.parseInt(ele.time) * 1000)
-    // );
-    // event status: end(0), inplay(1), upcoming(2)
-    // ele.status = 2;
-    //   modules.firebase
-    //     .collection(modules.db.sport_events)
-    //     .doc(ele.sport_id)
-    //     .collection(ele.league_id)
-    //     .doc(ele.id)
-    //     .set(ele, { merge: true });
-    // }
-    // // console.log(
-    // //   `${upcomingURL}?token=${token}&sport_id=${sport_id}&league_id=${league_id}&day=${date}`
-    // // );
-    // return events;
-
-    // for dummy data
-    for (let i = 0; i < data.results.length; i++) {
-      events.push(data.results[i]);
+    for (let i = 0; i < league_ids.length; i++) {
+      let { data } = await modules.axios(
+        `${upcomingURL}?token=${token}&sport_id=${sport_id}&league_id=${league_ids[i]}&day=${date}&page=1`
+      );
+      pushData(events, data.results);
+      // page loop
+      totalPage = Math.ceil(data.pager.total / data.pager.per_page);
+      if (totalPage > 1)
+        for (let j = 2; j <= totalPage; j++) {
+          data = await modules.axios(
+            `${upcomingURL}?token=${token}&sport_id=${sport_id}&league_id=${league_ids[i]}&day=${date}&page=${j}`
+          );
+          pushData(data.results);
+        }
     }
+
     events = repackage(events);
-    // add dummy data for virtual documents
-    // ref: https://stackoverflow.com/questions/47043651/this-document-does-not-exist-and-will-not-appear-in-queries-or-snapshots-but-id
-    // modules.firestore
-    //   .collection(modules.db.sport_events)
-    //   .doc(data.results[0].sport_id)
-    //   .set({ name: 'baseketball' });
     for (let i = 0; i < events.length; i++) {
       let ele = events[i];
+      // insert data time
+      ele.addTime = modules.firebaseAdmin.firestore.Timestamp.now();
+      // event start time
       ele.time = modules.firebaseAdmin.firestore.Timestamp.fromDate(
         new Date(Number.parseInt(ele.time) * 1000)
       );
+
       // event status: end(0), inplay(1), upcoming(2)
       ele.status = 2;
-
       await modules.firestore
         .collection(modules.db.sport_18)
         .doc(ele.id)
         .set(ele, { merge: true });
     }
-    return events;
+    // return events;
+    const message = `Get incoming events in ${modules.db.sport_18} successful by Tsai-Chieh`;
+    return message;
+
+    // for dummy data
+    // for (let i = 0; i < data.results.length; i++) {
+    //   events.push(data.results[i]);
+    // }
+    // events = repackage(events);
+    // for (let i = 0; i < events.length; i++) {
+    //   let ele = events[i];
+    //   ele.time = modules.firebaseAdmin.firestore.Timestamp.fromDate(
+    //     new Date(Number.parseInt(ele.time) * 1000)
+    //   );
+    //   // event status: end(0), inplay(1), upcoming(2)
+    //   ele.status = 2;
+
+    //   await modules.firestore
+    //     .collection(modules.db.sport_18)
+    //     .doc(ele.id)
+    //     .set(ele, { merge: true });
+    // }
+    // return events;
   } catch (error) {
     console.log(
-      'error happened in updateUpcomingEvent axios function by Tsai-Chieh',
+      'error happened in getUpcomingEvent axios function by Tsai-Chieh',
       error
     );
+    return error;
   }
 }
-
+function pushData(events, data) {
+  if (data.length !== 0) {
+    data.forEach(function(ele) {
+      events.push(ele);
+    });
+  }
+}
 function repackage(data) {
   data.forEach(function(ele) {
     if (ele.time_status) delete ele.time_status;
-    if (!ele.ss) delete ele.ss; // ss always null
+    if (!ele.ss) delete ele.ss; //// ss always null
   });
   return data;
 }
