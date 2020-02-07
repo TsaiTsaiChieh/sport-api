b84e0bda58f2b1aa973cc53b754244e8ab4a8051
蔡采潔 <d0177381@gmail.com>
2020-02-07 10:18:59 +0800

    add return in callback function

--- functions/index.js
+++ functions/index.js
@@ -37,9 +37,9 @@ const whitelist = [
 const corsOptions = {
   origin: function(origin, callback) {
     if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
+      return callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'));
+      return callback(new Error('Not allowed by CORS'));
     }
   }
 };
