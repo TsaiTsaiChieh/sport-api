84e1b6da798fa19c53e84dd5731325f4398c5861
蔡采潔 <d0177381@gmail.com>
2020-02-11 10:36:51 +0800

    rm busboy modules

--- functions/router/messages.js
+++ functions/router/messages.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express();
 const verification = require('../util/verification');
 const modules = require('../util/modules');
-const middlewares = require('../util/middlewares');
 
 // Just test
 router.get('/list', async (req, res) => {
