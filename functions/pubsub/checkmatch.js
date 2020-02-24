db136952fc780a99288b80c2ddccea291108293c
page40316 <page@gets-info.com>
2020-02-24 16:29:42 +0800

    scheduled test

--- functions/pubsub/checkmatch.js
+++ functions/pubsub/checkmatch.js
@@ -1,9 +1,12 @@
 const modules = require("../util/modules");
-
-const pbpNBA = require("./pbpNBA.js");
+const NBApbp = require("./pbpNBA.js");
+const NBApbpInplay = NBApbp.NBApbpInplay;
+const NBApbpHistory = NBApbp.NBApbpHistory;
+const timesPerLoop = 11;
+const firestoreName = "test_basketball";
 async function checkmatch(req, res) {
   //read event information from firestore
-  var data = await modules.firestore.collection("test_basketball").get();
+  var data = await modules.firestore.collection(firestoreName).get();
   var totalData = [];
   data.forEach(doc => {
     totalData.push(doc.data());
@@ -24,12 +27,14 @@ async function checkmatch(req, res) {
   // 所有賽事判斷
 
   for (var i = 0; i < totalData.length; i++) {
-    var gameID = totalData[i].id;
+    var betsID = totalData[i].bets_id;
+    var gameID = totalData[i].radar_id;
 
-    var gameTime = totalData[i].scheduleTime._seconds * 1000;
+    var gameTime = totalData[i].scheduled._seconds * 1000;
     var nowTime = Date.now();
     //event status 0:history 1:now 2:future
-    if (totalData[i].flag.status === 1) {
+    var eventStatus = totalData[i].flag.status;
+    if (eventStatus === 1) {
       // check the periods and event in firebase realtime database now
       var realtimeData;
       realtimeData = JSON.parse(
@@ -38,31 +43,69 @@ async function checkmatch(req, res) {
           await modules.database.ref(`basketball/NBA/${gameID}`).once("value")
         )
       );
-      //   eslint-disable-next-line no-await-in-loop
 
       var periodsNow = Object.keys(realtimeData.periods).length - 1; //how much periods
       var periodName = Object.keys(realtimeData.periods);
       var eventNow =
         Object.keys(realtimeData.periods[periodName[periodsNow]]).length - 1;
 
-      pbpNBA(gameID, periodsNow, eventNow);
-
-      //pbpNBA();
+      //write to the firebase realtime
+      var status = realtimeData.status;
+      if (status === "closed") {
+        NBApbpHistory(gameID, betsID);
+        modules.firestore
+          .collection(firestoreName)
+          .doc(betsID)
+          .set({ flag: { status: 0 } }, { merge: true });
+      } else {
+        var countForStatus = 0;
+        // eslint-disable-next-line no-loop-func
+        var timerForStatus = setInterval(async function() {
+          [periodsNow, eventNow] = await NBApbpInplay(
+            gameID,
+            betsID,
+            periodsNow,
+            eventNow
+          );
+          countForStatus = countForStatus + 1;
+          if (countForStatus >= timesPerLoop) {
+            clearInterval(timerForStatus);
+          }
+        }, 5000);
+      }
     }
-    if (totalData[i].flag.status === 2) {
+    if (eventStatus === 2) {
       if (gameTime <= nowTime) {
-        // eslint-disable-next-line no-await-in-loop
-        //change the status to 1
-        //pbpNBA();
-        // modules.firestore
-        //   .collection("test_basketball")
-        //   .doc(gameID)
-        //   .set({ flag: { status: 1 } }, { merge: true });
+        // change the status to 1
+        modules.firestore
+          .collection(firestoreName)
+          .doc(betsID)
+          .set({ flag: { status: 1 } }, { merge: true });
+        // write to firebase realtime
+
+        periodsNow = 0;
+        eventNow = 0;
+        var countForStatus2 = 0;
+
+        // eslint-disable-next-line no-loop-func
+        var timerForStatus2 = setInterval(async function() {
+          [periodsNow, eventNow] = await NBApbpInplay(
+            gameID,
+            betsID,
+            periodsNow,
+            eventNow
+          );
+          countForStatus2 = countForStatus2 + 1;
+
+          if (countForStatus2 >= timesPerLoop) {
+            clearInterval(timerForStatus2);
+          }
+        }, 5000);
       }
     }
   }
-
-  res.json(realtimeData);
+  //res.json(realtimeData);
+  res.json({ process: "success" });
 }
 
 module.exports = checkmatch;
--- functions/pubsub/pbpNBA.js
+++ functions/pubsub/pbpNBA.js
@@ -3,8 +3,9 @@ const modules = require("../util/modules");
 const axios = require("axios");
 const nba_api_key = "y7uxzm4stjju6dmkspnabaav";
 
-async function NBApbpInplay(gameID, periodsNow, eventsNow) {
+async function NBApbpInplay(gameID, betsID, periodsNow, eventsNow) {
   const URL = `http://api.sportradar.us/nba/trial/v7/en/games/${gameID}/pbp.json?api_key=${nba_api_key}`;
+
   try {
     dataFromAPI = await axios(URL);
     for (
@@ -17,11 +18,11 @@ async function NBApbpInplay(gameID, periodsNow, eventsNow) {
         eventsCount < dataFromAPI.periods[periodsCount].events.length;
         eventsCount++
       ) {
-        var ref = db.ref(
-          `basketball/${gameID}/pbp/periods${periodsCount}/events${eventsCount}`
+        var ref = modules.database.ref(
+          `basketball/NBA/${gameID}/periods${periodsCount}/events${eventsCount}`
         );
         // eslint-disable-next-line no-await-in-loop
-        // await ref.set(dataFromAPI.periods[periodsCount].events[eventsCount]);
+        await ref.set(dataFromAPI.periods[periodsCount].events[eventsCount]);
       }
     }
   } catch (error) {
@@ -31,6 +32,71 @@ async function NBApbpInplay(gameID, periodsNow, eventsNow) {
     );
     return error;
   }
-  console.log(gameID, periodsNow, eventsNow);
+
+  periodsNow = periodsCount - 1;
+  eventsNow = eventsCount - 1;
+  return [periodsNow, eventsNow];
 }
-module.exports = NBApbpInplay;
+async function NBApbpHistory(gameID, betsID) {
+  const URL = `http://api.sportradar.us/nba/trial/v7/en/games/${gameID}/pbp.json?api_key=${nba_api_key}`;
+  try {
+    dataFromAPI = await axios(URL);
+
+    var awayInfo = {
+      teamAlias: dataFromAPI.away.alias,
+      teamID: dataFromAPI.away.id,
+      teamMarket: dataFromAPI.away.market,
+      teamName: dataFromAPI.away.name,
+      teamSrID: dataFromAPI.away.sr_id
+    };
+    var homeInfo = {
+      teamAlias: dataFromAPI.home.alias,
+      teamID: dataFromAPI.home.id,
+      teamMarket: dataFromAPI.home.market,
+      teamName: dataFromAPI.home.name,
+      teamSrID: dataFromAPI.home.sr_id
+    };
+    var winner;
+    if (dataFromAPI.home.points > dataFromAPI.away.points) {
+      winner = "home";
+    } else {
+      winner = "away";
+    }
+    var finalScore = {
+      homePoints: dataFromAPI.home.points,
+      awayPoints: dataFromAPI.away.points,
+      winner: winner
+    };
+    var dataOutput = {
+      awayInfo: awayInfo,
+      homeInfo: homeInfo,
+      finalScore: finalScore,
+      gameID: dataFromAPI.id,
+      gameSrID: dataFromAPI.sr_id,
+      startTime: dataFromAPI.scheduled
+    };
+    var ref = modules.firestore.collection("test_basketball").doc(betsID);
+    await ref.set(dataOutput);
+    for (
+      var periodsCount = 0;
+      periodsCount < dataFromAPI.periods.length;
+      periodsCount++
+    ) {
+      for (
+        var eventsCount = 0;
+        eventsCount < dataFromAPI.periods[periodsCount].events.length;
+        eventsCount++
+      ) {
+        ref = db.collection("test_basketball").doc(betsID);
+        ref.set(data.periods[periodsCount].events[eventsCount]);
+      }
+    }
+  } catch (error) {
+    console.log(
+      "error happened in pubsub/NBApbpHistory function by page",
+      error
+    );
+    return error;
+  }
+}
+module.exports = { NBApbpInplay, NBApbpHistory };
