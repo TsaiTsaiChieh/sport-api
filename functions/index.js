46e38ad8a123beda8188b8f38327e4a34d023512
蔡采潔 <d0177381@gmail.com>
2020-02-26 15:14:20 +0800

    fix schedule time in cronHandicap function

--- functions/index.js
+++ functions/index.js
@@ -67,7 +67,7 @@ exports.cronPrematch = functions.pubsub
   .timeZone('Asia/Taipei')
   .onRun(require('./pubsub/prematch'));
 exports.cronHandicap = functions.pubsub
-  .schedule('* */24 * * *')
+  .schedule('* */1 * * *')
   .timeZone('Asia/Taipei')
   .onRun(require('./pubsub/handicap'));
 exports.api = functions.https.onRequest(app);
