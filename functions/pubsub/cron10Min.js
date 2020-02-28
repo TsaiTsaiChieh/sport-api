f7560bcd18761c8db0ee2293bd69f67395ed1123
蔡采潔 <d0177381@gmail.com>
2020-02-28 22:50:29 +0800

    set NBA API key to json setting

--- functions/pubsub/cron10Min.js
+++ functions/pubsub/cron10Min.js
@@ -1,8 +1,7 @@
 const modules = require('../util/modules');
-const NBA_api_key = '48v65d232xsk2am8j6yu693v';
 const NBA_functions = require('./util/prematchFunctions_NBA');
 function cron10Min() {
   const date = modules.moment();
-  NBA_functions.NBA.lineup(date, NBA_api_key);
+  NBA_functions.NBA.lineup(date, modules.db.basketball_NBA);
 }
 module.exports = cron10Min;
