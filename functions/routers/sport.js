93d6ffec4bfe4ca062a2a707532fcfddc16e671c
蔡采潔 <d0177381@gmail.com>
2020-03-02 13:47:22 +0800

    rm useless endpoint

--- functions/routers/sport.js
+++ functions/routers/sport.js
@@ -2,13 +2,4 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', require('../controller/sport/getSports'));
-// ?day=2016-12-01&page=0&sport_id=1
-router.get('/events', require('../controller/sport/getEventsController'));
-// for testing pubsub
-router.get('/testUpcoming', require('../pubsub/getUpcomingEvent'));
-// for test lineup
-router.get('/testLineups', require('../pubsub/getLineups'));
-// for testing handicap
-router.get('/testOdds', require('../pubsub/getHandicap'));
-router.get('/prematch', require('../controller/sport/prematchController'));
 module.exports = router;
