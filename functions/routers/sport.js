5a6a87414c8f34e61ec502488b03744654e24ae1
蔡采潔 <d0177381@gmail.com>
2020-03-02 18:18:22 +0800

    add prematch endpoint

--- functions/routers/sport.js
+++ functions/routers/sport.js
@@ -2,4 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', require('../controller/sport/getSports'));
+router.get('/prematch', require('../controller/sport/prematchController'));
+
 module.exports = router;
