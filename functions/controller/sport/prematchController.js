b804ed1ddc0206d14d57647a8ab190535140f348
蔡采潔 <d0177381@gmail.com>
2020-03-02 18:20:35 +0800

    prematch functions (not completed)

--- functions/controller/sport/prematchController.js
+++ functions/controller/sport/prematchController.js
@@ -1,12 +1,31 @@
+const modules = require('../../util/modules');
 const model = require('../../model/sport/prematchModel');
-async function prematch(req, res) {
-  console.log(req.query.date);
-  try {
-    await model(req.query);
-  } catch (error) {
-    console.log(error);
-  }
 
-  res.json(req.query.date);
+async function prematch(req, res) {
+  const schema = {
+    type: 'object',
+    require: ['date', 'league'],
+    properties: {
+      date: {
+        type: 'string',
+        format: 'date'
+      },
+      league: {
+        type: 'string',
+        enum: ['NBA']
+      }
+    }
+  };
+  const valid = modules.ajv.validate(schema, req.query);
+  if (!valid) {
+    res.status(400).json(modules.ajv.errors);
+    return;
+  }
+  try {
+    res.json(await model(req.query));
+  } catch (err) {
+    res.status(err.code).json(err);
+  }
 }
+
 module.exports = prematch;
--- functions/model/sport/prematchModel.js
+++ functions/model/sport/prematchModel.js
@@ -1,28 +1,84 @@
 const modules = require('../../util/modules');
+
 function premath(args) {
   return new Promise(async function(resolve, reject) {
-    const basketRef = modules.firestore.collection(modules.db.sport_18);
-    // let a = await basketRef.where('league.name', '==', args.league).get();
-    // var beginningDate = Date.now() - 604800000;
-    // var beginningDateObject = new Date(beginningDate);
-
-    const beginningDate = modules.moment(args.date);
-    const endDate = modules.moment(args.date).add(1, 'days');
+    const matchsRef = modules.firestore.collection(
+      collectionCodebook(args.league)
+    );
+    const beginningDate = modules.moment(args.date).utcOffset(8);
+    const endDate = modules
+      .moment(args.date)
+      .utcOffset(8)
+      .add(1, 'days');
 
     try {
-      let a = await basketRef
-        .where('league.name', '==', args.league)
+      const querys = await matchsRef
         .where('scheduled', '>=', beginningDate)
         .where('scheduled', '<', endDate)
         .get();
-      a.forEach(function(ele) {
-        console.log(ele.data().id);
+      const results = [];
+      querys.docs.map(function(ele) {
+        results.push(repackage(ele.data()));
       });
-
-      resolve(args.date);
-    } catch (error) {
-      reject(error);
+      resolve(results);
+    } catch (err) {
+      console.error('Error in sport/prematchModel by TsaiChieh', err);
+      reject({ code: 500, error: err });
+      return;
     }
   });
 }
+// eslint-disable-next-line consistent-return
+function collectionCodebook(league) {
+  switch (league) {
+    case 'NBA':
+      return modules.db.basketball_NBA;
+  }
+}
+function repackage(ele) {
+  data = {};
+  data.id = ele.bets_id;
+  data.handicap = {};
+  data.handicap.spread = {};
+  data.handicap.totals = {};
+
+  if (ele.handicap) {
+    if (ele.handicap.spread) {
+      for (const obj in ele.handicap.spread) {
+        data.handicap.spread[obj] = {
+          handicap: ele.handicap.spread[obj].handicap,
+          add_time: modules.timestampeFormat(ele.handicap.spread[obj].add_time),
+          home_odd: ele.handicap.spread[obj].home_odd,
+          away_odd: ele.handicap.spread[obj].away_odd,
+          insert_time: modules.timestampeFormat(
+            ele.handicap.spread[obj].add_time
+          )
+        };
+      }
+    }
+    if (ele.handicap.totals) {
+      for (const obj in ele.handicap.totals) {
+        data.handicap.totals[obj] = {
+          handicap: ele.handicap.totals[obj].handicap,
+          add_time: modules.timestampeFormat(ele.handicap.totals[obj].add_time),
+          home_odd: ele.handicap.totals[obj].home_odd,
+          away_odd: ele.handicap.totals[obj].away_odd,
+          insert_time: modules.timestampeFormat(
+            ele.handicap.totals[obj].add_time
+          )
+        };
+      }
+    }
+  }
+  data.home = {
+    // alias_ch
+    name: ele.home.name,
+    alias_ch: ele.home.alias_ch,
+    alias_name: ele.home.alias_name,
+    image_id: ele.home.image_id,
+    id: ele.home.radar_id
+  };
+  return data;
+}
+
 module.exports = premath;
