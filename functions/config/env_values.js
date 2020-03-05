20666a1e48e5a083e134db810b924b5421946d18
蔡采潔 <d0177381@gmail.com>
2020-03-05 13:42:59 +0800

    add sportRadar.rest

--- functions/config/env_values.js
+++ functions/config/env_values.js
@@ -3,12 +3,9 @@ exports.productURL = 'https://chat.doinfo.cc/';
 
 exports.domain = '.doinfo.cc';
 exports.release = false;
-
-// sport19y0715-dev 為正式版
+// 正式版 sport19y0715-dev
+// exports.projectId = 'sport19y0715';
 // exports.cert = require('../auth/sport19y0715-dev.json');
-// sportslottery-test-adminsdk 為測試版
-exports.cert = require('../auth/sportslottery-test-adminsdk.json');
-// 正式版
 // exports.firebaseConfig = {
 //   apiKey: 'AIzaSyB31V6WewUi-iY12231Ixahquf68uGaoCo',
 //   authDomain: 'sport19y0715.firebaseapp.com',
@@ -18,7 +15,9 @@ exports.cert = require('../auth/sportslottery-test-adminsdk.json');
 //   messagingSenderId: '179049951227',
 //   appId: '1:179049951227:web:15b2ae874d653216'
 // };
-// 測試版
+// 測試版 sportslottery-test-adminsdk
+exports.projectId = 'sportslottery-test';
+exports.cert = require('../auth/sportslottery-test-adminsdk.json');
 exports.firebaseConfig = {
   apiKey: 'AIzaSyByoBAdesDJHNpT-d31y08UYcOwt5KeaBE',
   authDomain: 'sportslottery-test.firebaseapp.com',
@@ -29,10 +28,6 @@ exports.firebaseConfig = {
   appId: '1:969081540385:web:da08ff289d0bec4ca9b860',
   measurementId: 'G-WRP22SQG9M'
 };
-// 正式版
-// exports.projectId = 'sport19y0715'; 
-// 測試版
-exports.projectId = 'sportslottery-test';
 
 exports.sharefilePath5T = 'share_files/'; //目前聊天室的檔案(5T區域)放置的有效資料夾,前端則是放到'uploadTemp/'
 //exports.base64test = 'data:image/png;base64,...';
