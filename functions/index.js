5597c97a5a5a3656cdbfac1f8d34ac9ea28fe7b2
蔡采潔 <d0177381@gmail.com>
2020-03-13 18:21:28 +0800

    rm node-schedule module

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,18 @@
 // const ssr = require("./server/index.js");
 
 /* eslint-disable no-unused-vars */
-const functions = require("firebase-functions");
-const express = require("express");
-const cors = require("cors");
+const functions = require('firebase-functions');
+const express = require('express');
+const cors = require('cors');
 
-let bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const nodeSchedule = require("node-schedule");
+let bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+// const nodeSchedule = require("node-schedule");
 const app = express();
 
 app.use(cookieParser());
-app.disable("x-powered-by");
-const helmet = require("helmet");
+app.disable('x-powered-by');
+const helmet = require('helmet');
 app.use(helmet());
 app.use(helmet.xssFilter());
 
@@ -20,59 +20,59 @@ app.use(helmet.frameguard());
 
 app.use(
   bodyParser.json({
-    limit: "50mb",
+    limit: '50mb',
     extended: true
   })
 );
 app.use(
   bodyParser.urlencoded({
-    limit: "50mb",
+    limit: '50mb',
     extended: true
   })
 );
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader(
-    "Cache-Control",
-    "no-store, no-cache, must-revalidate, private"
+    'Cache-Control',
+    'no-store, no-cache, must-revalidate, private'
   );
   next();
 });
 
 const whitelist = [
-  "https://chat.doinfo.cc",
-  "https://doinfo.cc",
-  "http://localhost:5000",
-  "http://localhost:8080",
-  "http://localhost:8081",
-  "http://192.168.0.195:8080",
-  "http://192.168.0.170:8080",
-  "http://192.168.0.113:8080",
-  "http://192.168.0.148:8080",
-  "http://dosports.web.app"
+  'https://chat.doinfo.cc',
+  'https://doinfo.cc',
+  'http://localhost:5000',
+  'http://localhost:8080',
+  'http://localhost:8081',
+  'http://192.168.0.195:8080',
+  'http://192.168.0.170:8080',
+  'http://192.168.0.113:8080',
+  'http://192.168.0.148:8080',
+  'http://dosports.web.app'
 ];
 const corsOptions = {
   origin: function(origin, callback) {
     if (whitelist.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
-      console.log("Not allowed by CORS", origin);
-      return callback(new Error("Not allowed by CORS"));
+      console.log('Not allowed by CORS', origin);
+      return callback(new Error('Not allowed by CORS'));
     }
   }
 };
 app.use(cors(corsOptions));
 
 app.use(express.json());
-app.use("/admin", require("./routers/admin"));
-app.use("/auth", require("./routers/authentication"));
-app.use("/user", require("./routers/user"));
-app.use("/messages", require("./routers/messages"));
-app.use("/sport", require("./routers/sport"));
+app.use('/admin', require('./routers/admin'));
+app.use('/auth', require('./routers/authentication'));
+app.use('/user', require('./routers/user'));
+app.use('/messages', require('./routers/messages'));
+app.use('/sport', require('./routers/sport'));
 // app.use('/messages_temp', require('./Deprecated/messages'));
 // for test pubsub endpoint
-app.use("/pubsub", require("./routers/pubsub"));
+app.use('/pubsub', require('./routers/pubsub'));
 
 exports.prematch = functions.pubsub
   .schedule('0 5 * * *')
