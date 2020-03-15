8582e76ce1bb3159fc95c2ae9ba1b7a0f6a14348
蔡采潔 <d0177381@gmail.com>
2020-03-15 22:59:50 +0800

    add backup & restore functions

---
+++ functions/pubsub/backupFirestore.js
@@ -0,0 +1,31 @@
+const modules = require('../util/modules');
+const testServiceAccount = require('../auth/sportslottery-test-adminsdk.json');
+const testDatabaseURL = 'https://sportslottery-test.firebaseio.com';
+const officialServiceAccount = require('../auth/sport19y0715-dev.json');
+const officialDatabaseURL = 'https://sport19y0715.firebaseio.com';
+const jsonFile = require('../json/input.json');
+
+function backupFirestore() {
+  // Initiate Firebase App
+  modules.firestoreService.initializeApp(testServiceAccount, testDatabaseURL);
+  const collections = ['NBA_TC'];
+  // Start exporting your data
+  modules.firestoreService.backups(collections).then(function(data) {
+    modules.fs.writeFile('./json/input.json', JSON.stringify(data), function(
+      err
+    ) {
+      if (err) throw err;
+      console.log(`Backups complete ${new Date()}`);
+    });
+  });
+}
+async function restoreFirestore() {
+  modules.firestoreService.initializeApp(
+    officialServiceAccount,
+    officialDatabaseURL
+  );
+  modules.firestoreService.restore(jsonFile, {
+    dates: ['update_time', 'scheduled']
+  });
+}
+module.exports = { backupFirestore, restoreFirestore };
