95f3db7b93e3c881a6f519ee9034668931db929f
蔡采潔 <d0177381@gmail.com>
2020-03-16 18:15:56 +0800

    just finish ajv check parameters

---
+++ functions/controller/user/predictMatchesController.js
@@ -0,0 +1,58 @@
+const modules = require('../../util/modules');
+const model = require('../../model/user/predictMatchesModel');
+
+// eslint-disable-next-line consistent-return
+async function predictMatches(req, res) {
+  const schema = {
+    type: 'object',
+    required: ['league', 'matches'],
+    properties: {
+      league: {
+        type: 'string',
+        enum: ['NBA']
+      },
+      matches: {
+        type: 'array',
+        items: {
+          type: 'object',
+          required: ['id'],
+          anyOf: [{ required: ['spread'] }, { required: ['totals'] }],
+          properties: {
+            id: {
+              type: 'string'
+            },
+            spread: {
+              type: 'array',
+              items: [
+                { type: 'string' },
+                { type: 'string', enum: ['home', 'away'] },
+                { type: 'integer', minimum: 1, maximum: 3 }
+              ]
+            },
+            totals: {
+              type: 'array',
+              item: [
+                { type: 'string' },
+                { type: 'string', enum: ['over', 'under'] },
+                { type: 'integer', minimum: 1, maximum: 3 }
+              ]
+            }
+          }
+        }
+      }
+    }
+  };
+
+  const valid = modules.ajv.validate(schema, req.body);
+  if (!valid) {
+    return res.status(400).json(modules.ajv.errors);
+  }
+  req.body.token = req.token;
+  try {
+    res.json(await model(req.body));
+  } catch (err) {
+    res.status(err.code).json(err);
+  }
+}
+
+module.exports = predictMatches;
---
+++ functions/model/user/predictMatchesModel.js
@@ -0,0 +1,11 @@
+const modules = require('../../util/modules');
+
+function predictMatches(args) {
+  return new Promise(function(resolve, reject) {
+    // 1. Check the match ids
+
+    resolve(args);
+  });
+}
+
+module.exports = predictMatches;
