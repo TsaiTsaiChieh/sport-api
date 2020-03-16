dfdac5aab62989792b74c944151c566e1d3cacd4
蔡采潔 <d0177381@gmail.com>
2020-03-16 16:49:34 +0800

    add predictMatches API

--- functions/routers/user.js
+++ functions/routers/user.js
@@ -2,34 +2,29 @@ const express = require('express');
 const router = express.Router();
 const verification = require('../util/verification');
 
-router.get(
-    '/getRanks/',
-    require('../controller/user/getRanks')
-);
+router.get('/getRanks/', require('../controller/user/getRanks'));
+router.post('/getUserProfile', require('../controller/user/getUserProfile'));
 router.post(
-    '/getUserProfile',
-    require('../controller/user/getUserProfile')
+  '/modifyUserProfile',
+  verification.token,
+  require('../controller/user/modifyUserProfile')
 );
+router.post('/checkUnique/', require('../controller/user/checkUnique'));
 router.post(
-    '/modifyUserProfile',
-    verification.token,
-    require('../controller/user/modifyUserProfile')
-);
-router.post(
-    '/checkUnique/',
-    require('../controller/user/checkUnique')
-);
-router.post(
-    '/accuse/',
-    verification.token,
-    require('../controller/user/accuseUser')
+  '/accuse/',
+  verification.token,
+  require('../controller/user/accuseUser')
 );
 router.get(
-    '/getTitlesAndSignature/:uid',
-    require('../controller/user/getTitlesAndSignatureController')
+  '/getTitlesAndSignature/:uid',
+  require('../controller/user/getTitlesAndSignatureController')
 );
-router.get(
-    '/getClaim/:uid',
-    require('../controller/user/getClaimController')
+router.get('/getClaim/:uid', require('../controller/user/getClaimController'));
+
+// 預測頁
+router.post(
+  '/predictMatches',
+  verification.token,
+  require('../controller/user/predictMatchesController')
 );
 module.exports = router;
