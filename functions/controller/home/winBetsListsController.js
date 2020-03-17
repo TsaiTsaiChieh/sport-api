c9e493431a777c50db41b20aeb95885e58946678
Alan <alan@Alans-MacBook-Pro.local>
2020-03-17 14:10:13 +0800

    勝注檔名修正 勝注第一次上傳

--- functions/controller/home/winBetsListsController.js
+++ functions/controller/home/winBetsListsController.js
@@ -1,140 +1,140 @@
 const modules = require('../../util/modules');
 
-async function winRateLists(req, res) {
-    let winRateLists = {
+async function winBetsLists(req, res) {
+    let winBetsLists = {
         NBA: [],
         MLB: [],
     }
 
     try {
-        for (const [key, value] of Object.entries(winRateLists)) {
-            const leagueWinRateLists = [];
+        for (const [key, value] of Object.entries(winBetsLists)) {
+            const leagueWinBetsLists = [];
             const results = [];
 
-            const leagueWinRateListsQuery = await modules.firestore.collection('users_win_lists')
-                .orderBy(`${key}_this_month_win_rate`, 'desc')
+            const leagueWinBetsListsQuery = await modules.firestore.collection('users_win_lists')
+                .orderBy(`${key}_this_month_win_bets`, 'desc')
                 .limit(5)
                 .get();
 
-            leagueWinRateListsQuery.forEach(function(data) {
+                leagueWinBetsListsQuery.forEach(function(data) {
                 results.push( data.data() );
             });
     
             results.forEach(async function(data) {
-                leagueWinRateLists.push( repackage(key, data) );
+                leagueWinBetsLists.push( repackage(key, data) );
             });
 
-            winRateLists[key] = leagueWinRateLists;
+            winBetsLists[key] = leagueWinBetsLists;
         } 
     }catch(err){
         console.log('Error in  home/godlists by YuHsien:  %o', err);
         return res.status(500);
     }
 
-    return res.status(200).json({win_rate_lists: win_rate_lists});
+    return res.status(200).json({win_bets_lists: winBetsLists});
 }
 
 function repackage(league, ele) {
     data = {
-        win_rate: '',
+        win_bets: '',
         uid: ele.uid,
         avatar: ele.avatar,
         displayname: ele.displayname,
         rank: ''
     };
 
-    data['win_rate'] = ele[`${league}_this_month_win_rate`];
+    data['win_bets'] = ele[`${league}_this_month_win_bets`];
     data['rank'] = ele[`${league}_rank`];
 
     return data;
 }
 
-module.exports = winRateLists;
+module.exports = winBetsLists;
 /**
- * @api {get} /winratelists Get Winrate Lists
+ * @api {get} /winbetslists Get Winbets Lists
  * @apiVersion 1.0.0
- * @apiName winratelists
+ * @apiName winbetslists
  * @apiGroup home
  * @apiPermission None
  *
- * @apiSuccess {JSON} result Available List of Winrate lists
+ * @apiSuccess {JSON} result Available List of Winbets lists
  *
  * @apiSuccessExample Success-Response:
  *  HTTP/1.1 200 OK
  {
-  "win_rate_lists": {
+  "win_bets_lists": {
     "NBA": [
       {
-        "win_rate": 94,
+        "win_bets": 65,
         "uid": "MyOPA8SzgVUq8iARhOa8mzQLC3e2",
         "avatar": "https://chat.doinfo.cc/statics/default-profile-avatar.jpg",
         "displayname": "紅色警報",
         "rank": "2"
       },
       {
-        "win_rate": 92,
+        "win_bets": 55,
         "uid": "BimYoqVdG3ONxyxZ4Vy855lXc9q2",
         "avatar": "https://chat.doinfo.cc/statics/default-profile-avatar.jpg",
         "displayname": "中哥大",
         "rank": "2"
       },
       {
-        "win_rate": 91,
-        "uid": "FZql5b9sOENEq373y4CiiHOXYe43",
-        "avatar": "https://chat.doinfo.cc/statics/default-profile-avatar.jpg",
-        "displayname": "台大科技",
-        "rank": "2"
-      },
-      {
-        "win_rate": 90,
+        "win_bets": 52,
         "uid": "6wrKIEue8MajxljlsSTujhLWNkm1",
         "avatar": "https://chat.doinfo.cc/statics/default-profile-avatar.jpg",
         "displayname": "台大科技",
         "rank": ""
       },
       {
-        "win_rate": 89,
+        "win_bets": 45,
         "uid": "6ls8rUgG2AQkjSmjh8sN0HoiM7v2",
         "avatar": "https://chat.doinfo.cc/statics/default-profile-avatar.jpg",
         "displayname": "運測2人",
         "rank": "3"
+      },
+      {
+        "win_bets": 44,
+        "uid": "B30GysZF0rMiIGGAA7FSyraf13D2",
+        "avatar": "https://chat.doinfo.cc/statics/default-profile-avatar.jpg",
+        "displayname": "大小人",
+        "rank": "2"
       }
     ],
     "MLB": [
       {
-        "win_rate": 88,
-        "uid": "B30GysZF0rMiIGGAA7FSyraf13D2",
+        "win_bets": 51,
+        "uid": "MyOPA8SzgVUq8iARhOa8mzQLC3e2",
         "avatar": "https://chat.doinfo.cc/statics/default-profile-avatar.jpg",
-        "displayname": "大小人",
+        "displayname": "紅色警報",
         "rank": ""
       },
       {
-        "win_rate": 78,
-        "uid": "Xw4dOKa4mWh3Kvlx35mPtAOX2P52",
+        "win_bets": 50,
+        "uid": "HppFr8j4sUVSQFKaiTGKjGZmQhw2",
         "avatar": "https://chat.doinfo.cc/statics/default-profile-avatar.jpg",
-        "displayname": "無19",
+        "displayname": "1D7",
         "rank": ""
       },
       {
-        "win_rate": 78,
-        "uid": "FXzqjqFlRygUYu7S20XMqsFNxom1",
+        "win_bets": 49,
+        "uid": "GfSp086CNOd43W89avovuRfsVQn1",
         "avatar": "https://chat.doinfo.cc/statics/default-profile-avatar.jpg",
-        "displayname": "工儘2",
+        "displayname": "16大學",
         "rank": ""
       },
       {
-        "win_rate": 78,
-        "uid": "6ls8rUgG2AQkjSmjh8sN0HoiM7v2",
+        "win_bets": 48,
+        "uid": "Fu5ygPPjlnaFxB1dsIMDttBHuai1",
         "avatar": "https://chat.doinfo.cc/statics/default-profile-avatar.jpg",
-        "displayname": "運測2人",
-        "rank": "2"
+        "displayname": "森森",
+        "rank": ""
       },
       {
-        "win_rate": 78,
-        "uid": "5kICOSoGJjWSExJx1vL56if0p1G2",
+        "win_bets": 47,
+        "uid": "FdqFph3P2QQ0jymNyENHlJcJVfg1",
         "avatar": "https://chat.doinfo.cc/statics/default-profile-avatar.jpg",
-        "displayname": "珊迪",
-        "rank": "3"
+        "displayname": "超大工具大",
+        "rank": ""
       }
     ]
   }
--- functions/routers/home.js
+++ functions/routers/home.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const verification = require('../util/verification');
 
 router.get('/godlists', require('../controller/home/godListsController') );
-router.get('/winratelists', require('../controller/home/winrateListsController') );
-router.get('/profitlists', require('../controller/home/profitListsController') );
+router.get('/winratelists', require('../controller/home/winRateListsController') );
+router.get('/winbetslists', require('../controller/home/winBetsListsController') );
 
 module.exports = router;
