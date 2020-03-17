43f241ad237ccee5e5b57c8967e927706e4e9a6e
ifyu666 <60864122+ifyu666@users.noreply.github.com>
2020-03-17 16:57:39 +0800

    test hotTopics 3

--- functions/controller/home/hotTopicsController.js
+++ functions/controller/home/hotTopicsController.js
@@ -6,26 +6,20 @@ async function hotTopics(req, res) {
   const topics = [];
   try {
     const query = await modules.firestore.collection('topics')
-      // .where('sell', '==', '1')
-      .orderBy('ranking.viewCount', 'desc')
-      .get();
-    console.log(query)
-
-    const sortedArr = query.docs.map(function(doc) {  // 轉換成array
-      return doc.data()
+    // .where('foo', '==', 'bar')
+    .orderBy('ranking.viewCount', 'desc')
+    .limit(2)
+    .get().then(querySnapshot => {
+      querySnapshot.forEach(doc => {
+        topics.push(doc.data())
+      });
     });
-
-    sortedArr.forEach(async function(data) {
-      topics.push(data);
-    });
-
-    await Promise.all(topics);
   }catch(err){
     console.log('Error in home/hotTopics by IFYU:  %o', err);
     return res.status(500);
   }
 
-  return res.status(200).json({test: topics});
+  return res.status(200).json({topics: topics});
 }
 
 module.exports = hotTopics;
