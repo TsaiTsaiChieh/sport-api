83139fa3a859ecb5a2cc64d7f0ada8f50eb0c39f
蔡采潔 <d0177381@gmail.com>
2020-03-25 23:30:56 +0800

    change save format and refactor(not finish)

--- functions/model/admin/giveTitleModel.js
+++ functions/model/admin/giveTitleModel.js
@@ -2,43 +2,20 @@ const modules = require('../../util/modules');
 
 function giveTitleModel(args) {
   return new Promise(async function(resolve, reject) {
-    /* step 1: check if user exists */
     try {
-      const userDoc = await modules.getDoc('users', args.uid);
-      const userSnapshot = await userDoc.get();
-      if (!userSnapshot.exists) {
-        reject({ code: 404, error: 'user not found' });
-        return;
-      }
-      const user = userSnapshot.data();
-      let titles = user.titles ? user.titles : [];
+      // check if user exists
+      const user = await getUserDoc(args.uid);
+      // check user is admin
+      await isAdmin(user.status);
+      // get from user_titles collection
+      const periodObj = modules.getTitlesPeriod(new Date());
+      const titles = await getTitles(args.uid, periodObj.period);
+      const result = await insertTitles(args, titles, periodObj);
+      console.log(result);
 
-      /* step 2: check user is admin */
-      if (user.status === 9) {
-        reject({ code: 403, error: 'forbidden, admin could not have a title' });
-        return;
-      }
       /* step 3: insert if titles.length === 0 */
       if (titles.length === 0) {
-        titles.push({
-          rank: args.rank,
-          league: args.league,
-          sport: args.sport
-        });
-
-        // set default title
-        userDoc.set(
-          { titles, defaultTitle: titles[0], status: 2 },
-          { merge: true }
-        );
-        modules.firebaseAdmin.auth().setCustomUserClaims(args.uid, { role: 2 });
-        console.log(
-          `Add title ${args.league}_${args.sport}_${args.rank} by admin uid: ${
-            args.adminUid
-          } on ${new Date()} by TsaiChieh`
-        );
-
-        resolve({
+        return resolve({
           uid: args.uid,
           title: [
             {
@@ -48,7 +25,6 @@ function giveTitleModel(args) {
             }
           ]
         });
-        return;
       }
 
       let duplicatedFlag = false;
@@ -82,11 +58,10 @@ function giveTitleModel(args) {
         }
       }
       if (duplicatedFlag) {
-        reject({
+        return reject({
           code: 403,
           error: 'forbidden, this user had the same title'
         });
-        return;
       } else if (!updateFlag) {
         titles.push({
           rank: args.rank,
@@ -95,7 +70,11 @@ function giveTitleModel(args) {
         });
       }
       // status 2 is god like
-      userDoc.set({ titles, status: 2 }, { merge: true });
+      // userDoc.set({ titles, status: 2 }, { merge: true });
+      modules.addDataInCollectionWithId('users', args.uid, {
+        titles,
+        status: 2
+      });
       modules.firebaseAdmin.auth().setCustomUserClaims(args.uid, { role: 2 });
       resolve({
         uid: args.uid,
@@ -115,6 +94,78 @@ function giveTitleModel(args) {
     } catch (err) {
       console.log('error happened...', err);
       reject({ code: 500, error: err });
+    }
+  });
+}
+
+function getUserDoc(uid) {
+  return new Promise(async function(resolve, reject) {
+    const userSnapshot = await modules.getSnapshot('users', uid);
+    if (!userSnapshot.exists) {
+      return reject({ code: 404, error: 'user not found' });
+    }
+    if (userSnapshot.exists) {
+      return resolve(userSnapshot.data());
+    }
+  });
+}
+
+function isAdmin(status) {
+  return new Promise(function(resolve, reject) {
+    if (status === 9)
+      return reject({
+        code: 403,
+        error: 'forbidden, admin could not have a title'
+      });
+    return resolve(true);
+  });
+}
+
+function getTitles(uid, period) {
+  return new Promise(async function(resolve, reject) {
+    try {
+      const titlesSnapshot = await modules.getSnapshot('users_titles', uid);
+      if (!titlesSnapshot.exists) return resolve([]);
+      if (titlesSnapshot.exists) {
+        const data = titlesSnapshot.data();
+        if (data[`${period}_period`]) {
+          return resolve(data[`${period}_period`].titles);
+        }
+        if (!data[`${period}_period`]) {
+          return resolve([]);
+        }
+      }
+    } catch (err) {
+      console.error(
+        'Error in model/admin/giveTitleModel getTitles function by TsaiChieh',
+        err
+      );
+      return reject({ code: 500, error: err });
+    }
+  });
+}
+
+function insertTitles(args, titles, periodObj) {
+  return new Promise(async function(resolve, reject) {
+    // insert if titles.length === 0
+    if (!titles.length) {
+      const title = { rank: args.rank, league: args.league, sport: args.sport };
+      const data = { uid: args.uid };
+      data[`${periodObj.period}_period`] = {
+        date: periodObj.date,
+        titles: [title]
+      };
+      modules.addDataInCollectionWithId('users', args.uid, {
+        defaultTitle: title,
+        status: 2
+      });
+      modules.addDataInCollectionWithId('users_titles', args.uid, data);
+      modules.firebaseAdmin.auth().setCustomUserClaims(args.uid, { role: 2 });
+      return resolve(
+        `Add title ${args.league}_${args.sport}_${args.rank} by admin uid: ${
+          args.adminUid
+        } on ${new Date()}`
+      );
     }
   });
 }
