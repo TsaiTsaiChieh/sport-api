eb44dfd0b81d5f5ed429b3367e6f4f14af009dc4
蔡采潔 <d0177381@gmail.com>
2020-03-26 14:27:51 +0800

    add title record and also update users collection title data

--- functions/model/admin/giveTitleModel.js
+++ functions/model/admin/giveTitleModel.js
@@ -65,13 +65,8 @@ function insertTitles(args, titles, periodObj) {
   return new Promise(async function(resolve, reject) {
     // insert if titles.length === 0
     if (!titles.length) {
-      modules.addDataInCollectionWithId('users', args.uid, {
-        defaultTitle: {
-          rank: args.rank,
-          league: args.league,
-          sport: args.sport
-        }
-      });
+      // insert default title
+      insertDefaultTitle(args, periodObj.period);
       insertFirestore(args, titles, periodObj);
     }
     // if already have titles, should check duplication
@@ -84,40 +79,94 @@ function insertTitles(args, titles, periodObj) {
       }
       if (!checkTitleDuplication(titles, args)) {
         updateResult = checkUpdateTitle(titles, args);
-        insertFirestore(
-          args,
-          updateResult.titles,
-          periodObj,
-          updateResult.updateFlag
-        );
+        insertDefaultTitle(args, periodObj.period);
+        insertFirestore(args, updateResult.titles, periodObj, updateResult);
       }
     }
     return resolve(
       `Add title ${args.league}_${args.sport}_${args.rank} by admin uid: ${
         args.adminUid
-      } on ${new Date()}`
+      } on ${modules.moment().format('MMMM Do YYYY, h:mm:ss a')}`
     );
   });
 }
-
-function insertFirestore(args, titles, periodObj, updateFlag = false) {
-  if (!updateFlag)
+function insertDefaultTitle(args, period) {
+  const data = {};
+  defaultTitle = {
+    rank: args.rank,
+    league: args.league,
+    sport: args.sport
+  };
+  modules.addDataInCollectionWithId('users', args.uid, {
+    defaultTitle
+  });
+  data[`${period}_period`] = {
+    default_title: defaultTitle
+  };
+  modules.addDataInCollectionWithId('users_titles', args.uid, data);
+}
+async function insertFirestore(args, titles, periodObj, updateResult = {}) {
+  const GOD_STATUS = 2;
+  if (!updateResult.updateFlag)
     titles.push({
       rank: args.rank,
       league: args.league,
       sport: args.sport
     });
-  const data = { uid: args.uid };
+  const titlesRecord = await getTitlesRecord(args);
+  record = updateTitleCount(titlesRecord, args.rank, updateResult);
+  const data = {
+    uid: args.uid,
+    rank1_count: record.rank1_count,
+    rank2_count: record.rank2_count,
+    rank3_count: record.rank3_count,
+    rank4_count: record.rank4_count
+  };
   data[`${periodObj.period}_period`] = {
     date: periodObj.date,
     titles
   };
+
   modules.addDataInCollectionWithId('users', args.uid, {
-    status: 2,
+    status: GOD_STATUS,
     titles
   });
   modules.addDataInCollectionWithId('users_titles', args.uid, data);
   modules.firebaseAdmin.auth().setCustomUserClaims(args.uid, { role: 2 });
+}
+async function getTitlesRecord(args) {
+  const titlesRecord = await modules.getSnapshot('users_titles', args.uid);
+  if (!titlesRecord.exists) {
+    return {
+      rank1_count: 0,
+      rank2_count: 0,
+      rank3_count: 0,
+      rank4_count: 0
+    };
+  }
+  if (titlesRecord.exists) {
+    const {
+      rank1_count,
+      rank2_count,
+      rank3_count,
+      rank4_count
+    } = titlesRecord.data();
+    return { rank1_count, rank2_count, rank3_count, rank4_count };
+  }
+}
+
+function updateTitleCount(record, rank, updateResult) {
+  if (updateResult.updateFlag) {
+    if (updateResult.oldRank === 1) record.rank1_count -= 1;
+    if (updateResult.oldRank === 2) record.rank2_count -= 1;
+    if (updateResult.oldRank === 3) record.rank3_count -= 1;
+    if (updateResult.oldRank === 4) record.rank4_count -= 1;
+  }
+  if (rank === 1) record.rank1_count += 1;
+  if (rank === 2) record.rank2_count += 1;
+  if (rank === 3) record.rank3_count += 1;
+  if (rank === 4) record.rank4_count += 1;
+  return record;
 }
 function checkTitleDuplication(titles, args) {
   // check if titles duplication
@@ -138,18 +187,19 @@ function checkTitleDuplication(titles, args) {
 
 function checkUpdateTitle(titles, args) {
   let updateFlag = false;
+  let oldRank = 0;
   for (let i = 0; i < titles.length; i++) {
     const ele = titles[i];
     if (
       ele.sport === args.sport &&
       ele.league === args.league &&
-      ele.rank !== 1 &&
-      args.rank !== 1
+      ele.rank !== args.rank
     ) {
+      oldRank = ele.rank;
       ele.rank = args.rank;
       updateFlag = true;
     }
   }
-  return { updateFlag, titles };
+  return { updateFlag, titles, oldRank };
 }
 module.exports = giveTitleModel;
