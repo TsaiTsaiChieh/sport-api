d223766386173076fa782eba27d3866b96400454
Alan <alan@Alans-MacBook-Pro.local>
2020-03-30 16:44:06 +0800

    預測單新架構分聯盟陣列給前端- 未完成，大架構ok

--- functions/model/user/predictInfoModel.js
+++ functions/model/user/predictInfoModel.js
@@ -13,6 +13,7 @@ function predictInfo(args) {
     const league = args.league;
 
     let predictionsInfoList = [];
+    let response = {};
 
     // 1.  
     try{
@@ -39,15 +40,17 @@ function predictInfo(args) {
     // 2.
     try{
       const now_YYYYMMDD = modules.moment().utcOffset(8).format('YYYYMMDD'); // 今天 年月日
+      const tomorrow_YYYYMMDD = modules.moment().add(1, 'days').utcOffset(8).format('YYYYMMDD'); // 今天 年月日
       const now = modules.moment(now_YYYYMMDD).unix() * 1000;
       const tomorrow = modules.moment(now_YYYYMMDD).add(2, 'days').unix() * 1000;
 
       // 使用者預測資訊
-      const predictionsInfoDocs = await modules.firestore.collection(`prediction_${league}`)
+      const predictionsInfoDocs = await modules.firestore.collection(`prediction_${league}_TC`)
         .where('uid', '==', userUid)
-        .where('date_timestamp', '>=', now)
-        .where('date_timestamp', '<', tomorrow) // 兩天內
-        //.where('date', 'in', [now, tomorrow]) // 兩天內
+        //.where('date_timestamp', '>=', now)
+        //.where('date_timestamp', '<', tomorrow) // 兩天內
+        .where('date', 'in', [tomorrow_YYYYMMDD, tomorrow_YYYYMMDD, '20200401']) // 兩天內
+        .where('scheduled', '>', now) // 賽前 (scheduled 開賽時間 > api呼叫時間)
         .get();
 
       // 使用者 一開始尚未預測
@@ -63,49 +66,63 @@ function predictInfo(args) {
       // }
     
       let predictonsInfoData = []; // 使用者預測資訊
-      let matchInfoDocs = [];
+      // let matchInfoDocs = [];
 
       predictionsInfoDocs.forEach(function(data){
         predictonsInfoData.push(data.data());
       });
 
-      // 查詢 matches 賽事相關資料
-      predictonsInfoData.forEach(function(data) {
-        for (const [key, value] of Object.entries(data.matches)) { 
-          const matchInfoDoc = modules.firestore.collection(collectionCodebook(league))
-            .where('flag.status', '==', 2) // 賽前
-            .where('bets_id', '==', key)
-            .get();
-          matchInfoDocs.push(matchInfoDoc);
-        }
-      });
+      // // 查詢 matches 賽事相關資料
+      // predictonsInfoData.forEach(function(data) {
+      //   for (const [key, value] of Object.entries(data.matches)) { 
+      //     const matchInfoDoc = modules.firestore.collection(collectionCodebook(league))
+      //       .where('flag.status', '==', 2) // 賽前
+      //       .where('bets_id', '==', key)
+      //       .get();
+      //     matchInfoDocs.push(matchInfoDoc);
+      //   }
+      // });
 
-      // 將取回賽事資料進行整理，給repackage使用，屬於額外資訊
-      const matchesInfos = await Promise.all(matchInfoDocs).then(function(docs) { 
-        const temp = {};
-        docs.forEach(function(doc) {
-          doc.forEach(function(ele){
-            temp[ele.data().bets_id] = ele.data(); // ES6 才能使用[variable key]
-          });
-        });
-        return temp;
-      });
+      // // 將取回賽事資料進行整理，給repackage使用，屬於額外資訊
+      // const matchesInfos = await Promise.all(matchInfoDocs).then(function(docs) { 
+      //   const temp = {};
+      //   docs.forEach(function(doc) {
+      //     doc.forEach(function(ele){
+      //       temp[ele.data().bets_id] = ele.data(); // ES6 才能使用[variable key]
+      //     });
+      //   });
+      //   return temp;
+      // });
 
       // 把賽事資料 重包裝格式
-      predictonsInfoData.forEach(function(data) {
-        for (const [key, value] of Object.entries(data.matches)) {
-          if (matchesInfos[key] === undefined) continue // 非賽前 (已開打、結束等) 不用處理打包
-          predictionsInfoList.push( 
-            repackage(value, {league: league, matchInfo: matchesInfos[key]} ) 
+      groupBy(predictonsInfoData, 'league').forEach(function(data) { // 分聯盟陣列
+        let league = '';
+        data.forEach(function(ele) { // 取出 聯盟陣列中的賽事
+          predictionsInfoList.push(
+            repackage(ele) 
           );
-        }
+          league = ele.league;
+        });
+        response[league] = predictionsInfoList;
+        predictionsInfoList = [];
+        console.log(response)
       });
+
+      // predictonsInfoData.forEach(function(data) {
+      //   predictionsInfoList.push(repackage(data));
+      //   for (const [key, value] of Object.entries(data.matches)) {
+      //     if (matchesInfos[key] === undefined) continue // 非賽前 (已開打、結束等) 不用處理打包
+      //     predictionsInfoList.push( 
+      //       repackage(value, {league: league, matchInfo: matchesInfos[key]} ) 
+      //     );
+      //   }
+      // });
     } catch (err) {
       console.error('Error 2. in user/predictonInfoModell by YuHsien', err);
       return reject(errs.errsMsg('500', '500', err));
     }
 
-    resolve(predictionsInfoList);
+    resolve(response);
     return;
   });
 }
@@ -130,38 +147,79 @@ function collectionCodebook(league) {
   }
 }
 
-function repackage(value, addInfo) {
+function groupBy(arr, prop) {
+  const map = new Map(Array.from(arr, obj => [obj[prop], []]));
+  arr.forEach(obj => map.get(obj[prop]).push(obj));
+  return Array.from(map.values());
+}
+
+function repackage(ele){
   let data = {
-    bets_id: addInfo.matchInfo.bets_id,
-    scheduled: addInfo.matchInfo.scheduled._seconds, // 開賽時間
-    league: addInfo.league,
-    home: addInfo.matchInfo.home.alias_ch,
-    away: addInfo.matchInfo.away.alias_ch,
+    bets_id: ele.bets_id,
+    scheduled: ele.scheduled, // 開賽時間
+    league: ele.league,
+    home: ele.home.alias,
+    home_ch: ele.home.alias_ch,
+    away: ele.alias,
+    away_ch: ele.alias_ch,
     spread: {},
     totals: {}
   };
-
-  if( !(value.spread === undefined) && Object.keys(value.spread).length > 0) { // 有讓分資料
+  if( !(ele.spread === undefined) && Object.keys(ele.spread).length > 0) { // 有讓分資料
     data['spread'] = {
-      predict: value.spread.predict,
-      handicap_id: value.spread.handicap_id, // 盤口id
-      handicap: value.spread.handicap,
+      predict: ele.spread.predict,
+      handicap_id: ele.spread.handicap_id,
+      handicap: ele.spread.handicap,
       percentage: Math.floor(Math.random()*50), // 目前先使用隨機數，將來有決定怎麼產生資料時，再處理
-      bets: value.spread.bets
+      bets: ele.spread.bets
     }
   }
 
-  if( !(value.totals === undefined) && Object.keys(value.totals).length > 0) { // 有大小資料
-    data['totals'] = {
-      predict: value.totals.predict,
-      handicap_id: value.totals.handicap_id,
-      handicap: value.totals.handicap,
+  if( !(ele.totals === undefined) && Object.keys(ele.totals).length > 0) { // 有大小資料
+    data['spread'] = {
+      predict: ele.totals.predict,
+      handicap_id: ele.totals.handicap_id,
+      handicap: ele.totals.handicap,
       percentage: Math.floor(Math.random()*50), // 目前先使用隨機數，將來有決定怎麼產生資料時，再處理
-      bets: value.totals.bets
+      bets: ele.totals.bets
     }
   }
 
   return data;
 }
+
+// function repackage(value, addInfo) {
+//   let data = {
+//     bets_id: addInfo.matchInfo.bets_id,
+//     scheduled: addInfo.matchInfo.scheduled._seconds, // 開賽時間
+//     league: addInfo.league,
+//     home: addInfo.matchInfo.home.alias_ch,
+//     away: addInfo.matchInfo.away.alias_ch,
+//     spread: {},
+//     totals: {}
+//   };
+
+//   if( !(value.spread === undefined) && Object.keys(value.spread).length > 0) { // 有讓分資料
+//     data['spread'] = {
+//       predict: value.spread.predict,
+//       handicap_id: value.spread.handicap_id, // 盤口id
+//       handicap: value.spread.handicap,
+//       percentage: Math.floor(Math.random()*50), // 目前先使用隨機數，將來有決定怎麼產生資料時，再處理
+//       bets: value.spread.bets
+//     }
+//   }
+
+//   if( !(value.totals === undefined) && Object.keys(value.totals).length > 0) { // 有大小資料
+//     data['totals'] = {
+//       predict: value.totals.predict,
+//       handicap_id: value.totals.handicap_id,
+//       handicap: value.totals.handicap,
+//       percentage: Math.floor(Math.random()*50), // 目前先使用隨機數，將來有決定怎麼產生資料時，再處理
+//       bets: value.totals.bets
+//     }
+//   }
+
+//   return data;
+// }
 
 module.exports = predictInfo;
