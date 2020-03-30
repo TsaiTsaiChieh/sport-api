e8e3dcf8c02595754c7bdebb644421e7a0ff5fb2
Alan <alan@Alans-MacBook-Pro.local>
2020-03-30 14:15:42 +0800

    多一個判斷是錯誤session測試

--- functions/test/user.test.js
+++ functions/test/user.test.js
@@ -9,9 +9,19 @@ const apidosports = 'https://api-dosports.web.app';
 const url = localhost // 依照需求自行切換測試網址
 
 // 可以自行替換登入後 cookie session 值
-const __session = '__session=eyJhbGciOiJSUzI1NiIsImtpZCI6IjdUWDJldyJ9.eyJpc3MiOiJodHRwczovL3Nlc3Npb24uZmlyZWJhc2UuZ29vZ2xlLmNvbS9zcG9ydHNsb3R0ZXJ5LXRlc3QiLCJuYW1lIjoibGVtb24iLCJwaWN0dXJlIjoiaHR0cHM6Ly9maXJlYmFzZXN0b3JhZ2UuZ29vZ2xlYXBpcy5jb20vdjAvYi9zcG9ydDE5eTA3MTUuYXBwc3BvdC5jb20vby9kZWZhdWx0JTJGYXZhdGFyJTJGZGVmYXVsdC1wcm9maWxlLWF2YXRhci5qcGc_YWx0XHUwMDNkbWVkaWFcdTAwMjZ0b2tlblx1MDAzZDc3NTMzODVmLTU0NTctNGZlMi1hZjhlLWFjZWY3NWZjY2NkOCIsInJvbGUiOjEsImF1ZCI6InNwb3J0c2xvdHRlcnktdGVzdCIsImF1dGhfdGltZSI6MTU4NTEyNjExNywidXNlcl9pZCI6IkRMUm5kNWlnUm1ha0MwVnJMeHo1UGg0NDNRajEiLCJzdWIiOiJETFJuZDVpZ1JtYWtDMFZyTHh6NVBoNDQzUWoxIiwiaWF0IjoxNTg1MTI5ODA5LCJleHAiOjE1ODU3MzQ2MDksImVtYWlsIjoia2VybzEzQGxpdmVtYWlsLnR3IiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7ImVtYWlsIjpbImtlcm8xM0BsaXZlbWFpbC50dyJdfSwic2lnbl9pbl9wcm92aWRlciI6InBhc3N3b3JkIn19.DeGc5u2yhDsVF6cS-9eFtWr4a9N294Qe7wLtMkrofpweRfplFGOc0nfcwrDfWFS1NbFZwHP10ao0atZfj6HaaVh61VO0WiMOszNCvcRPr2LTAxpFr-MU1Un-0svsv7ONVIrM_uXDRN6vBw3amc5tPy4PUaxe8g2Pq6iDLcb5GIC45u7xO2vwir-TzTW09bW7FuTRZHDprauajc5AF_nLm6LvUSdEVhqnVGGxPggjkRd6gaDx6A4vvCwjhLZu_uVeAWcQ_eFieYfsvokPno1trdZPLCPTmQw-rWTbc4dPOxkS5gpvAPo8f8Xug7SlFAy_o2fT446MTnKP3YXARL9WTQ';
+const __session = '__session=eyJhbGciOiJSUzI1NiIsImtpZCI6IjdUWDJldyJ9.eyJpc3MiOiJodHRwczovL3Nlc3Npb24uZmlyZWJhc2UuZ29vZ2xlLmNvbS9zcG9ydHNsb3R0ZXJ5LXRlc3QiLCJhdWQiOiJzcG9ydHNsb3R0ZXJ5LXRlc3QiLCJhdXRoX3RpbWUiOjE1ODU1NDIzMDAsInVzZXJfaWQiOiIyV01SZ0h5VXd2VEx5SHBMb0FOazdnV0FEWm4xIiwic3ViIjoiMldNUmdIeVV3dlRMeUhwTG9BTms3Z1dBRFpuMSIsImlhdCI6MTU4NTU0MjMxNCwiZXhwIjoxNTg2MTQ3MTE0LCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7fSwic2lnbl9pbl9wcm92aWRlciI6ImN1c3RvbSJ9fQ.Z_FItdDe2X7oyX9hllx7RB0ERo-KwlnIpkADosSSdQQvBGMkNlttL3IphldDpOZmml65UUb66G8pwiaUpsa8Vg1en33l2LToKZPkdt-Z3NSbQFlU-G025jyJSg-zWq9NiV0j-1Hh59cXobhQRmqRfz4c5VpVKv3GyWYLY-gEV_oP5Ek6aJFIPMOFZBpX9Hw_YGKAE4p5xymHiFfkwcmlCsGH_C5QRWrC5MPgWU8StMXxaq9Z6cKq6uAwOqgJwk_GkawVHnBNdd-LBX-VtrCbeFuIa56uitWzlpiifVaEk9AB_UH82xTteocCXVsBd2f08xJNA2GoCsiOjlJvhu-MUA';
+const __badsession = '__session=eyJhbGciOiJSUzI1NiIsImtpZCI6IjdUWDJldyJ9.eyJpc3Mi';
 
 describe('/user Endpoints', () => {
+  it('/user/predict_info 不正確登入session __badsession', async () => {
+    const res = await request(url)
+      .post('/user/predict_info')
+      .send({league: 'NBA'})
+      .set('Cookie', __badsession);
+
+    expect(res.statusCode).toEqual(401);
+  });
+
   it('/user/predict_info 正確參數 NBA', async () => {
     const res = await request(url)
       .post('/user/predict_info')
