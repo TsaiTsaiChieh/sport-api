66986168b2dfcbc68c0c406ec299030d467dba61
蔡采潔 <d0177381@gmail.com>
2020-04-13 09:38:24 +0800

    [add] scheduled_tw field

--- functions/pubsub/mysql/createTables.js
+++ functions/pubsub/mysql/createTables.js
@@ -7,70 +7,70 @@ const data_totals_NBA = require('../../json/totals/NBA.json');
 const data_match_NBA = require('../../json/matches/NBA.json');
 async function create(req, res) {
   try {
-    const League = await db.sequelize.models.match__league.sync({
-      force: true
-    });
-    for (let i = 0; i < data_league.length; i++) {
-      const ele = data_league[i];
-      League.create({
-        league_id: ele.league_id,
-        radar_id: ele.radar_id,
-        sport: ele.sport,
-        name: ele.name,
-        name_ch: ele.name_ch
-      });
-    }
-    const Spread = await db.sequelize.models.match__spread.sync({
-      force: true
-    });
-    for (let i = 0; i < data_spread_NBA.length; i++) {
-      const ele = data_spread_NBA[i];
+    // const League = await db.sequelize.models.match__league.sync({
+    //   force: true
+    // });
+    // for (let i = 0; i < data_league.length; i++) {
+    //   const ele = data_league[i];
+    //   League.create({
+    //     league_id: ele.league_id,
+    //     radar_id: ele.radar_id,
+    //     sport: ele.sport,
+    //     name: ele.name,
+    //     name_ch: ele.name_ch
+    //   });
+    // }
+    // const Spread = await db.sequelize.models.match__spread.sync({
+    //   force: true
+    // });
+    // for (let i = 0; i < data_spread_NBA.length; i++) {
+    //   const ele = data_spread_NBA[i];
 
-      const data = {
-        spread_id: ele.spread_id,
-        match_id: ele.match_id,
-        handicap: ele.handicap,
-        add_time: ele.add_time,
-        home_odd: ele.home_odd,
-        away_odd: ele.away_odd,
-        league_id: ele.league_id
-      };
-      if (ele.home_tw) data.home_tw = ele.home_tw;
-      if (ele.away_tw) data.away_tw = ele.away_tw;
-      Spread.create(data);
-    }
-    const Totals = await db.sequelize.models.match__total.sync({ force: true });
-    for (let i = 0; i < data_totals_NBA.length; i++) {
-      const ele = data_totals_NBA[i];
-      const data = {
-        totals_id: ele.totals_id,
-        match_id: ele.match_id,
-        handicap: ele.handicap,
-        add_time: ele.add_time,
-        over_odd: ele.over_odd,
-        under_odd: ele.under_odd,
-        league_id: ele.league_id,
-        over_tw: ele.over_tw
-      };
+    //   const data = {
+    //     spread_id: ele.spread_id,
+    //     match_id: ele.match_id,
+    //     handicap: ele.handicap,
+    //     add_time: ele.add_time,
+    //     home_odd: ele.home_odd,
+    //     away_odd: ele.away_odd,
+    //     league_id: ele.league_id
+    //   };
+    //   if (ele.home_tw) data.home_tw = ele.home_tw;
+    //   if (ele.away_tw) data.away_tw = ele.away_tw;
+    //   Spread.create(data);
+    // }
+    // const Totals = await db.sequelize.models.match__total.sync({ force: true });
+    // for (let i = 0; i < data_totals_NBA.length; i++) {
+    //   const ele = data_totals_NBA[i];
+    //   const data = {
+    //     totals_id: ele.totals_id,
+    //     match_id: ele.match_id,
+    //     handicap: ele.handicap,
+    //     add_time: ele.add_time,
+    //     over_odd: ele.over_odd,
+    //     under_odd: ele.under_odd,
+    //     league_id: ele.league_id,
+    //     over_tw: ele.over_tw
+    //   };
 
-      Totals.create(data);
-    }
+    //   Totals.create(data);
+    // }
 
-    const team_NBA = await db.sequelize.models.match__team__NBA.sync({
-      force: true
-    });
-    for (let i = 0; i < data_team_NBA.length; i++) {
-      const ele = data_team_NBA[i];
-      team_NBA.create({
-        team_id: ele.team_id,
-        radar_id: ele.radar_id,
-        image_id: ele.image_id,
-        alias: ele.alias,
-        alias_ch: ele.alias_ch,
-        name: ele.name,
-        name_ch: ele.name_ch
-      });
-    }
+    // const team_NBA = await db.sequelize.models.match__team__NBA.sync({
+    //   force: true
+    // });
+    // for (let i = 0; i < data_team_NBA.length; i++) {
+    //   const ele = data_team_NBA[i];
+    //   team_NBA.create({
+    //     team_id: ele.team_id,
+    //     radar_id: ele.radar_id,
+    //     image_id: ele.image_id,
+    //     alias: ele.alias,
+    //     alias_ch: ele.alias_ch,
+    //     name: ele.name,
+    //     name_ch: ele.name_ch
+    //   });
+    // }
     const Match = await db.sequelize.models.match__NBA.sync({ force: true });
     for (let i = 0; i < data_match_NBA.length; i++) {
       const ele = data_match_NBA[i];
@@ -83,6 +83,7 @@ async function create(req, res) {
         totals_id: ele.totals_id,
         sr_id: ele.sr_id,
         scheduled: ele.scheduled,
+        scheduled_tw: ele.scheduled * 1000,
         flag_prematch: ele.flag_prematch,
         status: ele.status
       };
