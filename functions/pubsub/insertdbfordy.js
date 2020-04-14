5bd95a72d55e6c5c7dae588697ffd9268d0127a3
page40316 <page@gets-info.com>
2020-04-14 19:02:26 +0800

    livescore調整完成

--- functions/pubsub/insertdbfordy.js
+++ functions/pubsub/insertdbfordy.js
@@ -1,19 +1,11 @@
 const modules = require('../util/modules');
 const transNBA = require('./translateNBA.js');
 const translateNBA = transNBA.translateNBA;
+let firestoreName = 'pagetest_NBA';
 async function inserttest() {
-  let realtimeData;
-  realtimeData = JSON.parse(
-    JSON.stringify(
-      // eslint-disable-next-line no-await-in-loop
-      await modules.database
-        .ref(`baseball/MLB/20200501/Summary/info`)
-        .once('value')
-    )
-  );
-  let homeData = realtimeData.home;
-  let awayData = realtimeData.away;
-
-  console.log(homeData.roster[Object.keys(homeData.roster)[0]].name);
+  modules.firestore
+    .collection(firestoreName)
+    .doc(betsID)
+    .set({ away: {} }, { merge: true });
 }
 inserttest();
