04078e8419f2e3343bb3d6af67b69ee42da7eeb1
蔡采潔 <d0177381@gmail.com>
2020-04-14 17:10:57 +0800

    [add] two test about on-line API

--- functions/test/sport/prematch.test.js
+++ functions/test/sport/prematch.test.js
@@ -1,4 +1,5 @@
 const URL = 'localhost:5000';
+const onlineURL = 'https://api-dosports.web.app';
 const request = require('supertest');
 const OLD_API = '/sport/prematch?league=NBA&date=2020-07-01';
 const NEW_API = '/sport/matches?league=NBA&date=2020-07-01';
@@ -10,6 +11,14 @@ describe('GET /sport/prematch', function () {
   });
   test('(2) Query matches when user not login with newest API', async function () {
     const res = await request(URL).get(NEW_API);
+    expect(res.statusCode).toEqual(200);
+  });
+  test('(3) Query matches when user not login with older on-line API', async function () {
+    const res = await request(onlineURL).get(OLD_API);
+    expect(res.statusCode).toEqual(200);
+  });
+  test('(4) Query matches when user not login with newest on-line  API', async function () {
+    const res = await request(onlineURL).get(NEW_API);
     expect(res.statusCode).toEqual(200);
   });
 });
