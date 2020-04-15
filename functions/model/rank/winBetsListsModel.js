e90f76d5d746968105e0db4a19efe39a0676796f
Alan <alan@Alans-MacBook-Pro.local>
2020-04-15 18:25:32 +0800

    補上 預設戰績名稱 未完成

--- functions/model/rank/winBetsListsModel.js
+++ functions/model/rank/winBetsListsModel.js
@@ -17,13 +17,17 @@ function winBetsLists(args) {
         // 依 聯盟 取出是 大神資料 且 有販售
         // 將來有排序條件，可以orderBy，但會和下面的order衝突
         const leagueWinBetsListsQuery = await db.sequelize.query(`
-          select winlist.uid, users.avatar, users.display_name, winlist.rank_id, 
+          select winlist.uid, users.avatar, users.display_name, 
                  winlist.last_month_win_bets, winlist.last_month_win_rate, 
                  winlist.last_week_win_bets, winlist.last_week_win_rate,
                  winlist.this_season_win_bets, winlist.this_season_win_rate,
                  winlist.this_period_win_bets, winlist.this_period_win_rate,
                  winlist.this_month_win_bets, winlist.this_month_win_rate,
-                 winlist.this_week_win_bets, winlist.this_week_win_rate
+                 winlist.this_week_win_bets, winlist.this_week_win_rate,
+                 titles.rank_id, titles.default_title,
+                 titles.continune, 
+                 titles.predict_rate1, titles.predict_rate2, titles.predict_rate3, titles.win_bets_continue,
+                 titles.matches_rate1, titles.matches_rate2, titles.matches_continue
             from users__win__lists winlist,
                  ( 
                    select league_id 
@@ -33,10 +37,12 @@ function winBetsLists(args) {
                  (
                    select * 
                      from users
-                    where status = 2
+                    where status in (1, 2)
                  ) users
+                 left join titles on users.uid = titles.uid 
            where winlist.league_id = leagues.league_id
              and winlist.uid = users.uid
+           order by ${rangeCodebook(range)} desc
         `, { limit: 30, type: db.sequelize.QueryTypes.SELECT });
 
         // const leagueWinBetsListsQuery = await modules.firestore.collection(`users_win_lists_${key}`)
@@ -45,7 +51,7 @@ function winBetsLists(args) {
         //   .get();
 
         leagueWinBetsListsQuery.forEach(function (data) { // 這裡有順序性
-          leagueWinBetsLists.push( repackage(data.data()) );
+          leagueWinBetsLists.push( repackage(data, rangeCodebook(range)) );
         });
 
         winBetsLists[key] = leagueWinBetsLists;
@@ -60,17 +66,27 @@ function winBetsLists(args) {
   });
 }
 
-function repackage(ele) {
+function repackage(ele, rangstr) {
   let data = {
     win_bets: '',
     uid: ele.uid,
     avatar: ele.avatar,
-    displayname: ele.displayname,
-    rank: ''
+    displayname: ele.display_name,
   };
 
-  data['win_bets'] = ele[`this_month_win_bets`];
-  data['rank'] = ele[`rank`];
+  data[rangstr] = ele[rangstr];
+
+  // 大神要 顯示 預設稱號
+  if ([1, 2, 3, 4].includes(ele.rank_id)){
+    data['rank'] = ele.rank_id;
+    data['default_title'] = ele.default_title; // 目前 使用者資料表 只有一個預設 title 值
+    data['continune'] = ele.continue; // 連贏Ｎ場
+    data['predict_rate'] = [ele.predict_rate1, ele.predict_rate2, ele.predict_rate3]; // 近N日 N過 N
+    data['predict_rate2'] = [ele.predict_rate1, ele.predict_rate3];  // 近N日過 N
+    data['win_bets_continue'] = ele.win_bets_continue, // 勝注連過 Ｎ日
+    data['matches_rate'] = [ele.matches_rate1, ele.matches_rate2], // 近 Ｎ 場過 Ｎ 場;
+    data['matches_continue'] = ele.matches_continue // 連贏Ｎ場
+  }
 
   return data;
 }
