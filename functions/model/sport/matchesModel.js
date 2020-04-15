e17cee5d783fab3e7fa8312b5829e9bd74093089
蔡采潔 <d0177381@gmail.com>
2020-04-15 16:04:09 +0800

    [refine] fix zone problem and add match result returning

--- functions/model/sport/matchesModel.js
+++ functions/model/sport/matchesModel.js
@@ -21,25 +21,17 @@ function getMatchesWithDate(args) {
   return new Promise(async function (resolve, reject) {
     const flag_prematch = 1;
     const { league } = args;
-    const beginString = modules
-      .moment(args.date)
-      .utcOffset(modules.UTF8)
-      .format('YYYYMMDD');
-    const begin = modules.moment(beginString).unix();
-    const endString = modules
-      .moment(args.date)
-      .utcOffset(modules.UTF8)
-      .add(1, 'days')
-      .format('YYYYMMDD');
-    const end = modules.moment(endString).unix() - 1;
-    console.log('************');
-    console.log(beginString, endString);
-    console.log(begin, end);
-    console.log('************');
+    const begin = modules.convertTimezone(args.date);
+    const end =
+      modules.convertTimezone(args.date, {
+        op: 'add',
+        value: 1,
+        unit: 'days'
+      }) - 1;
     try {
       // 下面的 SELECT 是當讓分或大小分都沒開盤的情況
       const results = await db.sequelize.query(
-        `SELECT game.bets_id AS id, game.scheduled, game.status, game.spread_id, game.totals_id, 
+        `SELECT game.bets_id AS id, game.scheduled, game.status, game.spread_id, game.totals_id, game.home_points, game.away_points, game.spread_result, game.totals_result,
                 home.name AS home_name, home.alias_ch AS home_alias_ch, home.alias AS home_alias, home.image_id AS home_image_id, 
                 away.name AS away_name, away.alias_ch AS away_alias_ch, away.alias AS away_alias, away.image_id AS away_image_id, 
                 spread.handicap AS spread_handicap, spread.home_tw AS spread_home_tw, spread.away_tw AS spread_away_tw, spread.add_time AS spread_add_time, 
@@ -56,7 +48,7 @@ function getMatchesWithDate(args) {
             AND (game.spread_id = spread.spread_id AND game.bets_id = spread.match_id) 
             AND (game.totals_id = totals.totals_id AND game.bets_id = totals.match_id) 
           UNION 
-         SELECT game.bets_id AS id, game.scheduled, game.status, game.spread_id, game.totals_id, 
+         SELECT game.bets_id AS id, game.scheduled, game.status, game.spread_id, game.totals_id, game.home_points, game.away_points, game.spread_result, game.totals_result,
                 home.name AS home_name, home.alias_ch AS home_alias_ch, home.alias AS home_alias, home.image_id AS home_image_id, 
                 away.name AS away_name, away.alias_ch AS away_alias_ch, away.alias AS away_alias, away.image_id AS away_image_id, 
                 NULL AS spread_handicap, NULL AS spread_home_tw, NULL AS spread_away_tw, NULL AS spread_add_time, 
@@ -75,8 +67,6 @@ function getMatchesWithDate(args) {
       );
       return resolve(results);
     } catch (error) {
-      console.log(error);
-
       return reject(new AppError.MysqlError());
     }
   });
@@ -127,7 +117,21 @@ function repackageMatches(results, args) {
     };
     if (!ele.spread_id) temp.spread.disable = true;
     if (!ele.totals_id) temp.totals.disable = true;
-
+    if (ele.home_points) temp.home.points = ele.home_points;
+    if (ele.away_points) temp.away.points = ele.away_points;
+    // 中分洞要亮原本顯示的盤口，否則亮過盤結果
+    if (ele.spread_result) {
+      if (ele.spread_result === 'fair') {
+        temp.spread.result = ele.spread_result;
+        if (ele.spread_home_tw) temp.spread.result = 'home';
+        else if (ele.spread_away_tw) temp.spread.result = 'away';
+      } else temp.spread.result = ele.spread_result;
+    }
+    if (ele.totals_result) {
+      if (ele.spread_result === 'fair') {
+        temp.totals.result = 'over';
+      } else temp.totals.result = ele.totals_result;
+    }
     if (ele.status === scheduledStatus) {
       data.scheduled.push(temp);
     } else if (ele.status === inPlayStatus) {
