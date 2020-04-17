62e937707ae4c2ebc73bc66056405086b3393bde
蔡采潔 <d0177381@gmail.com>
2020-04-17 23:52:07 +0800

    [finish] user/prediction_matches API version2

--- functions/model/user/_predictMatchesModel.js
+++ functions/model/user/_predictMatchesModel.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-await-in-loop */
 /* eslint-disable consistent-return */
 const modules = require('../../util/modules');
-const dbEngine = require('../../util/databaseEngine');
 const AppError = require('../../util/AppErrors');
 const db = require('../../util/dbUtil');
+const NORMAL_USER_SELL = -1;
 const NORMAL_USER = 1;
 const GOD_USER = 2;
 const matchScheduledStatus = 2;
@@ -13,9 +13,9 @@ function prematch(args) {
     // 有無賽事 ID，檢查是否可以下注了（且時間必須在 scheduled 前），盤口 ID 是否是最新的
     try {
       // 檢查此使用者身份
-      const customClaims = await dbEngine.findUser(args.token.uid);
-      await isGodBelongToLeague(args, customClaims.titles);
-      await isNormalUserSell(args.sell, customClaims.role);
+      // db.Prediction.sync({ force: true });
+      await isGodBelongToLeague(args);
+      await isNormalUserSell(args);
       const filter = await checkMatches(args);
       return resolve(await sendPrediction(args, filter));
     } catch (err) {
@@ -25,21 +25,22 @@ function prematch(args) {
 }
 
 // 檢查玩家想賣牌，是否屬於該聯盟的大神
-function isGodBelongToLeague(args, userTitles = []) {
-  console.log(args.token.customClaims);
-
+function isGodBelongToLeague(args) {
   return new Promise(function (resolve, reject) {
-    if (args.sell === 1 && !userTitles.includes(args.league))
+    if (
+      args.sell === 1 &&
+      !args.token.customClaims.titles.includes(args.league)
+    )
       return reject(new AppError.UserCouldNotSell());
     else return resolve();
   });
 }
 
 // 檢查玩家想賣牌，是否屬於大神
-function isNormalUserSell(sell, role) {
+function isNormalUserSell(args) {
   return new Promise(function (resolve, reject) {
-    role = Number.parseInt(role);
-    if (role === NORMAL_USER && sell === 1)
+    role = Number.parseInt(args.token.customClaims.role);
+    if (role === NORMAL_USER && args.sell !== NORMAL_USER_SELL)
       return reject(new AppError.UserCouldNotSell());
     else return resolve();
   });
@@ -49,30 +50,21 @@ async function checkMatches(args) {
   return new Promise(async function (resolve, reject) {
     const needed = [];
     const failed = [];
-    // console.log(args.matches);
 
     try {
       for (let i = 0; i < args.matches.length; i++) {
         const ele = args.matches[i];
-
         await isMatchValid(args, ele, { needed, failed });
-        //   await isMatchExist(args, ele, { needed, failed });
-        // }
-        // for (let i = 0; i < needed.length; i++) {
-        //   isBeforeScheduled(args.now, i, { needed, failed });
-        //   isOpened(i, { needed, failed });
-        //   isNewestHandicap(i, { needed, failed });
-        //   //
-        //   if (args.token.customClaims.role === GOD_USER) {
-        //     await isGodUpdate(args.token.uid, i, { needed, failed });
-        //     if (needed[i].length === undefined) {
-        //       // 有資料的
-        //       await isGodSellConsistent(args, i, { needed, failed });
-        //     }
-        //   }
       }
-      console.log({ needed, failed });
-
+      for (let i = 0; i < needed.length; i++) {
+        if (args.token.customClaims.role === GOD_USER) {
+          await isGodUpdate(args.token.uid, i, { needed, failed });
+          if (needed[i].length === undefined) {
+            // 有資料的
+            await isGodSellConsistent(args, i, { needed, failed });
+          }
+        }
+      }
       return resolve({ needed, failed });
     } catch (err) {
       return reject(err);
@@ -81,25 +73,21 @@ async function checkMatches(args) {
 }
 async function isMatchValid(args, ele, filter) {
   return new Promise(async function (resolve, reject) {
-    console.log(ele);
-
     const { league } = args;
-    let handicapType = '';
-    let handicapId;
-    if (ele.spread) {
-      handicapType = 'spread';
-      handicapId = Number.parseInt(ele.spread[0]);
-    } else if (ele.totals) {
-      handicapType = 'totals';
-      handicapId = Number.parseInt(ele.totals[0]);
-    }
+    const { handicapType, handicapId } = handicapProcessor(ele);
     try {
       const results = await db.sequelize.query(
-        `SELECT *
-           FROM match__${league}s AS game
+        `SELECT game.*, 
+                home.team_id AS home_id, home.alias_ch AS home_alias_ch, home.alias AS home_alias,  
+                away.team_id AS away_id, away.alias_ch AS away_alias_ch, away.alias AS away_alias
+           FROM match__${league}s AS game, 
+                match__team__${league}s AS home,
+                match__team__${league}s AS away
           WHERE game.bets_id = :id
             AND game.${handicapType}_id = :handicapId
-            AND game.status = ${matchScheduledStatus}`,
+            AND game.status = ${matchScheduledStatus}
+            AND game.home_id = home.team_id
+            AND game.away_id = away.team_id`,
         {
           replacements: { id: ele.id, handicapId },
           type: db.sequelize.QueryTypes.SELECT
@@ -107,9 +95,21 @@ async function isMatchValid(args, ele, filter) {
       );
       if (!results.length) {
         ele.code = 404;
-        ele.error = `Match id: ${ele.id} & Handicap id ${handicapId} in ${args.league} not found`;
+        ele.error = `Match id: ${ele.id} [${handicapType}_id: ${handicapId}] in ${args.league} not acceptable`;
         filter.failed.push(ele);
       } else if (results) {
+        ele.match_scheduled = results[0].scheduled;
+        ele.home = {
+          id: results[0].home_id,
+          alias: results[0].home_alias,
+          alias_ch: results[0].home_alias_ch
+        };
+        ele.away = {
+          id: results[0].away_id,
+          alias: results[0].away_alias,
+          alias_ch: results[0].away_alias_ch
+        };
+        ele.league_id = modules.leagueCodebook(league).id;
         filter.needed.push(ele);
       }
       resolve(filter);
@@ -118,35 +118,49 @@ async function isMatchValid(args, ele, filter) {
     }
   });
 }
-async function isMatchExist(args, ele, filter) {
-  const matchSnapshot = await modules.getSnapshot(
-    modules.leagueCodebook(args.league).match,
-    ele.id
-  );
 
-  if (!matchSnapshot.exists) {
-    ele.code = 404;
-    ele.error = `Match id: ${ele.id} in ${args.league} not found`;
-    filter.failed.push(ele);
-  } else if (matchSnapshot.exists) {
-    // append match information
-    ele.data = matchSnapshot.data();
-    filter.needed.push(ele);
-  }
-}
-
-function isBeforeScheduled(now, i, filter) {
+async function isGodUpdate(uid, i, filter) {
   const ele = filter.needed[i];
-  if (ele.data.flag.status !== 2) {
-    // if (now >= ele.data.scheduled._seconds * 1000) {
-    const error = {
-      code: 403,
-      error: `Match id: ${ele.id} already start or end, forbidden`
-    };
-    filterProcessor(filter, i, error);
+  const { handicapType, handicapId } = handicapProcessor(ele);
+  try {
+    const predictResults = await db.sequelize.query(
+      `SELECT *
+         FROM user__predictions AS prediction
+        WHERE prediction.uid = :uid AND
+              prediction.bets_id = ${ele.id} AND
+              ${handicapType}_id = ${handicapId}`,
+      {
+        replacements: { uid },
+        type: db.sequelize.QueryTypes.SELECT
+      }
+    );
+
+    if (predictResults.length) {
+      const error = {
+        code: 403,
+        error: `${handicapType} id: ${handicapId} already exist, locked`
+      };
+      filterProcessor(filter, i, error);
+    }
+  } catch (err) {
+    console.log(err);
+    return reject(new AppError.MysqlError());
   }
 }
 
+function handicapProcessor(ele) {
+  let handicapType = '';
+  let handicapId;
+
+  if (ele.spread) {
+    handicapType = 'spread';
+    handicapId = ele.spread[0];
+  } else if (ele.totals) {
+    handicapType = 'totals';
+    handicapId = ele.totals[0];
+  }
+  return { handicapType, handicapId };
+}
 function filterProcessor(filter, i, error) {
   const ele = filter.needed[i];
   ele.code = error.code;
@@ -156,28 +170,36 @@ function filterProcessor(filter, i, error) {
   filter.failed.push(ele);
 }
 
-async function isGodSellConsistent(args, i, filter) {
-  const date = modules
-    .moment(filter.needed[i].data.scheduled._seconds * 1000)
-    .utcOffset(TAIWAN_UTF)
-    .format('YYYYMMDD');
-  const query = await modules.firestore
-    .collection(modules.db.prediction)
-    .where('uid', '==', args.token.uid)
-    .where('date', '==', date)
-    .get();
+function isGodSellConsistent(args, i, filter) {
+  return new Promise(async function (resolve, reject) {
+    try {
+      const ele = filter.needed[i];
+      const date = modules.convertTimezoneFormat(ele.match_scheduled);
+      const begin = modules.convertTimezone(date);
+      const end =
+        modules.convertTimezone(date, { op: 'add', value: 1, unit: 'days' }) -
+        1;
 
-  if (query.size > 0) {
-    query.docs.map(function (doc) {
-      if (doc.data().sell !== args.sell) {
-        const error = {
-          code: 403,
-          error: `Cannot be changed to ${args === 1 ? 'sell' : 'free'}`
-        };
-        filterProcessor(filter, i, error);
-      }
-    });
-  }
+      const results = await db.sequelize.query(
+        `SELECT prediction.sell
+         FROM user__predictions AS prediction
+        WHERE prediction.uid = :uid AND
+              prediction.match_scheduled BETWEEN ${begin} AND ${end}
+        LIMIT 1`,
+        {
+          replacements: { uid: args.token.uid },
+          type: db.sequelize.QueryTypes.SELECT
+        }
+      );
+      if (results.length) {
+        if (results[0].sell !== args.sell) {
+          return reject(new AppError.GodSellInconsistent());
+        } else return resolve();
+      } else return resolve();
+    } catch (err) {
+      return reject(new AppError.MysqlError());
+    }
+  });
 }
 
 function sendPrediction(args, filter) {
@@ -203,97 +225,66 @@ function isNeeded(needed) {
 }
 
 async function insertDB(args, needed) {
-  const results = [];
-  for (let i = 0; i < needed.length; i++) {
-    const ele = needed[i];
-    const data = {
-      bets_id: ele.id,
-      sell: args.sell,
-      uid: args.token.uid
-    };
-    if (ele.spread) {
-      data.spread_id = ele.spread[0];
-      data.spread_option = ele.spread[1];
-      data.spread_bets = ele.spread[2];
-    } else if (ele.totals) {
-      data.totals_id = ele.totals[0];
-      data.totals_option = ele.totals[1];
-      data.totals_bets = ele.totals[2];
-    }
-
-    try {
+  return new Promise(async function (resolve, reject) {
+    const results = [];
+    for (let i = 0; i < needed.length; i++) {
+      const ele = needed[i];
       if (ele.length === undefined) {
-        results.push(await db.Prediction.create(data));
-        // results.push(
-        //   modules.addDataInCollectionWithId(
-        //     modules.db.prediction,
-        //     `${ele.id}_${args.token.uid}`,
-        //     repackagePrediction(args, ele)
-        //   )
-        // );
+        const data = repackagePrediction(args, ele);
+        const { handicapType, handicapId } = handicapProcessor(ele);
+        results.push(db.Prediction.upsert(data));
+        console.log(
+          `User(${
+            args.token.customClaims.role === NORMAL_USER ? 'Normal' : 'God'
+          }) update or insert match id: ${
+            ele.id
+          } [${handicapType}_id: ${handicapId}] successful`
+        );
       }
-      return await Promise.all(results);
-    } catch (error) {
-      // console.log(error);
-
-      return error;
     }
-  }
+    try {
+      return resolve(await Promise.all(results));
+    } catch (err) {
+      return reject(new AppError.MysqlError());
+    }
+  });
 }
 
 function repackagePrediction(args, ele) {
-  const date = modules
-    .moment(ele.data.scheduled._seconds * 1000)
-    .utcOffset(TAIWAN_UTF)
-    .format('YYYYMMDD');
   const data = {
     bets_id: ele.id,
-    uid: args.token.uid,
-    league: args.league,
-    user_status: args.token.customClaims.role,
+    league_id: ele.league_id,
     sell: args.sell,
-    date,
-    date_timestamp: modules.moment(date).valueOf(),
-    scheduled: ele.data.scheduled._seconds * 1000,
-    home: {
-      alias: ele.data.home.alias,
-      alias_ch: ele.data.home.alias_ch
-    },
-    away: {
-      alias: ele.data.away.alias,
-      alias_ch: ele.data.away.alias_ch
-    }
+    match_scheduled: ele.match_scheduled,
+    uid: args.token.uid,
+    user_status: args.token.customClaims.role
   };
   if (ele.spread) {
-    data.spread = {
-      handicap_id: ele.spread[0],
-      predict: ele.spread[1],
-      handicap: ele.data.spread[ele.spread[0]].handicap,
-      bets: ele.spread[2],
-      update_time: Date.now()
-    };
+    data.spread_id = ele.spread[0];
+    data.spread_option = ele.spread[1];
+    data.spread_bets = ele.spread[2];
+  } else if (ele.totals) {
+    data.totals_id = ele.totals[0];
+    data.totals_option = ele.totals[1];
+    data.totals_bets = ele.totals[2];
   }
-  if (ele.totals) {
-    data.totals = {
-      handicap_id: ele.totals[0],
-      predict: ele.totals[1],
-      bets: ele.totals[2],
-      handicap: ele.data.totals[ele.totals[0]].handicap,
-      update_time: Date.now()
-    };
-  }
-  // console.log(data);
-
   return data;
 }
-
 function repackageReturnData(filter) {
   filter.success = [];
   for (let i = 0; i < filter.needed.length; i++) {
     const ele = filter.needed[i];
     if (ele.length === undefined) {
-      delete ele.data;
+      delete ele.league_id;
+      delete ele.match_scheduled;
       filter.success.push(ele);
+    }
+  }
+  for (let i = 0; i < filter.failed.length; i++) {
+    const ele = filter.failed[i];
+    if (ele.length === undefined) {
+      delete ele.league_id;
+      delete ele.match_scheduled;
     }
   }
   delete filter.needed;
