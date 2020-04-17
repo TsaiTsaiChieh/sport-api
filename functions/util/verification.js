4404d4fa7167306549984cc5cc107fdfcc6dc849
蔡采潔 <d0177381@gmail.com>
2020-04-18 01:02:31 +0800

    [refine] token function and add confirmLogin_v2 function

--- functions/util/verification.js
+++ functions/util/verification.js
@@ -43,6 +43,26 @@ async function confirmLogin(req, res, next) {
   }
   return next();
 }
+async function confirmLogin_v2(req, res, next) {
+  try {
+    const bearerHeader = req.headers['authorization'];
+    if (bearerHeader) {
+      const bearer = bearerHeader.split(' ');
+      const bearerToken = bearer[1];
+      decodedIdToken = await modules.firebaseAdmin
+        .auth()
+        .verifySessionCookie(bearerToken, true);
+      req.token = decodedIdToken;
+      req.token.customClaims = getRoleAndTitles(decodedIdToken.uid);
+    } else {
+      // do nothing
+    }
+  } catch (err) {
+    console.error('Error in util/verification confirmLogin functions', err);
+    return res.status(500).json({ code: 500, error: err });
+  }
+  return next();
+}
 
 async function token(req, res, next) {
   try {
@@ -78,29 +98,8 @@ async function token_v2(req, res, next) {
       const decodedIdToken = await modules.firebaseAdmin
         .auth()
         .verifySessionCookie(bearerToken, true);
-
-      const userResults = await db.User.findOne({
-        where: { uid: decodedIdToken.uid },
-        attributes: ['status']
-      });
       req.token = decodedIdToken;
-      if (userResults.status === NORMAL_USER) {
-        req.token.customClaims = { role: NORMAL_USER, titles: [] };
-      } else if (userResults.status === GOD_USER) {
-        const titlesResult = await db.Title.findAll({
-          where: {
-            uid: decodedIdToken.uid,
-            period: modules.getTitlesPeriod(new Date()).period
-          },
-          attributes: ['league_id']
-        });
-        const titles = [];
-        for (let i = 0; i < titlesResult.length; i++) {
-          titles.push(modules.leagueDecoder(titlesResult[i].league_id));
-        }
-
-        req.token.customClaims = { role: GOD_USER, titles };
-      }
+      req.token.customClaims = getRoleAndTitles(decodedIdToken.uid);
     }
   } catch (err) {
     console.error('Error in util/verification token_v2 functions', err);
@@ -109,4 +108,28 @@ async function token_v2(req, res, next) {
   return next();
 }
 
-module.exports = { token, admin, confirmLogin, token_v2 };
+async function getRoleAndTitles(uid) {
+  const userResults = await db.User.findOne({
+    where: { uid },
+    attributes: ['status']
+  });
+  if (userResults.status === NORMAL_USER) {
+    // req.token.customClaims = { role: NORMAL_USER, titles: [] };
+    return { role: NORMAL_USER, titles: [] };
+  } else if (userResults.status === GOD_USER) {
+    const titlesResult = await db.Title.findAll({
+      where: {
+        uid,
+        period: modules.getTitlesPeriod(new Date()).period
+      },
+      attributes: ['league_id']
+    });
+    const titles = [];
+    for (let i = 0; i < titlesResult.length; i++) {
+      titles.push(modules.leagueDecoder(titlesResult[i].league_id));
+    }
+    // req.token.customClaims = { role: GOD_USER, titles };
+    return { role: GOD_USER, titles };
+  }
+}
+module.exports = { token, token_v2, admin, confirmLogin, confirmLogin_v2 };
