ff0fb4b193cc7d90cd4741b7a976c038db812ce3
蔡采潔 <d0177381@gmail.com>
2020-04-18 23:17:27 +0800

    [tune] description

--- functions/test/sport/prematch.test.js
+++ functions/test/sport/prematch.test.js
@@ -4,7 +4,7 @@ const OLD_API = '/sport/prematch?league=NBA&date=2020-07-01';
 const NEW_API = '/sport/matches?league=NBA&date=2020-07-01';
 
 describe('GET /sport/prematch', function () {
-  test('--- [舊版]玩家看賽事資訊 API 測試開始 ---', function () {});
+  test('--- [舊版]玩家看賽事資訊 ---', function () {});
   test('(1) (本機) 使用者未登入', async function () {
     const res = await request(setting.localhost).get(OLD_API);
     expect(res.statusCode).toEqual(200);
@@ -25,7 +25,7 @@ describe('GET /sport/prematch', function () {
       .set('Authorization', `bearer ${setting.godUserToken_9}`);
     expect(res.statusCode).toEqual(200);
   });
-  test('--- [新版]玩家看賽事資訊 API 測試開始 ---', function () {});
+  test('--- [新版]玩家看賽事資訊 ---', function () {});
   test('(1) (本機) 使用者未登入', async function () {
     const res = await request(setting.localhost).get(NEW_API);
     expect(res.statusCode).toEqual(200);
