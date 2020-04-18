618d24af6a0b34b97988055b3df11e5e5186b618
蔡采潔 <d0177381@gmail.com>
2020-04-18 22:51:07 +0800

    [finish] prematch testing comparison

--- functions/test/sport/prematch.test.js
+++ functions/test/sport/prematch.test.js
@@ -1,24 +1,51 @@
-const URL = 'localhost:5000';
-const onlineURL = 'https://api-dosports.web.app';
+const setting = require('../../auth/jestTestSetting.json');
 const request = require('supertest');
 const OLD_API = '/sport/prematch?league=NBA&date=2020-07-01';
 const NEW_API = '/sport/matches?league=NBA&date=2020-07-01';
 
 describe('GET /sport/prematch', function () {
-  test('(1) Query matches when user not login with older API', async function () {
-    const res = await request(URL).get(OLD_API);
+  test('--- [舊版]玩家看賽事資訊 API 測試開始 ---', function () {});
+  test('(1) (本機) 使用者未登入', async function () {
+    const res = await request(setting.localhost).get(OLD_API);
     expect(res.statusCode).toEqual(200);
   });
-  test('(2) Query matches when user not login with newest API', async function () {
-    const res = await request(URL).get(NEW_API);
+  test('(2) (本機) 大神使用者已登入', async function () {
+    const res = await request(setting.localhost)
+      .get(OLD_API)
+      .set('Authorization', `bearer ${setting.godUserToken_9}`);
     expect(res.statusCode).toEqual(200);
   });
-  test('(3) Query matches when user not login with older on-line API', async function () {
-    const res = await request(onlineURL).get(OLD_API);
+  test('(3) (上線) 使用者未登入', async function () {
+    const res = await request(setting.online_URL).get(OLD_API);
     expect(res.statusCode).toEqual(200);
   });
-  test('(4) Query matches when user not login with newest on-line  API', async function () {
-    const res = await request(onlineURL).get(NEW_API);
+  test('(4) (線上) 大神使用者已登入', async function () {
+    const res = await request(setting.online_URL)
+      .get(OLD_API)
+      .set('Authorization', `bearer ${setting.godUserToken_9}`);
+    expect(res.statusCode).toEqual(200);
+  });
+  test('--- [新版]玩家看賽事資訊 API 測試開始 ---', function () {});
+  test('(1) (本機) 使用者未登入', async function () {
+    const res = await request(setting.localhost).get(NEW_API);
+    expect(res.statusCode).toEqual(200);
+  });
+
+  test('(2) (本機) 大神使用者已登入', async function () {
+    const res = await request(setting.localhost)
+      .get(NEW_API)
+      .set('Authorization', `bearer ${setting.godUserToken_9}`);
+    expect(res.statusCode).toEqual(200);
+  });
+  test('(3) (線上) 使用者未登入', async function () {
+    const res = await request(setting.online_URL).get(NEW_API);
+    expect(res.statusCode).toEqual(200);
+  });
+
+  test('(4) (線上) 大神使用者已登入', async function () {
+    const res = await request(setting.online_URL)
+      .get(NEW_API)
+      .set('Authorization', `bearer ${setting.godUserToken_9}`);
     expect(res.statusCode).toEqual(200);
   });
 });
