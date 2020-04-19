6f579a0fc46e23aea5c60e680fe1ef15ddf574e4
蔡采潔 <d0177381@gmail.com>
2020-04-19 10:43:26 +0800

    [finish] API comparison

--- functions/test/user/predictions.test.js
+++ functions/test/user/predictions.test.js
@@ -25,51 +25,65 @@ describe('POST /user/predictions', function () {
       .post(OLD_API)
       .set('Authorization', `bearer ${setting.godUserToken_9}`)
       .send(oneMatchPrediction);
-    expect(res.statusCode).toEqual(403);
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('devcode', 1000);
   });
   test('(3) (線上) 一般使用者更新預測單', async function () {
     const res = await request(setting.online_URL)
       .post(OLD_API)
       .set('Authorization', `bearer ${setting.frontendNormalUserToken_7}`)
       .send(oneMatchPrediction);
+
     expect(res.statusCode).toEqual(200);
   });
   test('(4) (線上) 大神使用者更新預測單', async function () {
-    const res = await request(setting.localhost)
+    const res = await request(setting.online_URL)
       .post(OLD_API)
       .set('Authorization', `bearer ${setting.godUserToken_9}`)
       .send(oneMatchPrediction);
-    expect(res.statusCode).toEqual(403);
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('devcode', 1000);
   });
   test('--- [新版]使用者送出預測 ---', function () {});
-  const newOneMatchPrediction = Object.assign({}, oneMatchPrediction);
-  newOneMatchPrediction.sell = -1;
+  const normalPrediction = Object.assign({}, oneMatchPrediction);
+  normalPrediction.sell = -1;
+  const godPrediction = Object.assign({}, oneMatchPrediction);
+  godPrediction.sell = 1;
+
   test('(1) (本機) 一般使用者更新預測單', async function () {
     const res = await request(setting.localhost)
       .post(NEW_API)
       .set('Authorization', `bearer ${setting.frontendNormalUserToken_7}`)
-      .send(newOneMatchPrediction);
+      .send(normalPrediction);
+
     expect(res.statusCode).toEqual(200);
   });
   test('(2) (本機) 大神使用者更新預測單', async function () {
     const res = await request(setting.localhost)
       .post(NEW_API)
       .set('Authorization', `bearer ${setting.godUserToken_9}`)
-      .send(newOneMatchPrediction);
-    expect(res.statusCode).toEqual(403);
+      .send(godPrediction);
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('devcode', 1000);
   });
   test('(3) (線上) 一般使用者更新預測單', async function () {
     const res = await request(setting.online_URL)
       .post(NEW_API)
       .set('Authorization', `bearer ${setting.frontendNormalUserToken_7}`)
-      .send(newOneMatchPrediction);
+      .send(normalPrediction);
+
     expect(res.statusCode).toEqual(200);
   });
   test('(4) (線上) 大神使用者更新預測單', async function () {
     const res = await request(setting.online_URL)
       .post(NEW_API)
       .set('Authorization', `bearer ${setting.godUserToken_9}`)
-      .send(newOneMatchPrediction);
-    expect(res.statusCode).toEqual(403);
+      .send(godPrediction);
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('devcode', 1000);
   });
 });
