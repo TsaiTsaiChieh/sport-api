63b4935789ce6db323f73252c9c858bb96d494b7
Tsai, Tsai-Chieh <jecica196@gmail.com>
2020-04-24 17:48:20 +0800

    [fininsh] predictionRateModel

--- functions/model/user/predictionRateModel.js
+++ functions/model/user/predictionRateModel.js
@@ -9,13 +9,10 @@ const endStatus = 0;
 async function predictionRate(args) {
   return new Promise(async function (resolve, reject) {
     try {
-      // db.Prediction.sync();
       const matches = await searchMatches(args);
       const predictions = await searchPredictions(args, matches);
-      const results = calculatePredictionRate(predictions);
-      resolve(results);
+      resolve(repackageReturnData(args.league, predictions, matches));
     } catch (err) {
-      console.log(err);
       return reject(err);
     }
   });
@@ -41,6 +38,7 @@ function searchMatches(args) {
             AND game.league_id = ${modules.leagueCodebook(league).id}`,
         { type: db.sequelize.QueryTypes.SELECT }
       );
+
       !results.length ? reject(new AppError.MatchNotFound()) : resolve(results);
     } catch (err) {
       return reject(new AppError.MysqlError());
@@ -75,6 +73,7 @@ function searchPredictions(args, matches) {
               AND user_status = ${GOD_STATUS}`,
           { type: db.sequelize.QueryTypes.SELECT }
         );
+
         return resolve(results);
       }
     } catch (err) {
@@ -83,48 +82,83 @@ function searchPredictions(args, matches) {
   });
 }
 
-function calculatePredictionRate(predictions) {
-  // const data = {
-  //   scheduled: [],
-  //   inplay: [],
-  //   end: []
-  // };
-  // for (let i = 0; i < matches.length; i++) {
-  //   const match = matches[i];
-  //   if (match.status === scheduledStatus) {
-  //     data.push(match.bets_id);
-  //   }
-  // }
-  // for (const key in predictions) {
-  //   if (Object.prototype.hasOwnProperty.call(predictions, key)) {
-  //     const ele = predictions[key];
-  //     console.log(ele);
-  //   }
-  // }
+function repackageReturnData(league, predictions, matches) {
+  const data = {
+    scheduled: [],
+    inplay: [],
+    end: []
+  };
+  const statsRate = [];
+  // initial
+  for (let i = 0; i < matches.length; i++) {
+    matches[i] = {
+      id: matches[i].bets_id,
+      status: matches[i].status,
+      league,
+      spread: { home: 0, away: 0, home_rate: '0%', away_rate: '0%' },
+      totals: { under: 0, over: 0, under_rate: '0%', over_rate: '0%' }
+    };
+  }
 
-  // Group by color as key to the person array
-  const predictionGroupedMatchId = groupBy(predictions, 'bets_id');
+  // Group by bets_id as key to the prediction array
+  const predictionGroupedMatchId = modules.groupBy(predictions, 'bets_id');
+
   for (const key in predictionGroupedMatchId) {
-    if (predictionGroupedMatchId.hasOwnProperty(key)) {
-      //   const element = predictionGroupedMatchId[key];
-      for (let i = 0; i < predictionGroupedMatchId[key].length; i++) {
-        const element = array[i];
+    statsRate.push(calculatePredictionRate(predictionGroupedMatchId[key]));
+  }
+  // 預測單的陣列數量需和賽事的一致
+  const eachMatchRate = filloutStatsRate(matches, statsRate);
+  for (let i = 0; i < eachMatchRate.length; i++) {
+    const match = eachMatchRate[i];
+    if (match.status === scheduledStatus) {
+      data.scheduled.push(match);
+    } else if (match.status === inplayStatus) {
+      data.inplay.push(match);
+    } else if (match.status === endStatus) {
+      data.end.push(match);
+    }
+  }
+  return data;
+}
+
+// 計算各賽事的盤口預測數目
+function calculatePredictionRate(prediction) {
+  const result = {
+    id: '',
+    spread: { home: 0, away: 0, home_rate: 0, away_rate: 0 },
+    totals: { under: 0, over: 0, under_rate: 0, over_rate: 0 }
+  };
+
+  for (let i = 0; i < prediction.length; i++) {
+    result.id = prediction[i].bets_id;
+    if (prediction[i].spread_option === 'home') result.spread.home += 1;
+    if (prediction[i].spread_option === 'away') result.spread.away += 1;
+    if (prediction[i].totals_option === 'under') result.totals.under += 1;
+    if (prediction[i].totals_option === 'over') result.totals.over += 1;
+  }
+
+  const { spread, totals } = result;
+  // 避免分母為零
+  const spreadDeno = spread.home + spread.away ? spread.home + spread.away : 1;
+  const totalsDeno =
+    totals.under + totals.over ? totals.under + totals.over : 1;
+
+  spread.home_rate = `${Math.floor((spread.home / spreadDeno) * 100)}%`;
+  spread.away_rate = `${Math.floor((spread.away / spreadDeno) * 100)}%`;
+  totals.under_rate = `${Math.floor((totals.under / totalsDeno) * 100)}%`;
+  totals.over_rate = `${Math.floor((totals.over / totalsDeno) * 100)}%`;
+  return result;
+}
+
+function filloutStatsRate(matches, statsRate) {
+  for (let i = 0; i < matches.length; i++) {
+    for (let j = 0; j < statsRate.length; j++) {
+      if (matches[i].id === statsRate[j].id) {
+        matches[i].spread = statsRate[j].spread;
+        matches[i].totals = statsRate[j].totals;
       }
     }
-    console.log(key);
   }
-  console.log(predictionGroupedMatchId);
+  return matches;
 }
-
-function groupBy(array, key) {
-  return array.reduce(function (result, currentValue) {
-    // If an array already present for key, push it to the array. Else create an array and push the object
-    (result[currentValue[key]] = result[currentValue[key]] || []).push(
-      currentValue
-    );
-    // Return the current iteration `result` value, this will be taken as next iteration `result` value and accumulate
-    return result;
-  }, {});
-}
-
 module.exports = predictionRate;
