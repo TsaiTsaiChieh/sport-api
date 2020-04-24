006980178d3357764cc277732c0f4d737699e061
Tsai, Tsai-Chieh <jecica196@gmail.com>
2020-04-24 17:46:48 +0800

    [solve] conflict

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -14,35 +14,15 @@ module.exports = {
     sourceType: 'module'
   },
   rules: {
-<<<<<<< HEAD
     camelcase: 'off',
     semi: 0,
     'no-tabs': 0,
-||||||| merged common ancestors
-    'camelcase': 'off',
-    'semi':0,
-    'no-tabs':0,
-=======
-    'camelcase': 'off',
-    'no-tabs':0,
->>>>>>> test
+    camelcase: 'off',
+    'no-tabs': 0,
     'no-unused-vars': 0,
-<<<<<<< HEAD
     'no-async-promise-executor': 0,
     'prefer-promise-reject-errors': 0,
     'block-spacing': 0,
     'space-before-function-paren': 0
-||||||| merged common ancestors
-    'no-async-promise-executor':0,
-    'prefer-promise-reject-errors':0,
-    'block-spacing':0,
-    'space-before-function-paren':0
-=======
-    'no-async-promise-executor':0,
-    'prefer-promise-reject-errors':0,
-    'block-spacing':0,
-    'space-before-function-paren':['error','never'],
-    'semi':['error','always']
->>>>>>> test
   }
 };
