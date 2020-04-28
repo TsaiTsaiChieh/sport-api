70e950bc9cbf939e1f738f5b361a0ed0b06524ee
蔡采潔 <d0177381@gmail.com>
2020-04-28 18:09:32 +0800

    [pending] not finish all

---
+++ functions/controller/user/deletePredictsController.js
@@ -0,0 +1,218 @@
+const modules = require('../../util/modules');
+const model = require('../../model/user/deletePredictsModel');
+
+// eslint-disable-next-line consistent-return
+async function deletePredictions(req, res) {
+  const now = Date.now();
+  const spreadSchema = {
+    spread: {
+      type: 'string',
+      pattern: modules.acceptNumberAndLetter
+    }
+  };
+  const totalsSchema = {
+    totals: {
+      type: 'string',
+      pattern: modules.acceptNumberAndLetter
+    }
+  };
+
+  const schema = {
+    type: 'object',
+    required: ['league', 'matches'],
+    properties: {
+      league: {
+        type: 'string',
+        enum: ['NBA', 'eSoccer']
+      },
+      matches: {
+        type: 'array',
+        items: {
+          type: 'object',
+          required: ['id'],
+          maxItems: 30,
+          anyOf: [
+            {
+              type: 'object',
+              required: ['spread']
+            },
+            {
+              type: 'object',
+              required: ['totals']
+            }
+          ],
+          properties: {
+            id: {
+              type: 'string',
+              pattern: modules.acceptNumberAndLetter
+            },
+            spread: spreadSchema,
+            totals: totalsSchema
+          }
+        }
+      }
+    }
+  };
+
+  const valid = modules.ajv.validate(schema, req.body);
+  if (!valid) {
+    return res.status(400).json(modules.ajv.errors);
+  }
+  req.body.token = req.token;
+  req.body.now = now;
+  try {
+    res.json(await model(req.body));
+  } catch (err) {
+    console.error(
+      'Error in controller/user/deletePredictions by TsaiChieh',
+      err
+    );
+    res
+      .status(err.code)
+      .json(
+        err.isPublic
+          ? { error: err.name, devcode: err.status, message: err.message }
+          : err.code
+      );
+  }
+}
+
+module.exports = deletePredictions;
+
+/**
+ * @api {delete} /user/predictions Predict Matches
+ * @apiVersion 1.0.0
+ * @apiDescription Normal user delete own prediction form by Tsai-Chieh
+ * @apiName Delete own prediction form
+ * @apiGroup User
+ * @apiPermission login user with completed data
+ *
+ * @apiParam (Request cookie) {token} __session token generate from firebase Admin SDK
+ * @apiParam {String} league league name, the value enum are: `NBA`, `eSoccer`
+ * @apiParam {Array} matches prediction form
+ * @apiParam {String} matches.id match id
+ * @apiParam {String} [matches.spread] spread id 
+ * @apiParam {String} [matches.totals] totals id
+ *
+ * @apiParamExample {JSON} Request-Example
+{
+	"league":"NBA", 
+	"matches":[
+		{ "id": "2120643", "spread":"31298793" },
+		{ "id": "2120643", "totals":"34456538" }
+    ]
+}
+ * @apiSuccess {Array} success succeeded prediction
+ * @apiSuccess {String} success.id match id which is successful
+ * @apiSuccess {Array} [success.spread] spread[0] spread information array which is successful
+ * @apiSuccess {Array} [success.totals] totals[0] totals information array which is successful
+ *
+ * @apiSuccessExample {JSON} Success-Response
+ *  HTTP/1.1 200 OK
+{
+    "failed": [],
+    "success": [
+        {
+            "id": "2115973",
+            "spread": [
+                "31268919",
+                "home",
+                3
+            ]
+        },
+        {
+            "id": "2115973",
+            "totals": [
+                "34417671",
+                "over",
+                2
+            ]
+        }
+    ]
+}
+
+ * @apiError 200 OK (but all predictions are failed)
+ * @apiError 400 Bad Request
+ * @apiError 403 Forbidden
+ *
+ * @apiErrorExample {JSON} 200-Response
+ * {
+    "error": "UserPredictFailed",
+    "devcode": 1000,
+    "message": {
+        "failed": [
+            {
+                "id": "2118810",
+                "spread": [
+                    "31235573",
+                    "home",
+                    3
+                ],
+                "match_scheduled": 1593572400,
+                "home": {
+                    "id": "55290",
+                    "alias": "SAC",
+                    "alias_ch": "國王"
+                },
+                "away": {
+                    "id": "54878",
+                    "alias": "NOP",
+                    "alias_ch": "鵜鶘"
+                },
+                "league_id": 2274,
+                "code": 403,
+                "error": "spread id: 31235573 already exist, locked"
+            },
+            {
+                "id": "2118810",
+                "totals": [
+                    "34458529",
+                    "under",
+                    1
+                ],
+                "match_scheduled": 1593572400,
+                "home": {
+                    "id": "55290",
+                    "alias": "SAC",
+                    "alias_ch": "國王"
+                },
+                "away": {
+                    "id": "54878",
+                    "alias": "NOP",
+                    "alias_ch": "鵜鶘"
+                },
+                "league_id": 2274,
+                "code": 403,
+                "error": "totals id: 34458529 already exist, locked"
+            }
+        ]
+    }
+}
+ * @apiErrorExample {JSON} 400-Response
+ * HTTP/1.1 400 Bad Request
+ * [
+    {
+        "keyword": "enum",
+        "dataPath": ".league",
+        "schemaPath": "#/properties/league/enum",
+        "params": {
+            "allowedValues": [
+                "NBA"
+            ]
+        },
+        "message": "should be equal to one of the allowed values"
+    }
+]
+* @apiErrorExample {JSON} 403-Response
+ * HTTP/1.1 403 Bad Request
+ * {
+    "error": "UserPredictFailed",
+    "devcode": 1202
+}
+ * @apiErrorExample {JSON} 500-Response
+ * HTTP/1.1 500 Internal Server Error
+ * {
+    "code": 500,
+    "error": {}
+}
+ */
---
+++ functions/model/user/deletePredictsModel.js
@@ -0,0 +1,243 @@
+/* eslint-disable no-await-in-loop */
+/* eslint-disable consistent-return */
+const modules = require('../../util/modules');
+const AppError = require('../../util/AppErrors');
+const db = require('../../util/dbUtil');
+const NORMAL_USER = 1;
+const scheduledStatus = 2;
+const NORMAL_USER_SELL = -1;
+
+function deletePredictions(args) {
+  return new Promise(async function (resolve, reject) {
+    try {
+      await isNormalUser(args);
+
+      const filter = await checkMatches(args);
+
+      await deletePrediction(args, filter);
+      console.log('===', filter, '===');
+      return resolve();
+    } catch (err) {
+      return reject(err);
+    }
+  });
+}
+
+// 檢查使用者是否為一般玩家
+function isNormalUser(args) {
+  return new Promise(function (resolve, reject) {
+    const { role } = args.token.customClaims;
+    role === NORMAL_USER
+      ? resolve()
+      : reject(new AppError.OnlyAcceptNormalUser());
+  });
+}
+
+async function checkMatches(args) {
+  return new Promise(async function (resolve, reject) {
+    const needed = [];
+    const failed = [];
+
+    try {
+      for (let i = 0; i < args.matches.length; i++) {
+        const ele = args.matches[i];
+        await isMatchValid(args, ele, { needed, failed });
+      }
+      for (let i = 0; i < needed.length; i++) {
+        await isHandicapExist(args, i, { needed, failed });
+      }
+      return resolve({ needed, failed });
+    } catch (err) {
+      return reject(err);
+    }
+  });
+}
+
+// 檢查賽事是否合法
+function isMatchValid(args, ele, filter) {
+  return new Promise(async function (resolve, reject) {
+    const { league } = args;
+    try {
+      const result = await db.sequelize.query(
+        `SELECT bets_id
+           FROM matches
+          WHERE bets_id = :id
+            AND status = ${scheduledStatus}`,
+        {
+          type: db.sequelize.QueryTypes.SELECT,
+          replacements: { id: ele.id }
+        }
+      );
+
+      // 若賽事 id 無效，推到 failed；反之，推到 needed
+      if (!result.length) {
+        ele.code = 404;
+        ele.error = `Match id: ${ele.id} in ${league} not found`;
+        filter.failed.push(ele);
+      } else {
+        filter.needed.push(ele);
+      }
+      resolve(filter);
+    } catch (err) {
+      return reject(new AppError.MysqlError());
+    }
+  });
+}
+// 檢查盤口是否存在在該使用者的預測單裡
+function isHandicapExist(args, i, filter) {
+  return new Promise(async function (resolve, reject) {
+    const { league } = args;
+    const ele = filter.needed[i];
+    try {
+      const { handicapType, handicapId } = handicapProcessor(ele);
+
+      const result = await db.sequelize.query(
+        `SELECT * 
+           FROM user__predictions
+          WHERE uid = "${args.token.uid}"
+            AND bets_id = :id
+            AND ${handicapType}_id = "${handicapId}"
+            AND sell = ${NORMAL_USER_SELL}`,
+        {
+          type: db.sequelize.QueryTypes.SELECT,
+          replacements: { id: ele.id }
+        }
+      );
+
+      if (!result.length) {
+        const error = {
+          code: 404,
+          error: `${handicapType} id: ${handicapId} in ${league} not found`
+        };
+        filterProcessor(filter, i, error);
+      }
+      return resolve();
+    } catch (err) {
+      return reject(new AppError.MysqlError());
+    }
+  });
+}
+// 轉換要查詢哪種盤口種類
+function handicapProcessor(ele) {
+  let handicapType = '';
+  let handicapId;
+
+  if (ele.spread) {
+    handicapType = 'spread';
+    handicapId = ele.spread;
+  } else if (ele.totals) {
+    handicapType = 'totals';
+    handicapId = ele.totals;
+  }
+  return { handicapType, handicapId };
+}
+// 將無效的賽事 id 和盤口 id 推到 failed，也清空原本在 needed 的位置
+function filterProcessor(filter, i, error) {
+  const ele = filter.needed[i];
+  ele.code = error.code;
+  ele.error = error.error;
+  delete ele.data;
+  filter.needed[i] = []; // clear
+  filter.failed.push(ele);
+}
+
+function deletePrediction(args, filter) {
+  return new Promise(async function (resolve, reject) {
+    const neededResult = isNeeded(filter.needed);
+    if (!neededResult) {
+      return reject(new AppError.UserPredictFailed({ failed: filter.failed }));
+    } else if (neededResult) {
+      await deleteFromDB(args, filter);
+      return resolve(repackageReturnData(filter));
+    }
+  });
+}
+
+function isNeeded(needed) {
+  let count = 0;
+  for (let i = 0; i < needed.length; i++) {
+    const ele = needed[i];
+    if (ele.length === 0) count++;
+  }
+  if (count === needed.length) return false;
+  return true;
+}
+
+async function deleteFromDB(args, filter) {
+  return new Promise(async function (resolve, reject) {
+    try {
+      for (let i = 0; i < filter.needed.length; i++) {
+        const ele = filter.needed[i];
+        const { handicapType, handicapId } = handicapProcessor(ele);
+        if (ele.length === undefined) {
+          const result = await db.sequelize.query(
+            `UPDATE user__predictions
+              SET ${handicapType}_id = NULL, ${handicapType}_bets = NULL, ${handicapType}_option = NULL
+            WHERE ${handicapType}_id = "${handicapId}"
+              AND bets_id = :id
+              AND uid = "${args.token.uid}"`,
+            {
+              type: db.sequelize.QueryTypes.UPDATE,
+              replacements: { id: ele.id }
+            }
+          );
+          // result = [undefined, 1] 代表有更新成功；反之 [undefined, 0]
+          if (!result[1]) {
+            const error = {
+              code: 404,
+              error: `${handicapType} id: ${handicapId} in ${args.league} update failed`
+            };
+            filterProcessor(filter, i, error);
+          }
+        }
+      }
+      return resolve();
+      // return resolve(await Promise.all(results));
+    } catch (err) {
+      console.log('========', err);
+      return reject(new AppError.MysqlError());
+    }
+  });
+}
+
+function repackagePrediction(args, ele) {
+  const data = {
+    bets_id: ele.id,
+    league_id: ele.league_id,
+    sell: args.sell,
+    match_scheduled: ele.match_scheduled,
+    uid: args.token.uid,
+    user_status: args.token.customClaims.role
+  };
+  if (ele.spread) {
+    data.spread_id = ele.spread[0];
+    data.spread_option = ele.spread[1];
+    data.spread_bets = ele.spread[2];
+  } else if (ele.totals) {
+    data.totals_id = ele.totals[0];
+    data.totals_option = ele.totals[1];
+    data.totals_bets = ele.totals[2];
+  }
+  return data;
+}
+function repackageReturnData(filter) {
+  filter.success = [];
+  for (let i = 0; i < filter.needed.length; i++) {
+    const ele = filter.needed[i];
+    if (ele.length === undefined) {
+      delete ele.league_id;
+      delete ele.match_scheduled;
+      filter.success.push(ele);
+    }
+  }
+  for (let i = 0; i < filter.failed.length; i++) {
+    const ele = filter.failed[i];
+    if (ele.length === undefined) {
+      delete ele.league_id;
+      delete ele.match_scheduled;
+    }
+  }
+  delete filter.needed;
+  return filter;
+}
+module.exports = deletePredictions;
