cf7b53ba73e455390035bb7a1731dea94b4a815b
Alan <alan@Alans-MacBook-Pro.local>
2020-04-28 15:22:11 +0800

    修改 apidoc

--- functions/controller/user/settleWinListController.js
+++ functions/controller/user/settleWinListController.js
@@ -36,226 +36,40 @@ module.exports = settleWinList;
  * @apiPermission None
  *
  * @apiParam (Request header) {Authorization} Authorization bearer token generate from firebase Admin SDK
- * @apiParam (Request body) {bets_id} bets_id bets id (Match id)
+ * @apiParam (Request body) {date} date date
  * @apiSuccess {JSON} result Settle Win List success
  *
  * @apiSuccessExample Success-Response:
  *  HTTP/1.1 200 OK
 {
-  "NBA": [
-    {
-      "bets_id": "2115973",
-      "scheduled": 1585627804000,
-      "league": "NBA",
-      "home": "CHA",
-      "home_ch": "黃蜂",
-      "spread": {
-        "predict": "under",
-        "handicap": 210.5,
-        "percentage": 8,
-        "bets": 1
-      },
-      "totals": {}
+  "status": {
+    "1": {
+      "msg": "使用者-聯盟 歷史勝注勝率資料更新成功！",
+      "lists": [
+        {
+          "uid": "2WMRgHyUwvTLyHpLoANk7gWADZn1",
+          "league": 3939
+        },
+        {
+          "uid": "3IB0w6G4V8QUM2Ti3iCIfX4Viux1",
+          "league": 3939
+        }
+      ]
     },
-    {
-      "bets_id": "2115973",
-      "scheduled": 1585628027000,
-      "league": "NBA",
-      "home": "CHA",
-      "home_ch": "黃蜂",
-      "spread": {
-        "predict": "under",
-        "handicap": 210.5,
-        "percentage": 16,
-        "bets": 1
-      },
-      "totals": {}
-    },
-    {
-      "bets_id": "2117403",
-      "scheduled": 1585628027000,
-      "league": "NBA",
-      "home": "MIN",
-      "home_ch": "灰狼",
-      "spread": {
-        "predict": "away",
-        "handicap": 12.5,
-        "percentage": 17,
-        "bets": 1
-      },
-      "totals": {}
-    },
-    {
-      "bets_id": "2114519",
-      "scheduled": 1585714204000,
-      "league": "NBA",
-      "home": "PHI",
-      "home_ch": "76人",
-      "spread": {
-        "predict": "under",
-        "handicap": 214.5,
-        "percentage": 10,
-        "bets": 1
-      },
-      "totals": {}
-    },
-    {
-      "bets_id": "2114519",
-      "scheduled": 1585714427000,
-      "league": "NBA",
-      "home": "PHI",
-      "home_ch": "76人",
-      "spread": {
-        "predict": "under",
-        "handicap": 214.5,
-        "percentage": 11,
-        "bets": 1
-      },
-      "totals": {}
-    },
-    {
-      "bets_id": "2117404",
-      "scheduled": 1585714427000,
-      "league": "NBA",
-      "home": "GSW",
-      "home_ch": "勇士",
-      "spread": {
-        "predict": "under",
-        "handicap": 226.5,
-        "percentage": 20,
-        "bets": 2
-      },
-      "totals": {}
-    },
-    {
-      "bets_id": "2117404",
-      "scheduled": 1585800604000,
-      "league": "NBA",
-      "home": "GSW",
-      "home_ch": "勇士",
-      "spread": {
-        "predict": "under",
-        "handicap": 226.5,
-        "percentage": 42,
-        "bets": 2
-      },
-      "totals": {}
-    },
-    {
-      "bets_id": "2117404",
-      "scheduled": 1585800827000,
-      "league": "NBA",
-      "home": "GSW",
-      "home_ch": "勇士",
-      "spread": {
-        "predict": "under",
-        "handicap": 226.5,
-        "percentage": 32,
-        "bets": 2
-      },
-      "totals": {}
+    "2": {
+      "msg": "使用者-聯盟 勝注勝率資料更新成功！",
+      "lists": [
+        {
+          "uid": "2WMRgHyUwvTLyHpLoANk7gWADZn1",
+          "league": 3939
+        },
+        {
+          "uid": "3IB0w6G4V8QUM2Ti3iCIfX4Viux1",
+          "league": 3939
+        }
+      ]
     }
-  ],
-  "MLB": [
-    {
-      "bets_id": "2115973",
-      "scheduled": 1585628127000,
-      "league": "MLB",
-      "home": "CHA",
-      "home_ch": "黃蜂",
-      "spread": {
-        "predict": "under",
-        "handicap": 210.5,
-        "percentage": 32,
-        "bets": 1
-      },
-      "totals": {}
-    },
-    {
-      "bets_id": "2115973",
-      "scheduled": 1585714383000,
-      "league": "MLB",
-      "home": "CHA",
-      "home_ch": "黃蜂",
-      "spread": {
-        "predict": "under",
-        "handicap": 210.5,
-        "percentage": 32,
-        "bets": 1
-      },
-      "totals": {}
-    },
-    {
-      "bets_id": "2117403",
-      "scheduled": 1585714383000,
-      "league": "MLB",
-      "home": "MIN",
-      "home_ch": "灰狼",
-      "spread": {
-        "predict": "away",
-        "handicap": 12.5,
-        "percentage": 28,
-        "bets": 1
-      },
-      "totals": {}
-    },
-    {
-      "bets_id": "2114519",
-      "scheduled": 1585714527000,
-      "league": "MLB",
-      "home": "PHI",
-      "home_ch": "76人",
-      "spread": {
-        "predict": "under",
-        "handicap": 214.5,
-        "percentage": 8,
-        "bets": 1
-      },
-      "totals": {}
-    },
-    {
-      "bets_id": "2115973",
-      "scheduled": 1585714527000,
-      "league": "MLB",
-      "home": "CHA",
-      "home_ch": "黃蜂",
-      "spread": {
-        "predict": "under",
-        "handicap": 210.5,
-        "percentage": 12,
-        "bets": 1
-      },
-      "totals": {}
-    },
-    {
-      "bets_id": "2117403",
-      "scheduled": 1585714527000,
-      "league": "MLB",
-      "home": "MIN",
-      "home_ch": "灰狼",
-      "spread": {
-        "predict": "away",
-        "handicap": 12.5,
-        "percentage": 26,
-        "bets": 1
-      },
-      "totals": {}
-    },
-    {
-      "bets_id": "2117404",
-      "scheduled": 1585800927000,
-      "league": "MLB",
-      "home": "GSW",
-      "home_ch": "勇士",
-      "spread": {
-        "predict": "under",
-        "handicap": 226.5,
-        "percentage": 11,
-        "bets": 2
-      },
-      "totals": {}
-    }
-  ]
+  }
 }
  *
  * @apiError 404
@@ -264,15 +78,6 @@ module.exports = settleWinList;
  * HTTP/1.1 404 Not Found
  * {
     "code": "1301",
-    "msg": "使用者狀態異常"
- * }
- *
- * @apiError 404
- *
- * @apiErrorExample {JSON} Error-1302
- * HTTP/1.1 404 Not Found
- * {
-    "code": "1302",
     "msg": "使用者狀態異常"
  * }
  *
