1442e9c7571115ecaeb11ffc0c29098119fbf75e
henry <henry@gets-info.com>
2020-04-29 15:38:26 +0800

    firebaseLogin、news api

--- functions/model/user/newsModel.js
+++ functions/model/user/newsModel.js
@@ -1,64 +1,83 @@
 const modules = require('../../util/modules');
 const db      = require('../../util/dbUtil'); 
 
-function newsModel(args, uid) {
+function newsModel(method, args, uid) {
   return new Promise(async function(resolve, reject) {
     try {
-
-      console.log(args);
-      /*取前一個月時間*/
-      const end = modules.moment(new Date()).format('YYYY-MM-DD HH:mm:ss');;
-      const begin = modules.moment(new Date()).subtract(1,'months').format('YYYY-MM-DD HH:mm:ss');;
-
-      /*系統訊息設定*/
-      const page_system = args.page_s || 0;
-      const limit_system = args.limit_s || 10;
-      const start_system = page_system*limit_system;
-
-      /*使用者訊息設定*/
-      const page_user = args.page_u || 0;
-      const limit_user = args.limit_u || 10;
-      const start_user = page_user*limit_user;
       
-      /*系統訊息資料*/
-      const system = await db.sequelize.query(
-        `
-        SELECT * 
-          FROM user__news
-        WHERE createdAt BETWEEN '${begin}' and '${end}' 
-          AND status=0    
-          AND uid = '${uid}'
-        ORDER BY createdAt DESC
-        LIMIT ${start_system}, ${limit_system}
-        `,
-        {
-          type: db.sequelize.QueryTypes.SELECT
-        }
-      );
+      if(method=='POST'){
+        /*取前一個月時間*/
+        const end = modules.moment(new Date()).format('YYYY-MM-DD HH:mm:ss');;
+        const begin = modules.moment(new Date()).subtract(1,'months').format('YYYY-MM-DD HH:mm:ss');;
+
+        /*系統訊息設定*/
+        const page_system = args.page_system || 0;
+        const limit_system = args.limit_system || 10;
+        const start_system = page_system*limit_system;
+
+        /*使用者訊息設定*/
+        const page_user = args.page_user || 0;
+        const limit_user = args.limit_user || 10;
+        const start_user = page_user*limit_user;
+        
+        /*系統訊息資料*/
+        const system = await db.sequelize.query(
+          `
+          SELECT * 
+            FROM user__news
+          WHERE createdAt BETWEEN '${begin}' and '${end}' 
+            AND status=0
+          ORDER BY createdAt DESC
+          LIMIT ${start_system}, ${limit_system}
+          `,
+          {
+            type: db.sequelize.QueryTypes.SELECT
+          }
+        );
+      
     
-      /*使用者訊息資料*/
-      const user = await db.sequelize.query(
-        `
-        SELECT * 
-          FROM user__news un, users u
-        WHERE un.createdAt BETWEEN '${begin}' and '${end}'
-          AND u.uid = un.uid
-          AND un.status=1
-          AND un.uid = '${uid}'
-        ORDER BY un.createdAt DESC
-          LIMIT ${start_user}, ${limit_user}
-        `,
-        {
-          type: db.sequelize.QueryTypes.SELECT
-        }
-      );
+        /*使用者訊息資料*/
+        const user = await db.sequelize.query(
+          `
+          SELECT * 
+            FROM user__news un, users u
+          WHERE un.createdAt BETWEEN '${begin}' and '${end}'
+            AND u.uid = un.uid
+            AND un.status=1
+            AND un.uid = '${uid}'
+          ORDER BY un.createdAt DESC
+            LIMIT ${start_user}, ${limit_user}
+          `,
+          {
+            type: db.sequelize.QueryTypes.SELECT
+          }
+        );
 
-      const newsList = {
-        'system': system,
-        'user'  : user
+        const newsList = {
+          'system': system,
+          'user'  : user
+        }
+        resolve(newsList);
+      }else if(method=='DELETE'){
+        const del_join = args.join(",");
+
+        const del_res = db.sequelize.query(`DELETE FROM user__news WHERE uid = '${uid}' news_id in (${del_join})`);
+        
+        if(del_res){
+          const result = {
+            "code" : "200",
+            "msg"  : "刪除成功"
+          };
+          resolve(result);
+        }else{
+          const result = {
+            "code" : "400",
+            "msg"  : "刪除失敗"
+          };
+          resolve(result);
+        }
+        
       }
-      resolve(newsList);
-      
     } catch (err) {
       console.log('error happened...', err);
       reject({ code: 500, error: err });
