85cd670cbefbdc775cdefd31dc8216633bae9831
d0177381@gmail.com <>
2020-04-30 11:07:48 +0800

    [tune] apidoc

--- functions/controller/sport/matchesController.js
+++ functions/controller/sport/matchesController.js
@@ -75,163 +75,54 @@ module.exports = getMatches;
  * @apiSuccessExample {JSON} Success-Response
  *  HTTP/1.1 200 OK
  * {
+    "sell": -1,
     "scheduled": [
         {
-            "id": "2118809",
-            "scheduled": 1593574200,
+            "id": "2120643",
+            "scheduled": 1593563400,
+            "scheduled_tw": "2020-07-01T00:30:00.000Z",
             "status": 2,
             "league": "NBA",
             "home": {
-                "alias": "MEM",
-                "alias_ch": "灰熊",
-                "image_id": "3415"
+                "team_name": "OKC",
+                "alias": "OKC",
+                "alias_ch": "雷霆",
+                "image_id": "3418"
             },
             "away": {
-                "alias": "ORL",
-                "alias_ch": "魔術",
-                "image_id": "3437"
+                "team_name": "UTA",
+                "alias": "UTA",
+                "alias_ch": "爵士",
+                "image_id": "3434"
             },
             "spread": {
-                "id": "31236860",
-                "handicap": 2,
-                "home_tw": "2平",
+                "id": "31298793",
+                "handicap": 3.5,
+                "home_tw": "3輸",
                 "away_tw": null,
                 "disable": false
             },
             "totals": {
-                "id": "34364723",
-                "handicap": 218,
-                "over_tw": "218",
-                "disable": false
-            }
-        },
-        {
-            "id": "2118810",
-            "scheduled": 1593572400,
-            "status": 2,
-            "league": "NBA",
-            "home": {
-                "alias": "SAC",
-                "alias_ch": "國王",
-                "image_id": "3413"
-            },
-            "away": {
-                "alias": "NOP",
-                "alias_ch": "鵜鶘",
-                "image_id": "5539"
-            },
-            "spread": {
-                "id": "31235573",
-                "handicap": -1.5,
-                "home_tw": null,
-                "away_tw": "1輸",
-                "disable": false
-            },
-            "totals": {
-                "id": "34458529",
-                "handicap": 232,
-                "over_tw": "232",
-                "disable": false
-            }
-        },
-        {
-            "id": "2118816",
-            "scheduled": 1593568800,
-            "status": 2,
-            "league": "NBA",
-            "home": {
-                "alias": "CHI",
-                "alias_ch": "公牛",
-                "image_id": "3409"
-            },
-            "away": {
-                "alias": "CLE",
-                "alias_ch": "騎士",
-                "image_id": "3432"
-            },
-            "spread": {
-                "id": "31238069",
-                "handicap": 4,
-                "home_tw": "4平",
-                "away_tw": null,
-                "disable": false
-            },
-            "totals": {
-                "id": "34346860",
-                "handicap": 217.5,
-                "over_tw": "217.5",
-                "disable": false
-            }
-        },
-        {
-            "id": "2119439",
-            "scheduled": 1593568800,
-            "status": 2,
-            "league": "NBA",
-            "home": {
-                "alias": "POR",
-                "alias_ch": "拓荒者",
-                "image_id": "3414"
-            },
-            "away": {
-                "alias": "PHX",
-                "alias_ch": "太陽",
-                "image_id": "3416"
-            },
-            "spread": {
-                "id": "31249410",
-                "handicap": 5.5,
-                "home_tw": "5輸",
-                "away_tw": null,
-                "disable": false
-            },
-            "totals": {
-                "id": "34384039",
-                "handicap": 234,
-                "over_tw": "234",
-                "disable": false
-            }
-        },
-        {
-            "id": "2118817",
-            "scheduled": 1593567000,
-            "status": 2,
-            "league": "NBA",
-            "home": {
-                "alias": "SAS",
-                "alias_ch": "馬刺",
-                "image_id": "3429"
-            },
-            "away": {
-                "alias": "DAL",
-                "alias_ch": "獨行俠",
-                "image_id": "3411"
-            },
-            "spread": {
-                "id": "31237643",
-                "handicap": -4,
-                "home_tw": "+4 -50",
-                "away_tw": null,
-                "disable": false
-            },
-            "totals": {
-                "id": "34364794",
-                "handicap": 230.5,
-                "over_tw": "230.5",
+                "id": "34456538",
+                "handicap": 218.5,
+                "over_tw": "218.5",
                 "disable": false
             }
         },
         {
             "id": "2119917",
             "scheduled": 1593565200,
+            "scheduled_tw": "2020-07-01T01:00:00.000Z",
             "status": 2,
             "league": "NBA",
             "home": {
+                "team_name": "LAL",
                 "alias": "LAL",
                 "alias_ch": "湖人",
                 "image_id": "3427"
             },
             "away": {
+                "team_name": "HOU",
                 "alias": "HOU",
                 "alias_ch": "火箭",
                 "image_id": "3412"
@@ -251,31 +142,162 @@ module.exports = getMatches;
             }
         },
         {
-            "id": "2120643",
-            "scheduled": 1593563400,
+            "id": "2118817",
+            "scheduled": 1593567000,
+            "scheduled_tw": "2020-07-01T01:30:00.000Z",
             "status": 2,
             "league": "NBA",
             "home": {
-                "alias": "OKC",
-                "alias_ch": "雷霆",
-                "image_id": "3418"
+                "team_name": "SAS",
+                "alias": "SAS",
+                "alias_ch": "馬刺",
+                "image_id": "3429"
             },
             "away": {
-                "alias": "UTA",
-                "alias_ch": "爵士",
-                "image_id": "3434"
+                "team_name": "DAL",
+                "alias": "DAL",
+                "alias_ch": "獨行俠",
+                "image_id": "3411"
             },
             "spread": {
-                "id": "31298793",
-                "handicap": 3.5,
-                "home_tw": "3輸",
+                "id": "31237643",
+                "handicap": -4,
+                "home_tw": "+4 -50",
                 "away_tw": null,
                 "disable": false
             },
             "totals": {
-                "id": "34456538",
-                "handicap": 218.5,
-                "over_tw": "218.5",
+                "id": "34364794",
+                "handicap": 230.5,
+                "over_tw": "230.5",
+                "disable": false
+            }
+        },
+        {
+            "id": "2119439",
+            "scheduled": 1593568800,
+            "scheduled_tw": "2020-07-01T02:00:00.000Z",
+            "status": 2,
+            "league": "NBA",
+            "home": {
+                "team_name": "POR",
+                "alias": "POR",
+                "alias_ch": "拓荒者",
+                "image_id": "3414"
+            },
+            "away": {
+                "team_name": "PHX",
+                "alias": "PHX",
+                "alias_ch": "太陽",
+                "image_id": "3416"
+            },
+            "spread": {
+                "id": "31249410",
+                "handicap": 5.5,
+                "home_tw": "5輸",
+                "away_tw": null,
+                "disable": false
+            },
+            "totals": {
+                "id": "34384039",
+                "handicap": 234,
+                "over_tw": "234",
+                "disable": false
+            }
+        },
+        {
+            "id": "2118816",
+            "scheduled": 1593568800,
+            "scheduled_tw": "2020-07-01T02:00:00.000Z",
+            "status": 2,
+            "league": "NBA",
+            "home": {
+                "team_name": "CHI",
+                "alias": "CHI",
+                "alias_ch": "公牛",
+                "image_id": "3409"
+            },
+            "away": {
+                "team_name": "CLE",
+                "alias": "CLE",
+                "alias_ch": "騎士",
+                "image_id": "3432"
+            },
+            "spread": {
+                "id": "31238069",
+                "handicap": 4,
+                "home_tw": "4平",
+                "away_tw": null,
+                "disable": false
+            },
+            "totals": {
+                "id": "34346860",
+                "handicap": 217.5,
+                "over_tw": "217.5",
+                "disable": false
+            }
+        },
+        {
+            "id": "2118810",
+            "scheduled": 1593572400,
+            "scheduled_tw": "2020-07-01T03:00:00.000Z",
+            "status": 2,
+            "league": "NBA",
+            "home": {
+                "team_name": "SAC",
+                "alias": "SAC",
+                "alias_ch": "國王",
+                "image_id": "3413"
+            },
+            "away": {
+                "team_name": "NOP",
+                "alias": "NOP",
+                "alias_ch": "鵜鶘",
+                "image_id": "5539"
+            },
+            "spread": {
+                "id": "31235573",
+                "handicap": -1.5,
+                "home_tw": null,
+                "away_tw": "1輸",
+                "disable": false
+            },
+            "totals": {
+                "id": "34458529",
+                "handicap": 232,
+                "over_tw": "232",
+                "disable": false
+            }
+        },
+        {
+            "id": "2118809",
+            "scheduled": 1593574200,
+            "scheduled_tw": "2020-07-01T03:30:00.000Z",
+            "status": 2,
+            "league": "NBA",
+            "home": {
+                "team_name": "MEM",
+                "alias": "MEM",
+                "alias_ch": "灰熊",
+                "image_id": "3415"
+            },
+            "away": {
+                "team_name": "ORL",
+                "alias": "ORL",
+                "alias_ch": "魔術",
+                "image_id": "3437"
+            },
+            "spread": {
+                "id": "31236860",
+                "handicap": 2,
+                "home_tw": "2平",
+                "away_tw": null,
+                "disable": false
+            },
+            "totals": {
+                "id": "34364723",
+                "handicap": 218,
+                "over_tw": "218",
                 "disable": false
             }
         }
@@ -284,14 +306,17 @@ module.exports = getMatches;
         {
             "id": "2120647",
             "scheduled": 1593559800,
+            "scheduled_tw": "2020-06-30T23:30:00.000Z",
             "status": 1,
             "league": "NBA",
             "home": {
+                "team_name": "ORL",
                 "alias": "ORL",
                 "alias_ch": "魔術",
                 "image_id": "3437"
             },
             "away": {
+                "team_name": "CHI",
                 "alias": "CHI",
                 "alias_ch": "公牛",
                 "image_id": "3409"
@@ -309,21 +334,56 @@ module.exports = getMatches;
                 "over_tw": "217.5",
                 "disable": true
             }
+        },
+        {
+            "id": "2120646",
+            "scheduled": 1593561600,
+            "scheduled_tw": "2020-07-01T00:00:00.000Z",
+            "status": 1,
+            "league": "NBA",
+            "home": {
+                "team_name": "DAL",
+                "alias": "DAL",
+                "alias_ch": "獨行俠",
+                "image_id": "3411"
+            },
+            "away": {
+                "team_name": "DEN",
+                "alias": "DEN",
+                "alias_ch": "金塊",
+                "image_id": "3417"
+            },
+            "spread": {
+                "id": "31298686",
+                "handicap": -3,
+                "home_tw": null,
+                "away_tw": "3平",
+                "disable": true
+            },
+            "totals": {
+                "id": "34449967",
+                "handicap": 220.5,
+                "over_tw": "220.5",
+                "disable": true
+            }
         }
     ],
     "end": [
         {
             "id": "2118058",
             "scheduled": 1593558000,
+            "scheduled_tw": "2020-06-30T23:00:00.000Z",
             "status": 0,
             "league": "NBA",
             "home": {
+                "team_name": "LAL",
                 "alias": "LAL",
                 "alias_ch": "湖人",
                 "image_id": "3427",
                 "points": 102
             },
             "away": {
+                "team_name": "BKN",
                 "alias": "BKN",
                 "alias_ch": "籃網",
                 "image_id": "3436",
@@ -348,15 +408,18 @@ module.exports = getMatches;
         {
             "id": "2121183",
             "scheduled": 1593558000,
+            "scheduled_tw": "2020-06-30T23:00:00.000Z",
             "status": 0,
             "league": "NBA",
             "home": {
+                "team_name": "DEN",
                 "alias": "DEN",
                 "alias_ch": "老鷹",
                 "image_id": "3417",
                 "points": 131
             },
             "away": {
+                "team_name": "NYK",
                 "alias": "NYK",
                 "alias_ch": "尼克",
                 "image_id": "3421",
