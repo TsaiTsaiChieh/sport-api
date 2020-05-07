b1548659ce4d2579af8c636c110f07b13f402c03
page40316 <page@gets-info.com>
2020-05-07 16:26:20 +0800

    電競足球調整

--- functions/pubsub/insertdbfordy.js
+++ functions/pubsub/insertdbfordy.js
@@ -2,9 +2,15 @@ const modules = require('../util/modules');
 const db = require('../util/dbUtil');
 const settleMatchesModel = require('../model/user/settleMatchesModel');
 // inserttest();
-
 async function inserttest() {
-  const args = { token: { uid: '999' }, bets_id: '2352012' };
+  try{
+  const args = { token: { uid: '999' }, bets_id: '2349871' };
   await settleMatchesModel(args);
+  }catch(err)
+  {
+    console.log(err);
+    
+  }
+  
 }
 module.exports = inserttest;
