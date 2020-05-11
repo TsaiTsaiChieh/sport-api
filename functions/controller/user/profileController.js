a9bb8a4800e1578eceb7c8c40d7caeb6a5de9a9e
d0177381@gmail.com <>
2020-05-11 14:34:08 +0800

    [finish] user profile controller

--- functions/controller/user/profileController.js
+++ functions/controller/user/profileController.js
@@ -1,5 +1,12 @@
 
-function profile() {
-
+const model = require('../../model/user/profileModel');
+// This controller did not have any ajv validation, just pass valid user uid
+async function profile(req, res) {
+  try {
+    res.json(await model(req.token.uid));
+  } catch (err) {
+    console.error('Error in controller/user/profile function by TsaiChieh', err);
+    res.status(err.code).json(err.isPublic ? { error: err.name, devcode: err.status, message: err.message } : err.code);
+  }
 }
 module.exports = profile;
