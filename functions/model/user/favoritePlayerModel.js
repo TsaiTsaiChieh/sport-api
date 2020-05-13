5485ac967902dc9f4566796536197f9c334f57b7
Rex <rex@gets-info.com>
2020-05-13 11:06:59 +0800

    [merge] henry

--- functions/model/user/favoritePlayerModel.js
+++ functions/model/user/favoritePlayerModel.js
@@ -4,11 +4,10 @@ const db = require('../../util/dbUtil');
 function favoritePlayerModel(args) {
   return new Promise(async function(resolve, reject) {
     try {
-      const favorite_player = {};
+      let favorite_player = {};
       const uid = args.token.uid;
-      if(args.method==='POST'){
-      
-      favorite_player = await db.sequelize.query(
+      if (args.method === 'POST') {
+        favorite_player = await db.sequelize.query(
         `
         SELECT uf.god_uid as uid, u.avatar, u.display_name, uf.type, ROUND(uwl.this_month_win_bets, 2) as this_month_win_bets, ROUND(uwl.this_month_win_rate, 2) as this_month_win_rate
           FROM user__favoritegods uf, users__win__lists uwl, users u
@@ -17,12 +16,11 @@ function favoritePlayerModel(args) {
            AND uf.uid=$uid
          `,
         {
-          bind: { uid:uid },
+          bind: { uid: uid },
           type: db.sequelize.QueryTypes.SELECT
         });
-      
-      }else if(args.method==='DELETE'){
-        const god_uid=args.body.god_uid;
+      } else if (args.method === 'DELETE') {
+        const god_uid = args.body.god_uid;
         favorite_player = await db.sequelize.query(
           `
           DELETE FROM user__favoritegods
@@ -30,7 +28,7 @@ function favoritePlayerModel(args) {
              AND uid=$uid
            `,
           {
-            bind: { uid:uid, god_uid:god_uid },
+            bind: { uid: uid, god_uid: god_uid },
             type: db.sequelize.QueryTypes.SELECT
           });
       }
