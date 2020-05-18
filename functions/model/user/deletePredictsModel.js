a28fd5c1a09ffb39207707692e0d6f7b8559fd19
d0177381@gmail.com <>
2020-05-18 15:13:59 +0800

    [format]

--- functions/model/user/deletePredictsModel.js
+++ functions/model/user/deletePredictsModel.js
@@ -202,8 +202,7 @@ function deletePredictionsWhichAreNull(uid, league) {
             AND league_id = :league_id`,
         {
           type: db.sequelize.QueryTypes.DELETE,
-          replacements: { uid, league_id: modules.leagueCodebook(league).id,
-          raw: true
+          replacements: { uid, league_id: modules.leagueCodebook(league).id, raw: true }
         }
       );
       return resolve();
