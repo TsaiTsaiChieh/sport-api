fd3b1a3940599b96e80432ce28bf8cd9ce0f935a
d0177381@gmail.com <>
2020-05-18 11:26:27 +0800

    [pending]

--- functions/controller/livescore/predictionsController.js
+++ functions/controller/livescore/predictionsController.js
@@ -1,3 +1,36 @@
+const modules = require('../../util/modules');
+const model = require('../../model/livescore/predictionsModel');
 
-function predictions(req, res) {}
+async function predictions(req, res) {
+  const schema = {
+    type: 'object',
+    required: ['league'],
+    properties: {
+      league: {
+        type: 'string',
+        enum: modules.acceptLeague
+      }
+    }
+  };
+
+  const valid = modules.ajv.validate(schema, req.query);
+  if (!valid) return res.status(modules.httpStatus).json(modules.ajv.errors);
+
+  // append required parameter to model
+  const args = {
+    uid: req.token.uid,
+    league: req.query.league
+  };
+
+  try {
+    res.json(await model(args));
+  } catch (err) {
+    console.error('Error in controller/livescore/predictions by TsaiChieh', err);
+    res.status(err.code)
+      .json(err.isPublic
+        ? { error: err.name, devcode: err.status, message: err.message }
+        : err.code);
+  }
+}
+
 module.exports = predictions;
--- functions/model/livescore/predictionsModel.js
+++ functions/model/livescore/predictionsModel.js
@@ -1,3 +1,19 @@
+const modules = require('../../util/modules');
+const AppErrors = require('../../util/AppErrors');
+const db = require('../../util/dbUtil');
 
-function predictions(args) {}
+async function predictions(args) {
+  try {
+    const result = await db.sequelize.query(
+      `SELECT *
+         FROM matches
+        WHERE league_id = '${modules.leagueCodebook(args.league).id}'`,
+      {
+        type: db.sequelize.QueryTypes.SELECT
+      });
+    return Promise.resolve(result);
+  } catch (err) {
+    console.log(err);
+  }
+}
 module.exports = predictions;
