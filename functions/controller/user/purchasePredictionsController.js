92004b6309a5394349e23e83ca70f333e44e9536
d0177381@gmail.com <>
2020-05-20 16:24:05 +0800

    [format]

--- functions/controller/user/purchasePredictionsController.js
+++ functions/controller/user/purchasePredictionsController.js
@@ -26,17 +26,12 @@ async function purchasePredictions(req, res) {
 
   const valid = modules.ajv.validate(schema, req.body);
   if (!valid) return res.status(modules.httpStatus.BAD_REQUEST).json(modules.ajv.errors);
-  console.log(valid);
 
   try {
     res.json(await model(req.body));
   } catch (err) {
-    console.error(
-      'Error in controller/user/purchasePredictions by TsaiChieh',
-      err
-    );
-    res
-      .status(err.code)
+    console.error('Error in controller/user/purchasePredictions by TsaiChieh', err);
+    res.status(err.code)
       .json(
         err.isPublic
           ? { error: err.name, devcode: err.status, message: err.message }
