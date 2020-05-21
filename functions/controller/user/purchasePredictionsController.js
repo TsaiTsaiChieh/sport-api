233b8038abe8a279190cad779cb4f7d1eeafc959
d0177381@gmail.com <>
2020-05-21 17:14:05 +0800

    [add] comment in purchasePredictionsController.js

--- functions/controller/user/purchasePredictionsController.js
+++ functions/controller/user/purchasePredictionsController.js
@@ -26,6 +26,7 @@ async function purchasePredictions(req, res) {
 
   const valid = modules.ajv.validate(schema, req.body);
   if (!valid) return res.status(modules.httpStatus.BAD_REQUEST).json(modules.ajv.errors);
+  // append needed params to args
   const args = {
     token: req.token,
     god_uid: req.body.god_uid,
@@ -33,6 +34,7 @@ async function purchasePredictions(req, res) {
     matches_date: req.body.matches_date,
     discount: req.body.discount
   };
+
   try {
     res.json(await model(args));
   } catch (err) {
