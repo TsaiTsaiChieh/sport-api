03b00ae2e3f9eb67ac91fddd2d3cb209732b0abe
d0177381@gmail.com <>
2020-05-21 18:57:05 +0800

    [pending]

--- functions/model/user/purchasePredictionsModel.js
+++ functions/model/user/purchasePredictionsModel.js
@@ -1,7 +1,11 @@
 const modules = require('../../util/modules');
+const dbEngine = require('../../util/databaseEngine');
 const db = require('../../util/dbUtil');
 const AppErrors = require('../../util/AppErrors');
 
-function purchasePredictions(args) {}
+function purchasePredictions(args) {
+// 1. 檢查購買的大神是否有效
+  const godUserData = dbEngine.findUser(args.god_uid);
+}
 
 module.exports = purchasePredictions;
