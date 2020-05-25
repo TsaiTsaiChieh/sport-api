bdd3267c3723f8c0d15ab32d95d5b18c1aa60c9e
page40316 <page@gets-info.com>
2020-05-25 09:49:30 +0800

    mysql 要單引號才會觸發索引

--- functions/model/history/getSeasonDateModel.js
+++ functions/model/history/getSeasonDateModel.js
@@ -3,7 +3,7 @@ const db = require('../../util/dbUtil');
 // const AppErrors = require('../../util/AppErrors');
 
 async function getSeasonDate(args) {
-  return new Promise(async function(resolve, reject) {
+  return new Promise(async function (resolve, reject) {
     try {
       const result = await queryForSeasonDate(args);
       resolve(result);
@@ -14,14 +14,14 @@ async function getSeasonDate(args) {
 }
 
 function queryForSeasonDate(args) {
-  return new Promise(async function(resolve, reject) {
+  return new Promise(async function (resolve, reject) {
     try {
       const queries = await db.sequelize.query(
         // take 169 ms
         `(
           SELECT season.league_name AS league_name, season.start_date AS season_start_date, season.end_date AS season_end_date
             FROM match__seasons AS season
-           WHERE season.league_id = ${modules.leagueCodebook(args.league).id}
+           WHERE season.league_id = '${modules.leagueCodebook(args.league).id}'
          )`,
         {
           type: db.sequelize.QueryTypes.SELECT
