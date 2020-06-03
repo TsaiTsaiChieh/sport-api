ec57180bc36b0d96f3d91d084fbd28ea83f16e17
henry <henry@gets-info.com>
2020-06-03 16:21:22 +0800

    conflict

--- functions/model/user/transferModel.js
+++ functions/model/user/transferModel.js
@@ -1,23 +1,18 @@
 const errs = require('../../util/errorCode');
 const db = require('../../util/dbUtil');
-
+const modules = require('../../util/modules');
 function transferModel(method, args, uid) {
   return new Promise(async function(resolve, reject) {
     try {
-      const begin = args.begin;
-      const end = args.end;
-      const transfer = await db.sequelize.query(
-      `
-        SELECT transfer_id, scheduled, type, title, content 
-          FROM user__transfer__logs 
-          WHERE to_uid = $uid
-            AND updatedAt BETWEEN $begin AND $end
-        `,
-      {
-        bind: { uid: uid, begin: begin, end: end },
-        type: db.sequelize.QueryTypes.SELECT
+      const begin = modules.convertTimezone(args.begin);
+      const end = modules.convertTimezone(args.end);
+      const deposit = db.sequelize.models.cashflow_deposit.findAll({
+        attributes: [
+          'uid','coin','dividend','scheduled'
+        ],
+        raw: true
       });
-      resolve(transfer);
+      resolve(deposit);
     } catch (err) {
       console.log('Error in user/tranfer by henry:  %o', err);
       return reject(errs.errsMsg('500', '500', err.message));
