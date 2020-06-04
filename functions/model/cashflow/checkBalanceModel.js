d3aadec44a099b43a448278fa941828098da45a2
henry <henry@gets-info.com>
2020-06-04 11:57:09 +0800

    country_code add ajv、purse api、checkBalance api、transfer

--- functions/model/cashflow/checkBalanceModel.js
+++ functions/model/cashflow/checkBalanceModel.js
@@ -1,33 +1,37 @@
-function checkBalanceModel(data) {
-    const uid = data.uid;
-    const dividend_cost = data.dividend_cost || 0;
-    const coin_cost = data.coin_cost || 0;
-    const ingot_cost = data.ingot_cost || 0;
-    /*使用者錢包*/
-    const purse = await db.sequelize.query(
-      `SELECT ingot, coin, dividend
-         FROM users
-        WHERE uid = :uid`,
-      {
-        plain:true,
-        replacements: { uid: uid },
-        type: db.sequelize.QueryTypes.SELECT
-      });
-    /*判斷扣除後是否搞錠大於0*/
-    if(purse.ingot-ingot_cost<0){
-      reject({'error':'使用者搞錠不足'});
-      return false;
-    }
-    /*判斷扣除後是否搞幣大於0*/
-    if(purse.coin-coin_cost<0){
-      reject({'error':'使用者搞幣不足'});
-      return false;
-    }
-    /*判斷扣除後是否紅利大於0*/
-    if(purse.dividend-dividend_cost<0){
-      reject({'error':'使用者紅利不足'});
-      return false;
-    }
-  }
+async function checkBalanceModel(data) {
+    return new Promise(async function(resolve, reject) {
+        const uid = data.uid;
+        const dividend_cost = data.dividend_cost || 0;
+        const coin_cost = data.coin_cost || 0;
+        const ingot_cost = data.ingot_cost || 0;
+        /*使用者錢包*/
+        const purse = await db.sequelize.query(
+        `SELECT ingot, coin, dividend
+            FROM users
+            WHERE uid = :uid`,
+        {
+            plain:true,
+            replacements: { uid: uid },
+            type: db.sequelize.QueryTypes.SELECT
+        });
+        /*判斷扣除後是否搞錠大於0*/
+        if(purse.ingot-ingot_cost<0){
+        reject({'error':'使用者搞錠不足'});
+        return false;
+        }
+        /*判斷扣除後是否搞幣大於0*/
+        if(purse.coin-coin_cost<0){
+        reject({'error':'使用者搞幣不足'});
+        return false;
+        }
+        /*判斷扣除後是否紅利大於0*/
+        if(purse.dividend-dividend_cost<0){
+        reject({'error':'使用者紅利不足'});
+        return false;
+        }
+
+        resolve(purse);
+    });
+}
 
 module.exports = checkBalanceModel;
