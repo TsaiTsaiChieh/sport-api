af06258a02698031dac89cfb74cb40eb0896b5fa
ifyu666 <60864122+ifyu666@users.noreply.github.com>
2020-06-05 12:09:21 +0800

    fix 檢舉

--- functions/model/admin/service/dealModel.js
+++ functions/model/admin/service/dealModel.js
@@ -10,6 +10,8 @@ function dbEdit(type, id, insertData) {
           }
         });
         record.update(insertData);
+      } else if (type === 'blog') {
+        await db.sequelize.models.user__blocklog.create(insertData);
       } else if (type === 'topic') {
         const record = await db.sequelize.models.topic__article.findOne({
           where: {
@@ -64,33 +66,49 @@ async function model(args) {
         if (user.block_count === 0) block_days = 1;
         else if (user.block_count === 1) block_days = 3;
         else if (user.block_count === 2) block_days = 7;
-        else if (user.block_count === 3) block_days = 9999;
+        else if (user.block_count >= 3) block_days = 9999;
 
         const d = new Date();
         let new_blockdate = d.setDate(d.getDate() + block_days);
         new_blockdate = new Date(new_blockdate).toISOString();
+        const datenow = new Date().toISOString();
 
         const new_blockcount = user.block_count + 1;
         await dbEdit('user', args.uid, {
           block_count: new_blockcount,
           block_message: new_blockdate
         });
+        await dbEdit('blog', args.uid, {
+          uid: args.uid,
+          newcount: new_blockcount,
+          start: datenow,
+          end: new_blockdate
+        });
       }
       if (args.article_status) {
         if (args.type === 'topic') {
           await dbEdit('topic', args.article_id, {
-            status: args.article_status,
+            status: args.article_status
+          });
+        } else if (args.type === 'reply') {
+          await dbEdit('reply', args.article_id, {
+            status: args.article_status
+          });
+        }
+      }
+      if (args.article_status) {
+        if (args.type === 'topic') {
+          await dbEdit('topic', args.article_id, {
             delete_reason: args.reply
           });
         } else if (args.type === 'reply') {
           await dbEdit('reply', args.article_id, {
-            status: args.article_status,
             delete_reason: args.reply
           });
         }
       }
       await dbEdit('report', args.report_id, {
-        status: args.status
+        reply: args.reply
       });
 
       resolve({ code: 200 });
