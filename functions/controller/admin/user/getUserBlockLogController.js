cf1217d7462d26c7f1474ecd47ffaaa4ff72f32f
ifyu666 <60864122+ifyu666@users.noreply.github.com>
2020-06-05 12:10:01 +0800

    block log

---
+++ functions/controller/admin/user/getUserBlockLogController.js
@@ -0,0 +1,35 @@
+/* eslint-disable promise/always-return */
+const modules = require('../../../util/modules');
+const model = require('../../../model/admin/user/getUserBlockLogModel');
+async function controller(req, res) {
+  const schema = {
+    type: 'object',
+    properties: {
+      uid: {
+        type: 'string'
+      }
+    },
+    required: ['uid']
+  };
+  const valid = modules.ajv.validate(schema, req.body);
+  if (!valid) {
+    console.log(modules.ajv.errors);
+    const ajv_errs = [];
+    for (let i = 0; i < modules.ajv.errors.length; i++) {
+      ajv_errs.push('path: \'' + modules.ajv.errors[i].dataPath + '\': ' + modules.ajv.errors[i].message);
+    }
+    res.status(400).json({ code: 400, error: 'schema not acceptable', message: ajv_errs });
+    return;
+  }
+  // req.body.token = req.token;
+  const args = req.body;
+
+  model(args)
+    .then(function(body) {
+      res.json(body);
+    })
+    .catch(function(err) {
+      res.status(err.code).json(err);
+    });
+}
+module.exports = controller;
