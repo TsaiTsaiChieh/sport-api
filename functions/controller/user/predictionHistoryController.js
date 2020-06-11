804a9fa44cf781ec138245a50795cd10b662973e
d0177381@gmail.com <>
2020-06-11 12:14:59 +0800

    [add] controller & model in prediction_history API

---
+++ functions/controller/user/predictionHistoryController.js
@@ -0,0 +1,6 @@
+const modules = require('../../util/modules');
+const model = require('../../model/user/purchasePredictionsModel');
+
+async function predictionHistory(req, res) {}
+
+module.exports = predictionHistory;
---
+++ functions/model/user/predictionHistoryModel.js
@@ -0,0 +1,7 @@
+const modules = require('../../util/modules');
+const db = require('../../util/dbUtil');
+const AppErrors = require('../../util/AppErrors');
+
+function predictionHistory(args) {}
+
+module.exports = predictionHistory;
