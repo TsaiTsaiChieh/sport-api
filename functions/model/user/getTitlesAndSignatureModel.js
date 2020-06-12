ed6206e3d6be28a69a3d0b8324cb5f26c455fec2
d0177381@gmail.com <>
2020-06-12 18:28:44 +0800

    [pending]

--- functions/model/user/getTitlesAndSignatureModel.js
+++ functions/model/user/getTitlesAndSignatureModel.js
@@ -1,51 +1,53 @@
 const modules = require('../../util/modules');
+const db = require('../../util/dbUtil');
 const dbEngine = require('../../util/databaseEngine');
+const AppErrors = require('../../util/AppErrors');
+const GOD_STATUS = 2;
+
 function getTitlesAndSignature(args) {
   return new Promise(async function(resolve, reject) {
     try {
       // check if user exists
-      const customClaims = await dbEngine.findUser(args.uid);
-      const result = await getUserTitles(args.uid, customClaims);
-      return resolve(result);
+      let titles = [];
+      const userData = await dbEngine.findUser(args.uid);
+      titles = await getGodAllTitles(args, userData);
+      return resolve(repackageReturnData(userData, titles));
     } catch (err) {
       return reject({ code: err.code, error: err });
     }
   });
 }
 
-async function getUserTitles(uid, customClaims) {
-  const userSnapshot = await modules.getSnapshot('users', uid);
-  const user = userSnapshot.data();
-
-  const titles = user.titles ? user.titles : [];
-  const record = await getTitleRecord(user.uid);
-  return {
-    uid: user.uid,
-    signature: user.signature,
-    role: modules.userStatusCodebook(customClaims.role),
-    titles: titles,
-    record
-  };
+function getGodAllTitles(args, userData) {
+  return new Promise(async function(resolve, reject) {
+    try {
+      if (userData.status !== GOD_STATUS) return resolve([]);
+      if (userData.status === GOD_STATUS) {
+        const { period } = modules.getTitlesPeriod(args.now);
+        // index is index_merge(titles), taking 170ms
+        const result = await db.Title.findAll({
+          where: { uid: args.uid, period },
+          attributes: ['league_id', 'rank_id'],
+          raw: true
+        });
+        return resolve(result);
+      }
+    } catch (err) {
+      return reject(new AppErrors.MysqlError(`${err.stack} by Tsai-Chieh`));
+    }
+  });
 }
 
-async function getTitleRecord(uid) {
-  const usersTitlesSnapshot = await modules.getSnapshot('users_titles', uid);
-  if (!usersTitlesSnapshot.exists) {
-    return {
-      rank1_count: 0,
-      rank2_count: 0,
-      rank3_count: 0,
-      rank4_count: 0
+function repackageReturnData(userData, titles) {
+  try {
+    const data = {
+      signature: userData.signature
+
     };
-  }
-  if (usersTitlesSnapshot.exists) {
-    const record = usersTitlesSnapshot.data();
-    return {
-      rank1_count: record.rank1_count,
-      rank2_count: record.rank2_count,
-      rank3_count: record.rank3_count,
-      rank4_count: record.rank4_count
-    };
+  } catch (err) {
+    console.error(`${err.stack} by TsaiChieh`);
+    throw AppErrors.RepackageError(`${err.stack} by TsaiChieh`);
   }
 }
+
 module.exports = getTitlesAndSignature;
