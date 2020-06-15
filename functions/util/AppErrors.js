c4fc9e9311ad5e0068cbb4114cae50b21d7f22ad
d0177381@gmail.com <>
2020-06-15 11:27:14 +0800

    [add] PredictionHistoryError in util/AppErrors.js

--- functions/util/AppErrors.js
+++ functions/util/AppErrors.js
@@ -425,7 +425,7 @@ class CreateCashflowBuyRollback extends ExtendableError {
   }
 }
 
-class PredictionHistory extends ExtendableError {
+class PredictionHistoryError extends ExtendableError {
   constructor(
     message = '歷史紀錄 API 錯誤',
     status = 1521,
@@ -475,5 +475,5 @@ module.exports = {
   CreateUserBuysTableRollback,
   UpdateUserCoinORDividendRollback,
   CreateCashflowBuyRollback,
-  PredictionHistory
+  PredictionHistoryError
 };
