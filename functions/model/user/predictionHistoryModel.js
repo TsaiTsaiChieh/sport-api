346386bb822390eada3def48f1046221f880bf03
d0177381@gmail.com <>
2020-06-15 18:16:54 +0800

    [pending]

--- functions/model/user/predictionHistoryModel.js
+++ functions/model/user/predictionHistoryModel.js
@@ -1,7 +1,17 @@
 const modules = require('../../util/modules');
 const db = require('../../util/dbUtil');
+const dbEngine = require('../../util/databaseEngine');
 const AppErrors = require('../../util/AppErrors');
 
-function predictionHistory(args) {}
+/*
+* 1. 檢查該使用者存在與否
+* 2.
+*/
+
+async function predictionHistory(args) {
+  const [err, userData] = await modules.to(dbEngine.findUser(args.uid));
+  if (err) throw new AppErrors.PredictionHistoryError(err.stack, err.status);
+  // return Promise.reject(new AppErrors.PredictionHistoryError(err.stack, err.status));
+}
 
 module.exports = predictionHistory;
