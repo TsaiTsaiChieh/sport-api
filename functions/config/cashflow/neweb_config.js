b093a29d195665996f200ee49665f86cbff35ce7
henry <henry@gets-info.com>
2020-06-17 17:46:51 +0800

    neweb config

--- functions/config/cashflow/neweb_config.js
+++ functions/config/cashflow/neweb_config.js
@@ -4,11 +4,12 @@ const hashIV     = "C454ZaIyVYTvWLcP"; 																					//HashIV
 const url        = "https://ccore.newebpay.com/MPG/mpg_gateway"; //測試環境URL
 const ver        = "1.5";
 
-const ReturnURL       = "/cashflow_neweb/thanks.html"; 	     //支付完成 返回商店網址
-const NotifyURL_atm   = "/cashflow_neweb/atm_notify"; 		//ATM   支付通知網址
-const NotifyURL_webatm   = "/cashflow_neweb/webatm_notify"; 		//WEBATM   支付通知網址
-const NotifyURL_ccard = "/cashflow_neweb/ccard_notify"; 	    //信用卡 支付通知網址
-const ClientBackURL   = "/cashflow_neweb/"; 					//支付取消 返回商店網址
+const test_url = 'https://dosports.web.app/';
+const ReturnURL          = testurl+"/cashflow_neweb/thanks.html"; 	     //支付完成 返回商店網址
+const NotifyURL_atm      = testurl+"/cashflow_neweb/atm_notify"; 		//ATM   支付通知網址
+const NotifyURL_webatm   = testurl+"/cashflow_neweb/webatm_notify"; 		//WEBATM   支付通知網址
+const NotifyURL_ccard    = testurl+"/cashflow_neweb/ccard_notify"; 	    //信用卡 支付通知網址
+const ClientBackURL      = testurl+"/cashflow_neweb/"; 					//支付取消 返回商店網址
 
 const ATM_ExpireDate = 3;						//ATM付款到期日
 
