91a555261f181dedb96203a6c2de2c0bc3079b61
Rex <rex@gets-info.com>
2020-06-19 12:11:37 +0800

    [merge] henry

--- functions/config/cashflow/neweb_config.js
+++ functions/config/cashflow/neweb_config.js
@@ -1,34 +1,34 @@
-const merchantID = "MS311997730"; 																					//商店代號
-const hashKey    = "AlrsIeyCp6BKNn9pDGcSO1lxtm8T90KF"; 																					//HashKey
-const hashIV     = "C454ZaIyVYTvWLcP"; 																					//HashIV
-const url        = "https://ccore.newebpay.com/MPG/mpg_gateway"; //測試環境URL
-const ver        = "1.5";
+const merchantID = 'MS311997730'; 																					// 商店代號
+const hashKey = 'AlrsIeyCp6BKNn9pDGcSO1lxtm8T90KF'; 																					// HashKey
+const hashIV = 'C454ZaIyVYTvWLcP'; 																					// HashIV
+const url = 'https://ccore.newebpay.com/MPG/mpg_gateway'; // 測試環境URL
+const ver = '1.5';
 
-const test_url           = 'https://dosports.web.app/';
-const ReturnURL          = test_url+"/cashflow_neweb/thanks"; 	    //支付完成 返回商店網址
-const NotifyURL_atm      = test_url+"/cashflow_neweb/atm_notify"; 		//ATM   支付通知網址
-const NotifyURL_webatm   = test_url+"/cashflow_neweb/webatm_notify";    //WEBATM   支付通知網址
-const NotifyURL_barcode  = test_url+"/cashflow_neweb/barcode_notify"; 	//BARCODE   支付通知網址
-const NotifyURL_code     = test_url+"/cashflow_neweb/code_notify"; 		//CODE   支付通知網址
-const NotifyURL_ccard    = test_url+"/cashflow_neweb/ccard_notify"; 	//信用卡 支付通知網址
-const NotifyMpgURL       = test_url+"/cashflow_neweb/mpg_notify";     //MPG notify url
-const ClientBackURL      = test_url+"/get-points/"; 					//支付取消 返回商店網址
+const test_url = 'https://dosports.web.app/';
+const ReturnURL = test_url + '/cashflow_neweb/thanks'; // 支付完成 返回商店網址
+const NotifyURL_atm = test_url + '/cashflow_neweb/atm_notify'; 		// ATM   支付通知網址
+const NotifyURL_webatm = test_url + '/cashflow_neweb/webatm_notify'; // WEBATM   支付通知網址
+const NotifyURL_barcode = test_url + '/cashflow_neweb/barcode_notify'; 	// BARCODE   支付通知網址
+const NotifyURL_code = test_url + '/cashflow_neweb/code_notify'; 		// CODE   支付通知網址
+const NotifyURL_ccard = test_url + '/cashflow_neweb/ccard_notify'; 	// 信用卡 支付通知網址
+const NotifyMpgURL = test_url + '/cashflow_neweb/mpg_notify'; // MPG notify url
+const ClientBackURL = test_url + '/get-points/'; 					// 支付取消 返回商店網址
 
-const ATM_ExpireDate = 10;						                        //ATM付款到期日
+const ATM_ExpireDate = 10; // ATM付款到期日
 
 module.exports = {
-    merchantID,
-    hashKey,
-    hashIV,
-    url,
-    ver,
-    ReturnURL,
-    NotifyURL_atm,
-    NotifyURL_webatm,
-    NotifyURL_barcode,
-    NotifyURL_code,
-    NotifyURL_ccard,
-    NotifyMpgURL,
-    ClientBackURL,
-    ATM_ExpireDate
-}
+  merchantID,
+  hashKey,
+  hashIV,
+  url,
+  ver,
+  ReturnURL,
+  NotifyURL_atm,
+  NotifyURL_webatm,
+  NotifyURL_barcode,
+  NotifyURL_code,
+  NotifyURL_ccard,
+  NotifyMpgURL,
+  ClientBackURL,
+  ATM_ExpireDate
+};
--- functions/util/cashflow_sdk/neweb_sdk.js
+++ functions/util/cashflow_sdk/neweb_sdk.js
@@ -1,24 +1,20 @@
 const moment = require('moment');
 const httpBuildQuery = require('http-build-query');
 const CryptoJS = require('crypto-js');
-const crypto   = require('crypto');
-/*HashKey AES 加解密 */
-function create_mpg_aes_encrypt (parameter = "" , key = "", iv = "") {
-    
-  
-  
-  
+const crypto = require('crypto');
+/* HashKey AES 加解密 */
+function create_mpg_aes_encrypt(parameter = '', key = '', iv = '') {
   return_str = '';
-    if (parameter!=='') {
-    //將參數經過 URL ENCODED QUERY STRING
+  if (parameter !== '') {
+    // 將參數經過 URL ENCODED QUERY STRING
     return_str = httpBuildQuery(parameter);
-    }
+  }
 
-    let cipher = CryptoJS.AES.encrypt(return_str, CryptoJS.enc.Utf8.parse(key), {
-        iv: CryptoJS.enc.Utf8.parse(iv), // parse the IV 
-        padding: CryptoJS.pad.Pkcs7,
-        mode: CryptoJS.mode.CBC
-    });
+  const cipher = CryptoJS.AES.encrypt(return_str, CryptoJS.enc.Utf8.parse(key), {
+    iv: CryptoJS.enc.Utf8.parse(iv), // parse the IV
+    padding: CryptoJS.pad.Pkcs7,
+    mode: CryptoJS.mode.CBC
+  });
     // console.log(CryptoJS.enc.Utf8.parse(iv));
     // var encryptedData = CryptoJS.AES.encrypt(return_str, key, {
     //     mode: CryptoJS.mode.CBC,
@@ -28,11 +24,11 @@ function create_mpg_aes_encrypt (parameter = "" , key = "", iv = "") {
     // let cipher = encrypt(return_str, key, iv);
     // console.log(decrypt(cipher, key, iv));
 
-    // console.log(enc);
-    // return bin2hexJS(openssl_encrypt(addpadding(return_str), 'aes-256-cbc', key, OPENSSL_RAW_DATA|OPENSSL_ZERO_PADDING, iv)).trim();
-    // console.log(cipher);
-    // return;
-    return cipher.ciphertext;
+  // console.log(enc);
+  // return bin2hexJS(openssl_encrypt(addpadding(return_str), 'aes-256-cbc', key, OPENSSL_RAW_DATA|OPENSSL_ZERO_PADDING, iv)).trim();
+  // console.log(cipher);
+  // return;
+  return cipher.ciphertext;
 }
 
 // /*HashKey AES 解密 */
@@ -52,124 +48,119 @@ function create_mpg_aes_encrypt (parameter = "" , key = "", iv = "") {
 //  }
 // }
 
-function create_mpg_aes_decrypt(encryptedBase64Str, key, iv){
-
-  let cipher = CryptoJS.AES.decrypt(encryptedBase64Str, CryptoJS.enc.Utf8.parse(key), {
-      iv: CryptoJS.enc.Utf8.parse(iv), // parse the IV 
-      padding: CryptoJS.pad.Pkcs7,
-      mode: CryptoJS.mode.CBC
+function create_mpg_aes_decrypt(encryptedBase64Str, key, iv) {
+  const cipher = CryptoJS.AES.decrypt(encryptedBase64Str, CryptoJS.enc.Utf8.parse(key), {
+    iv: CryptoJS.enc.Utf8.parse(iv), // parse the IV
+    padding: CryptoJS.pad.Pkcs7,
+    mode: CryptoJS.mode.CBC
   });
 
   return cipher;
 }
 
 function encrypt2(text) {
-    let iv = crypto.randomBytes(IV_LENGTH);
-    let cipher = crypto.createCipheriv('aes-256-cbc', new Buffer(SECRET), iv);
-    let encrypted = cipher.update(text);
-    
-    encrypted = Buffer.concat([encrypted, cipher.final()]);
-  
-    return iv.toString('hex') + ':' + encrypted.toString('hex');
-  }
-function encrypt(val, ENC_KEY, IV){
-    let cipher = crypto.createCipheriv('aes-256-cbc', ENC_KEY, IV);
-    let encrypted = cipher.update(val, 'utf8', 'base64');
-    encrypted += cipher.final('base64');
-    return encrypted;
-  };
+  const iv = crypto.randomBytes(IV_LENGTH);
+  const cipher = crypto.createCipheriv('aes-256-cbc', new Buffer(SECRET), iv);
+  let encrypted = cipher.update(text);
 
-function decrypt(val, ENC_KEY, IV){
-    let decipher = crypto.createDecipheriv('aes-256-cbc', ENC_KEY, IV);
-    let decrypted = decipher.update(val, 'base64', 'utf8');
-    return (decrypted + decipher.final('utf8'));
-  };
+  encrypted = Buffer.concat([encrypted, cipher.final()]);
+
+  return iv.toString('hex') + ':' + encrypted.toString('hex');
+}
+function encrypt(val, ENC_KEY, IV) {
+  const cipher = crypto.createCipheriv('aes-256-cbc', ENC_KEY, IV);
+  let encrypted = cipher.update(val, 'utf8', 'base64');
+  encrypted += cipher.final('base64');
+  return encrypted;
+};
+
+function decrypt(val, ENC_KEY, IV) {
+  const decipher = crypto.createDecipheriv('aes-256-cbc', ENC_KEY, IV);
+  const decrypted = decipher.update(val, 'base64', 'utf8');
+  return (decrypted + decipher.final('utf8'));
+};
 
 function addpadding(string, blocksize = 32) {
- const len = string.length;
- const pad = blocksize - (len % blocksize);
- string += (pad.charCodeAt).repeat(pad);
+  const len = string.length;
+  const pad = blocksize - (len % blocksize);
+  string += (pad.charCodeAt).repeat(pad);
 
- return string;
+  return string;
 }
 
+function bin2hexJS(s) {
+  //  discuss at: https://locutus.io/php/bin2hex/
+  // original by: Kevin van Zonneveld (https://kvz.io)
+  // bugfixed by: Onno Marsman (https://twitter.com/onnomarsman)
+  // bugfixed by: Linuxworld
+  // improved by: ntoniazzi (https://locutus.io/php/bin2hex:361#comment_177616)
+  //   example 1: bin2hex('Kev')
+  //   returns 1: '4b6576'
+  //   example 2: bin2hex(String.fromCharCode(0x00))
+  //   returns 2: '00'
 
-function bin2hexJS (s) {
-    //  discuss at: https://locutus.io/php/bin2hex/
-    // original by: Kevin van Zonneveld (https://kvz.io)
-    // bugfixed by: Onno Marsman (https://twitter.com/onnomarsman)
-    // bugfixed by: Linuxworld
-    // improved by: ntoniazzi (https://locutus.io/php/bin2hex:361#comment_177616)
-    //   example 1: bin2hex('Kev')
-    //   returns 1: '4b6576'
-    //   example 2: bin2hex(String.fromCharCode(0x00))
-    //   returns 2: '00'
-  
-    var i
-    var l
-    var o = ''
-    var n
-  
-    s += ''
-  
-    for (i = 0, l = s.length; i < l; i++) {
-      n = s.charCodeAt(i)
-        .toString(16)
-      o += n.length < 2 ? '0' + n : n
-    }
-  
-    return o
+  var i;
+  var l;
+  var o = '';
+  var n;
+
+  s += '';
+
+  for (i = 0, l = s.length; i < l; i++) {
+    n = s.charCodeAt(i)
+      .toString(16);
+    o += n.length < 2 ? '0' + n : n;
   }
 
-
-/*HashIV SHA256 加密*/
-function SHA_str(key="", tradeinfo="", iv=""){
-	const HashIV_Key = "HashKey=" + key + "&" + tradeinfo + "&HashIV=" + iv;
-
-	return HashIV_Key;
+  return o;
 }
 
-function SHA256(str){
-    const sha = CryptoJS.SHA256(str)
-    return sha;
+/* HashIV SHA256 加密 */
+function SHA_str(key = '', tradeinfo = '', iv = '') {
+  const HashIV_Key = 'HashKey=' + key + '&' + tradeinfo + '&HashIV=' + iv;
+
+  return HashIV_Key;
 }
 
-function CheckOut(URL="", MerchantID="", TradeInfo="", SHA256="", VER="") {
-	let szHtml = '<!doctype html>';
-	szHtml +='<html>';
-	szHtml +='<head>';
-	szHtml +='<meta charset="utf-8">';
-	szHtml +='</head>';
-	szHtml +='<body>';
-	szHtml +='<form name="newebpay" id="newebpay" method="post" action="' + URL + '" style="display:none;">';
-	szHtml +='<input type="text" name="MerchantID" value="' + MerchantID + '" type="hidden">';
-	szHtml +='<input type="text" name="TradeInfo" value="' + TradeInfo + '"   type="hidden">';
-	szHtml +='<input type="text" name="TradeSha" value="' + SHA256 + '" type="hidden">';
-	szHtml +='<input type="text" name="Version"  value="' + VER + '" type="hidden">';
-	szHtml +='</form>';
-	szHtml +='<script type="text/javascript">';
-	szHtml +='document.getElementById("newebpay").submit();';
-	szHtml +='</script>';
-	szHtml +='</body>';
-    szHtml +='</html>';
-    console.log(szHtml);
-    
-    return szHtml;
-}
-/*取得訂單編號*/
-function getOrderNo(){
-    /*需加入預設時區*/
-    const serial_number = moment().format('YYYYMMDDHHMMSS');
-	return serial_number;
+function SHA256(str) {
+  const sha = CryptoJS.SHA256(str);
+  return sha;
 }
 
+function CheckOut(URL = '', MerchantID = '', TradeInfo = '', SHA256 = '', VER = '') {
+  let szHtml = '<!doctype html>';
+  szHtml += '<html>';
+  szHtml += '<head>';
+  szHtml += '<meta charset="utf-8">';
+  szHtml += '</head>';
+  szHtml += '<body>';
+  szHtml += '<form name="newebpay" id="newebpay" method="post" action="' + URL + '" style="display:none;">';
+  szHtml += '<input type="text" name="MerchantID" value="' + MerchantID + '" type="hidden">';
+  szHtml += '<input type="text" name="TradeInfo" value="' + TradeInfo + '"   type="hidden">';
+  szHtml += '<input type="text" name="TradeSha" value="' + SHA256 + '" type="hidden">';
+  szHtml += '<input type="text" name="Version"  value="' + VER + '" type="hidden">';
+  szHtml += '</form>';
+  szHtml += '<script type="text/javascript">';
+  szHtml += 'document.getElementById("newebpay").submit();';
+  szHtml += '</script>';
+  szHtml += '</body>';
+  szHtml += '</html>';
+  console.log(szHtml);
 
+  return szHtml;
+}
+/* 取得訂單編號 */
+function getOrderNo() {
+  /* 需加入預設時區 */
+  const serial_number = moment().format('YYYYMMDDHHMMSS');
+  return serial_number;
+}
 
 module.exports = {
-    getOrderNo,
-    create_mpg_aes_encrypt,
-    create_mpg_aes_decrypt,
-    SHA_str,
-    SHA256,
-    CheckOut
-}
+  getOrderNo,
+  create_mpg_aes_encrypt,
+  create_mpg_aes_decrypt,
+  SHA_str,
+  SHA256,
+  CheckOut
+};
