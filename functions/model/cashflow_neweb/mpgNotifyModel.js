05ade55436148c1fe7e5a0a7397529784fb67263
Rex <rex@gets-info.com>
2020-06-19 17:49:39 +0800

    [merge] henry

--- functions/model/cashflow_neweb/mpgNotifyModel.js
+++ functions/model/cashflow_neweb/mpgNotifyModel.js
@@ -3,18 +3,18 @@ const db = require('../../util/dbUtil');
 async function mpgNotifyModel(args) {
   return new Promise(async function(resolve, reject) {
     try {
-      // const ret = JSON.stringify(args);
+      const ret = args;
       const expire = await db.sequelize.query(
         `
         INSERT cashflow_deposits (uid, money, money_real, coin, coin_real, dividend, dividend_real, serial_number) VALUES ('33333', 3, 3, 3, 3, 1, 1, :ret)
         `,
         {
           logging: true,
-          replacements: 
+          replacements:
           {
             ret: ret
           },
-            type: db.sequelize.QueryTypes.INSERT
+          type: db.sequelize.QueryTypes.INSERT
         });
       resolve(expire);
     } catch (err) {
