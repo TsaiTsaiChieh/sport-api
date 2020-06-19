a0b634eecf5556f6acc5d52ff854fd2b8cb7282d
henry <henry@gets-info.com>
2020-06-19 17:23:43 +0800

    test

--- functions/model/cashflow_neweb/mpgNotifyModel.js
+++ functions/model/cashflow_neweb/mpgNotifyModel.js
@@ -3,7 +3,7 @@ const db = require('../../util/dbUtil');
 async function mpgNotifyModel(args) {
   return new Promise(async function(resolve, reject) {
     try {
-      const ret = JSON.stringify(args);
+      // const ret = JSON.stringify(args);
       const expire = await db.sequelize.query(
         `
         INSERT cashflow_deposits (uid, money, money_real, coin, coin_real, dividend, dividend_real, serial_number) VALUES ('33333', 3, 3, 3, 3, 1, 1, :ret)
