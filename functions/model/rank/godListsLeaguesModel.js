5eee258e7933594af3b4f0ff2f17fc2a224f5e54
Alan <alan@Alans-MacBook-Pro.local>
2020-06-29 15:51:33 +0800

    修改 去掉無用參數，除移 prmoise

--- functions/model/rank/godListsLeaguesModel.js
+++ functions/model/rank/godListsLeaguesModel.js
@@ -1,37 +1,35 @@
-const modules = require('../../util/modules');
+const { getTitlesPeriod } = require('../../util/modules');
 const errs = require('../../util/errorCode');
 const db = require('../../util/dbUtil');
 
-function godlistsLeagues(args) {
-  return new Promise(async function(resolve, reject) {
-    const leagueLists = [];
-    // 取得當期期數
-    const period = modules.getTitlesPeriod(new Date()).period;
+async function godlistsLeagues() {
+  const leagueLists = [];
+  // 取得當期期數
+  const period = getTitlesPeriod(new Date()).period;
 
-    try {
-      // 依 聯盟 取出是 大神資料 和 大神賣牌狀態 sell (-1：無狀態  0：免費  1：賣牌)
-      const LeagueListsQuery = await db.sequelize.query(`
+  try {
+    // 依 聯盟 取出是 大神資料 和 大神賣牌狀態 sell (-1：無狀態  0：免費  1：賣牌)
+    const LeagueListsQuery = await db.sequelize.query(`
         select distinct leagues.name
           from titles, view__leagues leagues
          where titles.league_id = leagues.league_id
            and titles.period = :period
       `, {
-        replacements: {
-          period: period
-        },
-        type: db.sequelize.QueryTypes.SELECT
-      });
+      replacements: {
+        period: period
+      },
+      type: db.sequelize.QueryTypes.SELECT
+    });
 
-      LeagueListsQuery.forEach(function(data) {
-        leagueLists.push(data.name);
-      });
-    } catch (err) {
-      console.log('Error in  rank/godlists by YuHsien:  %o', err);
-      return reject(errs.errsMsg('500', '500', err.message));
-    }
+    LeagueListsQuery.forEach(function(data) {
+      leagueLists.push(data.name);
+    });
+  } catch (err) {
+    console.log('Error in  rank/godlists by YuHsien:  %o', err);
+    throw errs.errsMsg('500', '500', err.message);
+  }
 
-    resolve(leagueLists);
-  });
+  return leagueLists;
 }
 
 module.exports = godlistsLeagues;
