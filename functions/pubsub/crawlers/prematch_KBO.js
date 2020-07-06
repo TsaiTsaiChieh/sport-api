c1642ebbcb92fda266fb66cbdd0743b8cd4246fe
d0177381@gmail.com <>
2020-07-06 17:46:13 +0800

    [not finish] in pubsub/crawlers/prematch_KBO.js

--- functions/pubsub/crawlers/prematch_KBO.js
+++ functions/pubsub/crawlers/prematch_KBO.js
@@ -1,5 +1,6 @@
 const modules = require('../../util/modules');
-const { KBO } = require('../util/prematchFunctions_KBO');
+const AppErrors = require('../../util/AppErrors');
+// const { KBO } = require('../util/prematchFunctions_KBO');
 const KBO_URL = 'https://mykbostats.com/';
 
 // 1. 取得各隊伍的資訊
@@ -9,9 +10,25 @@ async function prematch_KBO(req, res) {
 
 function getTeamsStandings() {
   return new Promise(async function(resolve, reject) {
-    const { data } = await modules.axios.get(KBO_URL);
-    const $ = modules.cheerio.load(data);
-    console.log($);
+    try {
+      const { data } = await modules.axios.get(KBO_URL);
+      const $ = modules.cheerio.load(data); // load in the HTML
+      // const teamTable = $('#tbl-teams').children()[0]; // team table unique id
+      // const a = $('thread');
+
+      // const a = $().attr('name'); // div[class^="test"]
+      console.log(a.children().length);
+
+      // console.log(a.text(), '===');
+
+      // console.log('--', teamTable.get(0).thread.text());
+      // console.log(teamTableTitles);
+      // console.log(teamTableTitles.text());
+      // console.log(teamTableTitles.length);
+    } catch (err) {
+      console.error(err, '=-----');
+      return reject(new AppErrors.CrawlersError(`${err.stack} by TsaiChieh`));
+    }
   });
 }
 module.exports = prematch_KBO;
