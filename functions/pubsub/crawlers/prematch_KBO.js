8b25eadc0a8c07871276af3f23362105e9200f0e
d0177381@gmail.com <>
2020-07-06 15:41:39 +0800

    [not finish] in pubsub/crawlers/prematch_KBO.js

--- functions/pubsub/crawlers/prematch_KBO.js
+++ functions/pubsub/crawlers/prematch_KBO.js
@@ -1,7 +1,17 @@
 const modules = require('../../util/modules');
+const { KBO } = require('../util/prematchFunctions_KBO');
+const KBO_URL = 'https://mykbostats.com/';
 
-function test(req, res) {
-  res.json('ok');
+// 1. 取得各隊伍的資訊
+async function prematch_KBO(req, res) {
+  const d = await getTeamsStandings();
 }
 
-module.exports = test;
+function getTeamsStandings() {
+  return new Promise(async function(resolve, reject) {
+    const { data } = await modules.axios.get(KBO_URL);
+    const $ = modules.cheerio.load(data);
+    console.log($);
+  });
+}
+module.exports = prematch_KBO;
