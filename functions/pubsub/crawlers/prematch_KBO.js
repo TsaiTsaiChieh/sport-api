5f38f23081f155eb1f77527c88e76dcc3df29e5e
d0177381@gmail.com <>
2020-07-09 12:21:45 +0800

    [not finsih all]

--- functions/pubsub/crawlers/prematch_KBO.js
+++ functions/pubsub/crawlers/prematch_KBO.js
@@ -6,7 +6,8 @@ const teamTableFieldCount = teamTableTitles.length;
 
 // 1. 取得各隊伍的資訊
 async function prematch_KBO(req, res) {
-  const d = await getTeamsStandings();
+  // const d = await getTeamsStandings();
+  res.json(await getTeamsStandings());
 }
 
 function getTeamsStandings() {
@@ -14,7 +15,8 @@ function getTeamsStandings() {
     try {
       const { data } = await modules.axios.get(KBO_URL);
       const $ = modules.cheerio.load(data); // load in the HTML
-      const a = await getTeamsStats($);
+
+      return resolve(await getTeamsStats($));
     } catch (err) {
       console.error(err, '=-----');
       return reject(new AppErrors.CrawlersError(`${err.stack} by TsaiChieh`));
@@ -35,18 +37,19 @@ function getTeamsStats($) {
           tdArray[i] = removeLineBreaks;
         } else tdArray[i] = $(this).text();
       });
-      const a = decompose_STRK_LAST(tdArray);
-      return resolve(repackageTeamStats(tdArray));
+      const data = decompose_STRK_LAST(tdArray);
+      return resolve(repackageTeamStats(data));
     } catch (err) {
       return reject(new AppErrors.CrawlersError(`${err.stack} by TsaiChieh`));
     }
   });
 }
 
+// ex: 將 3W / 7W 3L 0D 拆成 3W 和 7W 3L 0D (連贏連敗/近十場)
 function decompose_STRK_LAST(tdArray) {
-  const temp = [...tdArray]; // deep copy
-  // const temp = tdArray;
-  const L10_Array = [];
+  const temp = [...tdArray]; // Deep copy
+
+  let L10_Array = [];
   for (let i = 0; i < tdArray.length; i++) {
     for (let j = 0; j < tdArray.length; j = j + 7) {
       if (i === 6) {
@@ -59,39 +62,51 @@ function decompose_STRK_LAST(tdArray) {
       }
     }
   }
-  // console.log(L10_Array);
-  // console.log('---');
-  // console.log(temp);
-  let j = 0;
 
-  for (let i = 7; i <= temp.length; i = i + 7 + 1) {
-    // for (let j = 0; j < L10_Array.length; j++) {
-    //  const element = array[j];
+  L10_Array = repackage_L10(L10_Array);
+  let j = 0;
+  for (let i = teamTableFieldCount; i <= temp.length; i = i + teamTableFieldCount + 1) {
     temp.splice(i, 0, L10_Array[j]);
-    j = j + 1;
-    // }
+    j++;
   }
-  // console.log(temp, '--');
-  temp.forEach(function(ele, i) {
-    console.log(i, ele);
-  });
+  return temp;
 }
+// 7W 3L 0D 改為 7-0-3
+function repackage_L10(data) {
+  const temp = [];
+  for (let i = 0; i < data.length; i++) {
+    const ele = data[i];
+    const winIndex = ele.indexOf('W');
+    const lossIndex = ele.indexOf('L');
+    // const fairIndex = ele.indexOf('D');
+    const win = ele.substring(0, winIndex).trim();
+    const loss = ele.substring(winIndex + 1, lossIndex).trim();
+    const fair = ele.substring(lossIndex + 1, ele.length - 1).trim();
+    const L10 = `${win}-${fair}-${loss}`;
+    temp.push(L10);
+  }
+  return temp;
+}
+
 function repackageTeamStats(teamsStats) {
   const data = {
-    KBO_2020: []
+    season_2020: []
   };
-  for (let i = 0; i < teamsStats.length; i = i + 7) {
+  for (let i = 0; i < teamsStats.length; i = i + teamTableFieldCount + 1) {
     const temp = {
       team_alias: teamsStats[i],
-      win_count: teamsStats[i + 1],
-      loss_count: teamsStats[i + 2],
-      fair_count: teamsStats[i + 3],
-      win_rate: teamsStats[i + 4],
-      game_behind: teamsStats[i + 5],
-      last_10_games: teamsStats[i + 6]
+      G: teamsStats[i + 1] + teamsStats[i + 2],
+      Win: teamsStats[i + 1],
+      Draw: teamsStats[i + 3],
+      Loss: teamsStats[i + 2],
+      PCT: `0${teamsStats[i + 4]}`,
+      GB: teamsStats[i + 5],
+      STRK: teamsStats[i + 6],
+      L10: teamsStats[i + 7]
     };
-    data.KBO_2020.push(temp);
+    data.season_2020.push(temp);
   }
   return data;
 }
+
 module.exports = prematch_KBO;
