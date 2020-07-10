c373895872abb1906fa7db49ca6af4ef860e4753
Rex <rex@gets-info.com>
2020-07-10 11:37:26 +0800

    [merge] alan

--- functions/index.js
+++ functions/index.js
@@ -124,12 +124,11 @@ app.use('/cashflow_api', require('./routers/cashflow_api'));// 金流介接(gash
 app.use('/cashflow_neweb', require('./routers/cashflow_neweb')); // 金流介接(藍新)
 // app.use('/invoice_ezpay', require('./routers/invoice_ezpay')); // 電子發票介接(ezpay)
 // keep firebase cloud function :API awake
-app.use('/mission', require('./routers/mission'));
+// app.use('/mission', require('./routers/mission'));
 app.get('/awakeAPI', (req, res) => {
   res.status(200).json({ test: 'awake0528v01' });
 });
 
-// app.use('/mission', require('./routers/mission'));//搞任務
 // API cloud function
 exports.api = functions.runWith(runtimeOpts).https.onRequest(app);
 // admin cloud function
