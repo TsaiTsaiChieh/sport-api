a699da578307c236620f2a82b7c77f394ca38ef9
Alan <alan@Alans-MacBook-Pro.local>
2020-07-13 17:45:29 +0800

    新增 每日任務 未登入部份

---
+++ functions/model/mission/missionDailyModel.js
@@ -0,0 +1,52 @@
+/* eslint-disable prefer-const */
+const { date3UnixInfo } = require('../../util/modules');
+const { dailyMission } = require('../../util/missionUtil');
+
+async function missionDaily(args) {
+  // 要區分 未登入、已登入
+  const userUid = !args.token ? null : args.token.uid; // 需要判斷 有登入的話，有 uesr uid
+
+  const nowInfo = date3UnixInfo(Date.now());
+  const todayUnix = nowInfo.dateBeginUnix;
+
+  let result = { daily: [] };
+
+  //
+  // 未登入處理
+  //
+  if (!userUid) {
+    const missionLists = await dailyMission(todayUnix);
+    if (missionLists.length === 0) return result; // 回傳 空Array
+
+    for (const data of Object.values(missionLists)) {
+      data.status = 0;
+      data.now_finish_nums = 0;
+      result.daily.push(repackageDaily(data));
+    }
+    return result;
+  }
+
+  return result;
+}
+
+function repackageDaily(ele) {
+  const data = {
+    title: ele.title,
+    desc: ele.desc, // 開賽時間
+    start_date: !ele.start_date ? '' : ele.start_date,
+    end_date: !ele.end_date ? '' : ele.end_date,
+    item_id: !ele.mission_item_id ? '' : ele.mission_item_id,
+    target: ele.target,
+    reward_class: ele.reward_class, // 獎勵類型 0: 單一獎勵  1: 不同角色不同獎勵
+    reward_type: ele.reward_type, // 獎勵幣型 ingot: 搞錠  coin: 搞幣  dividend: 紅利
+    reward_num: ele.reward_num,
+    reward_class_num: ele.reward_class === 1 ? ele.reward_class_num : '',
+    status: !ele.status ? 0 : ele.status, // 任務狀態 0: 前往(預設)  1: 領取  2: 已完成
+    need_finish_nums: ele.need_finish_nums,
+    now_finish_nums: ele.now_finish_nums
+  };
+
+  return data;
+}
+
+module.exports = missionDaily;
