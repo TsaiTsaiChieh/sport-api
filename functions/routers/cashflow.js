3b2186a0257d643b9e06b60b9078065ec8b60fee
Alan <alan@Alans-MacBook-Pro.local>
2020-07-14 09:33:59 +0800

    修改 pub/sub cashflow 移除，補回 route cashflow

---
+++ functions/routers/cashflow.js
@@ -0,0 +1,36 @@
+const modules = require('../util/modules');
+const router = modules.express.Router();
+
+/* 取得14天後到期紅利 */
+router.post(
+  '/dividend_expire',
+  require('../controller/cashflow/dividendExpireController')
+);
+/* 計算14天後到期紅利 */
+router.put(
+  '/dividend_expire',
+  require('../controller/cashflow/dividendExpireController')
+);
+/* 扣除到期紅利 */
+router.delete(
+  '/dividend_expire',
+  require('../controller/cashflow/dividendExpireController')
+);
+
+/* 紅利回饋 */
+router.post(
+  '/dividend_refund',
+  require('../controller/cashflow/dividendRefundController')
+);
+/* 搞錠轉換現金 */
+router.post(
+  '/ingot_transfer',
+  require('../controller/cashflow/ingotTransferController')
+);
+
+/* 購買搞錠列表 */
+router.get(
+  '/purchase_list',
+  require('../controller/cashflow/purchaseListController')
+);
+module.exports = router;
