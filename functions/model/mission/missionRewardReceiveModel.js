935acd9bcfe1852390aac269a21296394bd55645
Alan <alan@Alans-MacBook-Pro.local>
2020-07-15 15:52:56 +0800

    修改 目前設計上每日和活動的 item_id 會衝突，無法直接用 daily activity判斷，改用 mission_item 來做處理，代表 mission_item 有 daily activity 兩種的情況 處理時要特別判斷，先預留 mission_god mission_deposit

--- functions/model/mission/missionRewardReceiveModel.js
+++ functions/model/mission/missionRewardReceiveModel.js
@@ -6,36 +6,80 @@ const { setUserMissionStatus } = require('../../util/missionUtil');
 
 async function missionRewardReceive(args) {
   const userUid = args.token.uid;
-  const item_id = args.query.item_id;
+  const type = args.query.type;
+  const id = args.query.id;
   const nowInfo = date3UnixInfo(Date.now());
   const todayUnix = nowInfo.dateBeginUnix;
+  let check1;
 
-  // 取得 mission type 0: 每日  1: 進階  2: 活動
-  const check1 = await db.sequelize.query(`
-    select missions.type
-      from missions, mission_items
-     where missions.mission_id = mission_items.mission_id
-       and mission_items.mission_id = :item_id
-  `, {
-    replacements: {
-      item_id: item_id
-    },
-    type: db.sequelize.QueryTypes.SELECT
-  });
-  if (check1.length > 1) throw errs.dbErrsMsg('404', '15210'); // 取得多筆不正確
-  if (check1.length === 0) throw errs.dbErrsMsg('404', '15013'); // 查不到
+  // 取得 mission type： mission_item(每日、活動-預測)  mission_god(活動-大神)  mission_deposit(活動-儲值)
+  // mission_item(每日、活動-預測)
+  if (type === 'mission_item') {
+    check1 = await db.sequelize.query(`
+      select missions.type
+        from missions, mission_items
+       where missions.mission_id = mission_items.mission_id
+         and mission_items.mission_item_id = :id
+    `, {
+      replacements: {
+        id: id
+      },
+      type: db.sequelize.QueryTypes.SELECT
+    });
+    if (check1.length > 1) throw errs.dbErrsMsg('404', '15210'); // 取得多筆不正確
+    if (check1.length === 0) throw errs.dbErrsMsg('404', '15215'); // 查不到
+  }
+
+  // if (type === 'mission_god') {
+  //   check1 = await db.sequelize.query(`
+  //     select missions.type
+  //       from missions, mission_gods
+  //      where missions.mission_id = mission_gods.mission_id
+  //        and mission_gods.mission_god_id = :id
+  //   `, {
+  //     replacements: {
+  //       id: id
+  //     },
+  //     type: db.sequelize.QueryTypes.SELECT
+  //   });
+  //   if (check1.length > 1) throw errs.dbErrsMsg('404', '15211'); // 取得多筆不正確
+  //   if (check1.length === 0) throw errs.dbErrsMsg('404', '15216'); // 查不到
+  // }
+
+  // if (type === 'mission_deposit') {
+  //   check1 = await db.sequelize.query(`
+  //     select missions.type
+  //       from missions, mission_deposits
+  //      where missions.mission_id = mission_deposits.mission_id
+  //        and mission_deposits.mission_deposit_id = :id
+  //   `, {
+  //     replacements: {
+  //       id: id
+  //     },
+  //     type: db.sequelize.QueryTypes.SELECT
+  //   });
+  //   if (check1.length > 1) throw errs.dbErrsMsg('404', '15212'); // 取得多筆不正確
+  //   if (check1.length === 0) throw errs.dbErrsMsg('404', '15217'); // 查不到
+  // }
 
   let missionStatusUpdateParms, check2;
   const trans = await db.sequelize.transaction();
 
+  // 取得 mission type 0: 每日  1: 進階  2: 活動
   if (check1[0].type === 0) { // 每日 daily
     // daily 要多輸入該天Unix() = todayUnix
     // daily 部份 領取前需要確認 有領取資格(status: 1)
-    missionStatusUpdateParms = { status: 1, mission_item_id: item_id, dateUnix: todayUnix };
-    check2 = await setUserMissionStatus(userUid, missionStatusUpdateParms, 2, trans);
+    missionStatusUpdateParms = { status: 1, mission_item_id: id, dateUnix: todayUnix };
   }
 
-  // 活動
+  if (check1[0].type === 2) { // 活動 activity 來源有多個
+    // 領取前需要確認 有領取資格(status: 1)
+    if (type === 'mission_item') missionStatusUpdateParms = { status: 1, mission_item_id: id };
+    // if (type === 'mission_god') missionStatusUpdateParms = { status: 1, mission_item_id: id };
+    // if (type === 'mission_deposit') missionStatusUpdateParms = { status: 1, mission_item_id: id };
+  }
+
+  check2 = await setUserMissionStatus(userUid, missionStatusUpdateParms, 2, trans);
 
   // 需確認是否有獎勵  結果為 0 代表 沒有獎勵的
   if (check2[0] === 0) {
