4a928299e1828e4e99f8ec03ee943e45d0f8d4ce
Alan <alan@Alans-MacBook-Pro.local>
2020-07-16 17:37:03 +0800

    修改 當日 單一 聯盟 預測達5盤勝利 未完成

--- functions/model/mission/missionFuncModel.js
+++ functions/model/mission/missionFuncModel.js
@@ -70,8 +70,36 @@ async function topicCheckByDateBetween(uid, beginUnix, endUnix, category) {
 
   return topics;
 }
+
+// 預測賽事 當日 單(同)一聯盟 ?盤勝利 (限一次)
+// begin、end: 日期區間
+async function predictCorrectLeagueDailyByDateBetween(userUid, beginUnix, endUnix) {
+  const matchs = await db.sequelize.query(`
+    select league_id, match_date, sum(spread_corrct_count)  correct_count
+      from (
+             select league_id, match_date, 
+                    if (spread_result_flag > 0, 1, 0) spread_corrct_count
+               from user__predictions
+              where uid = :userUid
+                and match_date between :begin and :end
+           ) a
+     group by league_id, match_date
+     order by match_date
+  `, {
+    replacements: {
+      userUid: userUid,
+      begin: beginUnix,
+      end: endUnix
+    },
+    type: db.sequelize.QueryTypes.SELECT
+  });
+
+  return matchs;
+}
+
 module.exports = {
   predictLeagueMatchCheckByDateBetween,
   predictMatchCheckByDateBetween,
-  topicCheckByDateBetween
+  topicCheckByDateBetween,
+  predictCorrectLeagueDailyByDateBetween
 };
