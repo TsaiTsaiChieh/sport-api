8786967e19b7f0e3601d0ebced99513c02e4db61
d0177381@gmail.com <>
2020-07-21 01:01:05 +0800

    [not finish all]

--- functions/pubsub/crawlers/prematch_KBO_2.js
+++ functions/pubsub/crawlers/prematch_KBO_2.js
@@ -3,18 +3,19 @@ const configs = {
   official_URL: 'http://eng.koreabaseball.com/',
   blackboard_URL: 'https://mykbostats.com/',
   // TODO table titles should be dynamic to crawler
-  officialTeamStandingsTitles: ['RK', 'TEAM', 'GAMES', 'W', 'L', 'D', 'PCT', 'GB', 'STREAK', 'HOME', 'AWAY'],
+  officialTeamStandingsUpperTitles: ['RK', 'TEAM', 'GAMES', 'W', 'L', 'D', 'PCT', 'GB', 'STREAK', 'HOME', 'AWAY'],
+  officialTeamStandingsLowerTitles: ['RK', 'TEAM', 'AVG', 'ERA', 'RUNS', 'RUNS ALLOWED', 'HR'],
   blackboardTeamTableTitles: ['Rank/Team', 'W', 'L', 'D', 'PCT', 'GB', 'STRK/LAST 10G'],
   blackboardTeamSplitsTitles: ['Season', 'G', 'W', 'L', 'D', 'W%', 'R/G', '-R/G', 'R', '-R', 'dR', 'H/G', '-H/G', 'H',
     '-H', 'dH', 'HR/G', '-HR/G', 'HR', '-HR', 'dHR', 'BA', 'ERA_sp', 'ERA_rp'],
-
   teamNumber: 10,
   collectionName: 'baseball_KBO'
 };
 const modules = require('../../util/modules');
 const dbEngine = require('../../util/databaseEngine');
 const AppErrors = require('../../util/AppErrors');
-
+const teamsMapping = require('../../util/teamsMapping');
+const teamStandings = {};
 // TODO crawler KBO prematch information:
 // 1. 隊伍資訊 ex: team_base, team_hit
 // team_base: 近十場戰績 L10，（本季）戰績 W-L-D，（本季）主客隊戰績 at_home/at_away，（本季）平均得分/失分 RG/-RG
@@ -24,6 +25,7 @@ const AppErrors = require('../../util/AppErrors');
 async function prematch_KBO() {
   return new Promise(async function(resolve, reject) {
     try {
+      // season should be a function to catch error
       const season = await getSeason(configs.league);
       await crawler_KBO(season);
     } catch (err) {
@@ -46,15 +48,20 @@ function crawler_KBO(season) {
   return new Promise(async function(resolve, reject) {
     try {
       // 官網
-      const $_officialData = await crawler(`${configs.official_URL}Standings/TeamStandings.aspx`);
+      // TODO searchDate should be today(default)
+      const today = modules.convertTimezoneFormat(Math.floor(Date.now() / 1000), { format: 'YYYY-MM-DD' });
+      console.log(today);
+      const $_officialData = await crawler(`${configs.official_URL}Standings/TeamStandings.aspx?searchDate=2020-07-19`);
       const officialData = await getTeamStandingsFromOfficial($_officialData);
-      combineTwoTableFromOfficial(officialData);
+      insertTeamToFirestore(officialData, season);
+      // combineTwoTableFromOfficial(officialData);
+
       // 黑板
-      const $_blackboardData = await crawler(configs.blackboard_URL);
-      const blackboardTeamData = await getTeamsStandingsFromBlackboard($_blackboardData);
-      const $_blackboardTeamSplitsData = await crawler(`${configs.blackboard_URL}stats/team_splits/${season}`);
-      const blackboardTeamSplitsData = await getTeamSplitsFromBlackboard($_blackboardTeamSplitsData);
-      console.log(blackboardTeamData, blackboardTeamSplitsData);
+      // const $_blackboardData = await crawler(configs.blackboard_URL);
+      // const blackboardTeamData = await getTeamsStandingsFromBlackboard($_blackboardData);
+      // const $_blackboardTeamSplitsData = await crawler(`${configs.blackboard_URL}stats/team_splits/${season}`);
+      // const blackboardTeamSplitsData = await getTeamSplitsFromBlackboard($_blackboardTeamSplitsData);
+      // console.log(blackboardTeamData, blackboardTeamSplitsData);
     } catch (err) {
       return reject(new AppErrors.KBOCrawlersError(`${err.stack} by TsaiChieh`));
     }
@@ -76,17 +83,16 @@ function crawler(URL) {
 function getTeamStandingsFromOfficial($) {
   return new Promise(async function(resolve, reject) {
     try {
-      const upperTable = [];
+      const upperTable = []; // team_base
       const lowerTable = [];
       $('td').each(function(i) {
         // upper table
-        if (i < configs.officialTeamStandingsTitles.length * configs.teamNumber) {
+        if (i < configs.officialTeamStandingsUpperTitles.length * configs.teamNumber) {
           upperTable[i] = $(this).text();
         } else { // lower table
           lowerTable.push($(this).text());
         }
       });
-      // console.log(upperTable, lowerTable);
       return resolve({ upperTable, lowerTable });
     } catch (err) {
       return reject(new AppErrors.CrawlersError(`${err.stack} by TsaiChieh`));
@@ -94,14 +100,100 @@ function getTeamStandingsFromOfficial($) {
   });
 }
 
-function combineTwoTableFromOfficial(officialData) {
+function insertTeamToFirestore(officialData, season) {
+  return new Promise(async function(resolve, reject) {
+    try {
+      const { upperTable, lowerTable } = officialData;
+      // 由於官網的本季資訊會隨當天日期而變化，若無比賽或未更新完成，lowerTable 會為空陣列，藉此來判斷本季資訊有無更新再做變動
+      if (lowerTable.length !== 0) {
+        for (let i = 0; i < upperTable.length; i++) {
+          if (i % configs.officialTeamStandingsUpperTitles.length === 0) {
+            const teamName = upperTable[i + 1];
+            teamStandings[teamName] = [];
+            const teamId = teamsMapping.KBO_teamName2id(teamName);
+            const G = upperTable[i + 2]; // games
+            const Win = upperTable[i + 3]; // W
+            const Loss = upperTable[i + 4]; // L
+            const Draw = upperTable[i + 5]; // D
+            const PCT = upperTable[i + 6]; // 勝率
+            const GB = upperTable[i + 7]; // 勝差
+            const STRK = upperTable[i + 8]; // streak 連勝/連輸
+            const at_home = upperTable[i + 9];
+            const at_away = upperTable[i + 10];
+            // const data = repackageUpperTable({ i, teamName, G, Win, Loss, Draw, PCT, GB, STRK, at_home, at_away }, season);
+            teamStandings[teamName].push({ G });
+            await insertFirestoreForUpperTable({ G, Win, Loss, Draw, PCT, GB, STRK, at_home, at_away }, teamId, season);
+          }
+        }
+
+        for (let i = 0; i < lowerTable.length; i++) {
+          if (i % configs.officialTeamStandingsLowerTitles.length === 0) {
+            const teamName = lowerTable[i + 1];
+            const teamId = teamsMapping.KBO_teamName2id(teamName);
+            const R = lowerTable[i + 4]; // Runs
+            const allow_R = lowerTable[i + 5]; // Runs Allowed
+            const G = parseInt(teamStandings[teamName][0].G); // games
+            const per_R = String((parseInt(R) / G).toFixed(3));
+            const per_allow_R = String((parseInt(allow_R) / G).toFixed(3));
+            await insertFirestoreForLowerTable({ R, allow_R, per_R, per_allow_R }, teamId, season);
+          }
+        }
+      }
+    } catch (err) {
+      return reject(new AppErrors.RepackageError(`${err.stack} by TsaiChieh`));
+    }
+  });
+}
+
+function repackageUpperTable(upperTable, season) {
   try {
-    console.log(officialData);
+    const { G, Win, Loss, Draw, PCT, GB, STRK, at_home, at_away } = upperTable;
+    return {
+      G, Win, Loss, Draw, PCT, GB, STRK, at_home, at_away
+    };
   } catch (err) {
     console.error(`${err.stack} by TsaiChieh`);
     throw new AppErrors.RepackageError(`${err.stack} by TsaiChieh`);
   }
 }
+
+function insertFirestoreForUpperTable(data, teamId, season) {
+  return new Promise(async function(resolve, reject) {
+    try {
+      const temp = {};
+      temp[`season_${season}`] = {};
+      temp[`season_${season}`].team_base = data;
+      await modules.firestore.collection(configs.collectionName).doc(teamId).set(temp, { merge: true });
+      return resolve();
+    } catch (err) {
+      console.error(err);
+      return reject(new AppErrors.FirebaseCollectError(`${err.stack} by TsaiChieh`));
+    }
+  });
+}
+
+function insertFirestoreForLowerTable(data, teamId, season) {
+  return new Promise(async function(resolve, reject) {
+    try {
+      const temp = {};
+      temp[`season_${season}`] = {};
+      temp[`season_${season}`].team_base = data;
+      await modules.firestore.collection(configs.collectionName).doc(teamId).set(temp, { merge: true });
+      return resolve();
+    } catch (err) {
+      console.error(err);
+      return reject(new AppErrors.FirebaseCollectError(`${err.stack} by TsaiChieh`));
+    }
+  });
+}
+// function combineTwoTableFromOfficial(officialData) {
+//   try {
+//     console.log(officialData);
+//   } catch (err) {
+//     console.error(`${err.stack} by TsaiChieh`);
+//     throw new AppErrors.RepackageError(`${err.stack} by TsaiChieh`);
+//   }
+// }
 function getTeamsStandingsFromBlackboard($) {
   return new Promise(async function(resolve, reject) {
     try {
@@ -192,27 +284,37 @@ function decomposeTeamBaseAndName(teamsStats) {
 }
 
 function teamName2id(name) {
-  name = name.toLowerCase();
+  name = name.toLowerCase().trim();
   switch (name) {
-    case 'lotte giants' || 'lotte':
+    case 'lotte giants':
+    case 'lotte':
       return 2408;
-    case 'samsung lions' || 'samsung':
+    case 'samsung lions':
+    case 'samsung':
       return 3356;
-    case 'kia tigers' || 'kt':
+    case 'kia tigers':
+    case 'kia':
       return 4202;
-    case 'doosan bears' || 'doosan':
+    case 'doosan bears':
+    case 'doosan':
       return 2406;
-    case 'hanwha eagles' || 'hanwha':
+    case 'hanwha eagles':
+    case 'hanwha':
       return 2405;
-    case 'sk wyverns' || 'sk':
+    case 'sk wyverns':
+    case 'sk':
       return 8043;
-    case 'lg twins' || 'lg':
+    case 'lg twins':
+    case 'lg':
       return 2407;
-    case 'kiwoom heroes' || 'kiwoom':
+    case 'kiwoom heroes':
+    case 'kiwoom':
       return 269103;
-    case 'nc dinos' || 'nc':
+    case 'nc':
+    case 'nc dinos':
       return 3353;
-    case 'kt wiz' || 'kt':
+    case 'kt wiz':
+    case 'kt':
       return 3354;
     default:
       return 'Unknown team name';
