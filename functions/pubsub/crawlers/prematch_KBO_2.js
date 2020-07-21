338927ea20e41050554dd9c2085b8715090ab167
d0177381@gmail.com <>
2020-07-21 23:16:18 +0800

    [not finish all]

--- functions/pubsub/crawlers/prematch_KBO_2.js
+++ functions/pubsub/crawlers/prematch_KBO_2.js
@@ -4,8 +4,12 @@ const configs = {
   // TODO table titles should be dynamic to crawler
   officialTeamStandingsUpperTitles: ['RK', 'TEAM', 'GAMES', 'W', 'L', 'D', 'PCT', 'GB', 'STREAK', 'HOME', 'AWAY'],
   officialTeamStandingsLowerTitles: ['RK', 'TEAM', 'AVG', 'ERA', 'RUNS', 'RUNS ALLOWED', 'HR'],
+  pitcherByTeamPage1Titles: ['PLAYER', 'TEAM', 'ERA',	'G',	'CG', 'SHO', 'W', 'L', 'SV', 'HLD', 'PCT', 'PA', 'NP', 'IP', 'H', '2B', '3B', 'HR'],
+  pitcherByTeamPage2Titles: ['PLAYER', 'TEAM', 'SAC', 'SF', 'BB', 'IBB', 'HBP', 'SO', 'WP', 'BK', 'R', 'ER', 'BS', 'WHIP', 'OAVG', 'QS'],
   teamNumber: 10,
-  collectionName: 'baseball_KBO'
+  collectionName: 'baseball_KBO',
+  // DOOSAN, KIWOOM, SK, LG, NC, KT, KIA, SAMSUNG, HANWHA, LOTTE
+  teamCode: ['OB', 'WO', 'SK', 'LG', 'NC', 'KT', 'HT', 'SS', 'HH', 'LT']
 };
 const modules = require('../../util/modules');
 const dbEngine = require('../../util/databaseEngine');
@@ -14,9 +18,10 @@ const teamsMapping = require('../../util/teamsMapping');
 const teamStandings = {};
 // TODO crawler KBO prematch information:
 // 1. 隊伍資訊 ex: team_base, team_hit
-// team_base: 近十場戰績 L10，（本季）戰績 W-L-D，（本季）主客隊戰績 at_home/at_away，（本季）平均得分/失分 RG/-RG
+// team_base: 近十場戰績 L10，（本季）戰績 W-L-D，（本季）主客隊戰績 at_home/at_away，（本季）平均得分/失分 RG/-RG (per_R & allow_per_R)
 // team_hit: 得分，安打，全壘打數，打擊率，上壘率，長打率
-// 2. 球員資訊
+// 2. 本季投手資訊
+// 勝敗，防禦率，三振數
 
 async function prematch_KBO() {
   return new Promise(async function(resolve, reject) {
@@ -24,6 +29,7 @@ async function prematch_KBO() {
       // season should be a function to catch error
       const season = await getSeason(configs.league);
       await crawler_KBO(season);
+      await crawlerPitcher(season);
     } catch (err) {
       return reject(new AppErrors.KBOCrawlersError(err.stack));
     }
@@ -47,9 +53,10 @@ function crawler_KBO(season) {
       // TODO searchDate should be today(default)
       const today = modules.convertTimezoneFormat(Math.floor(Date.now() / 1000), { format: 'YYYY-MM-DD' });
       console.log(today);
-      const $_officialData = await crawler(`${configs.official_URL}Standings/TeamStandings.aspx?searchDate=2020-07-19`);
+      const $_officialData = await crawler(`${configs.official_URL}Standings/TeamStandings.aspx?searchDate=2020-07-20`);
       const officialData = await getTeamStandingsFromOfficial($_officialData);
       insertTeamToFirestore(officialData, season);
+      return resolve();
     } catch (err) {
       return reject(new AppErrors.KBOCrawlersError(`${err.stack} by TsaiChieh`));
     }
@@ -147,4 +154,74 @@ function insertFirestore(data, teamId, season) {
   });
 }
 
+function crawlerPitcher(season) {
+  return new Promise(async function(resolve, reject) {
+    try {
+      // TODO deploy should change to configs.teamCode.length
+      for (let i = 0; i < 1; i++) {
+        const teamCode = configs.teamCode[i];
+        const $_pitchingStats = await crawler(`${configs.official_URL}Stats/PitchingByTeams.aspx?codeTeam=${teamCode}`);
+        const pitchingStatsData = await getPitchingStatsFromOfficial($_pitchingStats);
+        repackagePitcherData(pitchingStatsData, season);
+      }
+    } catch (err) {
+      return reject(new AppErrors.KBOCrawlersError(`${err.stack} by TsaiChieh`));
+    }
+  });
+}
+
+function getPitchingStatsFromOfficial($) {
+  return new Promise(async function(resolve, reject) {
+    try {
+      const pitcherStats = [];
+      const pitcherIds = [];
+      $('td').each(function(i, ele) {
+        if (i % configs.pitcherByTeamPage1Titles.length === 0) {
+          const pitcherId = $(ele).find('a').attr('href').replace('/teams/playerinfopitcher/summary.aspx?pcode=', '');
+          pitcherIds.push(pitcherId);
+        }
+        pitcherStats.push($(this).text());
+      });
+      return resolve({ pitcherStats, pitcherIds });
+    } catch (err) {
+      return reject(new AppErrors.CrawlersError(`${err.stack} by TsaiChieh`));
+    }
+  });
+}
+
+function repackagePitcherData(pitchingStats, season) {
+  const { pitcherStats, pitcherIds } = pitchingStats;
+  // console.log(pitcherIds);
+  let j = 0;
+  const data = {};
+  const teamId = teamsMapping.KBO_teamName2id(pitcherStats[1]);
+  for (let i = 0; i < pitcherStats.length; i++) {
+    if (i % configs.pitcherByTeamPage1Titles.length === 0) {
+      const name = pitcherStats[i];
+      const pitcherId = pitcherIds[j];
+      const ERA = pitcherStats[i + 3];
+      const G = pitcherStats[i + 4];
+      data[pitcherId] = { name, ERA, G };
+      j++;
+    }
+  }
+  insertPitcherToFirestore(data, teamId, season);
+}
+
+function insertPitcherToFirestore(officialData, teamId, season) {
+  return new Promise(async function(resolve, reject) {
+    try {
+      const data = {};
+      data[`season_${season}`] = {};
+      data[`season_${season}`].pitchers = {};
+      data[`season_${season}`].pitchers = officialData;
+      // console.log(data);
+      // console.log(officialData.pitcherId);
+      const result = await modules.firestore.collection(configs.collectionName).doc(teamId).set(data, { merge: true });
+      console.log(result);
+    } catch (err) {
+      return reject(new AppErrors.FirebaseCollectError(`${err.stack} by TsaiChieh`));
+    }
+  });
+}
 module.exports = prematch_KBO;
