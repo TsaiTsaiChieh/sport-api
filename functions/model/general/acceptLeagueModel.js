fa62fb867876103252ec7aa1064bb3a0031ab754
page40316 <page@gets-info.com>
2020-07-21 10:32:08 +0800

    排程錯誤與acceptleague修正

--- functions/model/general/acceptLeagueModel.js
+++ functions/model/general/acceptLeagueModel.js
@@ -15,13 +15,13 @@ async function acceptLeague() {
 
 async function repackage(acceptLeague) {
   try {
-    const result = [];
-    for (let i = 0; i < acceptLeague.length; i++) {
-      const leagueFlag = await queryEvent(acceptLeague[i]);
-      if (leagueFlag.length > 0) {
-        result.push(acceptLeague[i]);
-      }
-    }
+    // const result = [];
+    // for (let i = 0; i < acceptLeague.length; i++) {
+    //  const leagueFlag = await queryEvent(acceptLeague[i]);
+    //  if (leagueFlag.length > 0) {
+    //    result.push(acceptLeague[i]);
+    //  }
+    // }
 
     return acceptLeague;
   } catch (err) {
@@ -30,27 +30,25 @@ async function repackage(acceptLeague) {
   }
 }
 
-async function queryEvent(leagueID, date1, date2) {
-  return new Promise(async function(resolve, reject) {
-    try {
-      const queries = await db.sequelize.query(
-        `
-				 SELECT game.bets_id AS bets_id, game.scheduled AS scheduled, game.status AS status
-					 FROM matches AS game			      
-					WHERE (game.status = ${modules.MATCH_STATUS.SCHEDULED} OR game.status = ${modules.MATCH_STATUS.INPLAY} OR game.status = ${modules.MATCH_STATUS.END})
-						AND game.league_id = ${leagueID}
-						AND game.scheduled BETWEEN ${date1} AND ${date2}
-			 `,
-        {
-          type: db.sequelize.QueryTypes.SELECT
-        }
-      );
-      return resolve(queries);
-    } catch (err) {
-      return reject(
-        new AppErrors.MysqlError(`${err} at checkmatch_another by DY`)
-      );
-    }
-  });
-}
+// async function queryEvent(leagueID, date1, date2) {
+//  return new Promise(async function (resolve, reject) {
+//    try {
+//      const queries = await db.sequelize.query(
+//        `
+//				 SELECT game.bets_id AS bets_id, game.scheduled AS scheduled, game.status AS status
+//					 FROM matches AS game
+//					WHERE (game.status = ${modules.MATCH_STATUS.SCHEDULED} OR game.status = ${modules.MATCH_STATUS.INPLAY} OR game.status = ${modules.MATCH_STATUS.END})
+//						AND game.league_id = ${leagueID}
+//						AND game.scheduled BETWEEN ${date1} AND ${date2}
+//			 `,
+//        {
+//          type: db.sequelize.QueryTypes.SELECT
+//        }
+//      );
+//      return resolve(queries);
+//    } catch (err) {
+//      return reject(new AppErrors.MysqlError(`${err} at acceptleague by DY`));
+//    }
+//  });
+// }
 module.exports = acceptLeague;
