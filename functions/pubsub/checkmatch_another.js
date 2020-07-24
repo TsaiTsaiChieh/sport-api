46214a595d79a6f40c849f070e9a407a2488a504
page40316 <page@gets-info.com>
2020-07-24 20:12:38 +0800

    [Add] 新增MLB比分

--- functions/pubsub/checkmatch_another.js
+++ functions/pubsub/checkmatch_another.js
@@ -6,7 +6,7 @@ const AnotherpbpInplay = Anotherpbp.AnotherpbpInplay;
 const AnotherpbpHistory = Anotherpbp.AnotherpbpHistory;
 const Match = db.Match;
 // const leagueArray = ['KBO', 'CPBL', 'NPB', 'CBA', 'Soccer']; //ori
-const leagueArray = ['KBO', 'CPBL', 'NPB', 'Soccer']; // 2020/07/23
+const leagueArray = ['KBO', 'CPBL', 'NPB', 'Soccer','MLB']; // 2020/07/23
 // const leagueArray = ['Soccer'];
 async function checkmatch_another() {
   return new Promise(async function(resolve, reject) {
