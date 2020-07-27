5005414261f29e492f31505caf6eecacbd8b5093
Alan <alan@Alans-MacBook-Pro.local>
2020-07-27 18:08:44 +0800

    [Fix]把這星期一更新上星期 和 這個月第一天更新上個月部份，移到 賽事勝注勝率計算之前

--- functions/pubsub/god_settleWinList_A.js
+++ functions/pubsub/god_settleWinList_A.js
@@ -6,6 +6,10 @@ const settleWinList = require('../model/user/settleWinListModel');
 const settleGodTitle = require('../model/user/settleGodTitleModel');
 const { redis } = require('../util/redisUtil');
 
+const db = require('../util/dbUtil');
+const to = require('await-to-js').default;
+const errs = require('../util/errorCode');
+
 const logger = require('firebase-functions/lib/logger');
 const util = require('util');
 function log(...args) {
@@ -37,8 +41,79 @@ async function god_settleWinList_A() {
   log('nowDayOfMonth: ', nowDayOfMonth);
 
   //
+  // 3. `每天` `下午5點` `這個星期的星期一日期` 更新 `上星期` 並清空 `本星期` 設為 0
+  //
+  log('========== pubsub god_1OfWeek start ==========');
+  if (nowHHmm === '1700' && nowDayOfWeek === 1) {
+    log('每天 17:00 這這個星期的星期一日期 更新 上星期記錄，並清空 本星期記錄 設為 0 run');
+
+    const [err, r] = await to(db.sequelize.query(`
+      update users__win__lists 
+         set last_week_win_bets = this_week_win_bets,
+             last_week_win_rate = this_week_win_rate,
+             last_week_correct_counts = this_week_correct_counts,
+             last_week_fault_counts = this_week_fault_counts
+    `, { type: db.sequelize.QueryTypes.UPDATE }));
+
+    log('更新 users__win__lists 筆數: ', r);
+    if (err) {logger.warn(err); logger.warn(errs.dbErrsMsg('404', '50010', { addMsg: err.parent.code }));}
+
+    const [err2, r2] = await to(db.sequelize.query(`
+      update users__win__lists 
+         set this_week_win_bets = 0,
+             this_week_win_rate = 0,
+             this_week_correct_counts = 0,
+             this_week_fault_counts = 0
+    `, { type: db.sequelize.QueryTypes.UPDATE }));
+
+    log('更新 users__win__lists 筆數: ', r2);
+    if (err) {logger.warn(err2); logger.warn(err2.dbErrsMsg('404', '50010', { addMsg: err.parent.code }));}
+
+    await redis.specialDel('*titles*', 100);
+    await redis.specialDel('*users__win__lists*', 100);
+  }
+  //
+  log('========== pubsub god_1OfWeek end ==========');
+
+  //
+  // 4. `每天` `下午5點` `這個月第一天日期` 更新 ` 上個月`記錄，並清空 `本月`記錄 設為 0
+  //
+  log('========== pubsub god_1OfMonth start ==========');
+  if (nowHHmm === '1700' && nowDayOfMonth === '01') {
+    log('每天 17:00 這個月第一天日期 更新 上個月記錄，並清空 本月記錄 設為 0 run');
+
+    const [err, r] = await to(db.sequelize.query(`
+        update users__win__lists 
+           set last_month_win_bets = this_month_win_bets,
+               last_month_win_rate = this_month_win_rate,
+               last_month_correct_counts = this_month_correct_counts,
+               last_month_fault_counts = this_month_fault_counts
+      `, { type: db.sequelize.QueryTypes.UPDATE }));
+
+    log('更新 users__win__lists 筆數: ', r);
+    if (err) {logger.warn(err); logger.warn(errs.dbErrsMsg('404', '50011', { addMsg: err.parent.code }));}
+
+    const [err2, r2] = await to(db.sequelize.query(`
+        update users__win__lists 
+           set this_month_win_bets = 0,
+               this_month_win_rate = 0,
+               this_month_correct_counts = 0,
+               this_month_fault_counts = 0
+      `, { type: db.sequelize.QueryTypes.UPDATE }));
+
+    log('更新 users__win__lists 筆數: ', r2);
+    if (err) {logger.warn(err2); logger.warn(err2.dbErrsMsg('404', '50011', { addMsg: err.parent.code }));}
+
+    await redis.specialDel('*titles*', 100);
+    await redis.specialDel('*users__win__lists*', 100);
+  }
+  //
+  log('========== pubsub god_1OfMonth end ==========');
+
+  //
   // 2. `每天` `下午5點` 賽事勝注勝率計算 `A部份`
   //
+  log('========== pubsub god_settleWinList_A start ==========');
   // if (nowHHmm === '1700') {
   log('每天 17:00 賽事勝注勝率計算 run');
   await settleWinList({ token: { uid: '999' }, date: nowYYYYMMDD });
@@ -50,6 +125,7 @@ async function god_settleWinList_A() {
   // }
 
   log('========== pubsub god_settleWinList_A end ==========');
+
   return '{ status: \'ok\' }'; // res.json({ status: 'ok' });
 }
 
