ad204bba51d214de34d271513f474f67f04d46be
d0177381@gmail.com <>
2020-07-28 17:43:37 +0800

    [Add] crawler schedule in index.js

--- functions/index.js
+++ functions/index.js
@@ -260,3 +260,17 @@ exports.cashflow_dividendExpire1 = functions.pubsub
   .schedule('0 0 1 * *')
   .timeZone('Asia/Taipei')
   .onRun(require('./pubsub/cashflow_dividendExpire1'));
+
+// prematch_baseball 排程
+exports.prematch_crawler_KBO = functions.pubsub
+  .schedule('0 23 * * *')
+  .timeZone('Asia/Taipei')
+  .onRun(require('./pubsub/crawlers/prematch_KBO'));
+exports.prematch_crawler_CPBL = functions.pubsub
+  .schedule('0 23 * * *')
+  .timeZone('Asia/Taipei')
+  .onRun(require('./pubsub/crawlers/prematch_CPBL'));
+exports.prematch_crawler_NPB = functions.pubsub
+  .schedule('0 23 * * *')
+  .timeZone('Asia/Taipei')
+  .onRun(require('./pubsub/crawlers/prematch_NPB'));
