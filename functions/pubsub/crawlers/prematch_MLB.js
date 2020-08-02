8580ac3a91eb72902bebccb776a1e3855943eb83
d0177381@gmail.com <>
2020-08-02 21:39:10 +0800

    [Add] some team_base fields, but some field still missing

--- functions/pubsub/crawlers/prematch_MLB.js
+++ functions/pubsub/crawlers/prematch_MLB.js
@@ -1,35 +1,21 @@
 const AppErrors = require('../../util/AppErrors');
 const { getSeason } = require('../../util/databaseEngine');
 const { leagueCodebook } = require('../../util/leagueUtil');
-const { crawler } = require('../../util/crawlerUtil');
+const { getDataByAxios } = require('../../util/crawlerUtil');
+const { MLB_teamName2id } = require('../../util/teamsMapping');
 // const https = require('https');
 
 const configs = {
   league: 'MLB',
-  official_URL: 'https://www.mlb.com'
+  // official_URL: 'https://www.mlb.com'
+  teamBaseAPI: 'https://statsapi.mlb.com/api/v1/standings?leagueId=103,104&season=2020&date=2020-09-27&standingsTypes=regularSeason&hydrate=division,conference,sport,league,team(nextSchedule(team,gameType=%5BR,F,D,L,W,C%5D,inclusive=false),previousSchedule(team,gameType=%5BR,F,D,L,W,C%5D,inclusive=true))'
 };
 
-const options = {
-  hostname: 'www.mlb.com',
-  path: '/standings/league',
-  method: 'GET',
-  headers: { 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36' }
-};
 async function prematch_MLB(req, res) {
   return new Promise(async function(resolve, reject) {
     try {
       const season = await getSeason(leagueCodebook(configs.league).id);
-      // await crawlerTeamBase(season);
-      const str = '';
-      // https.get(options, function(res) {
-      //   console.log(res.statusCode);
-      //   console.log(res.headers);
-      //   res.on('data', function(chunk) {
-      //     // console.log(chunk);
-      //     // str += chunk;
-      //     process.stdout.write(chunk);
-      //   });
-      // });
+      await crawlerTeamBase(season);
 
       return resolve(res.json('MLB'));
     } catch (err) {
@@ -41,9 +27,8 @@ async function prematch_MLB(req, res) {
 function crawlerTeamBase(season) {
   return new Promise(async function(resolve, reject) {
     try {
-      const $_teamBase = await crawler(`${configs.official_URL}standings/league`);
-      // const $_teamBase = await crawler('http://eng.koreabaseball.com/Standings/TeamStandings.aspx');
-      console.log(`${configs.official_URL}standings/league`);
+      const data = await getDataByAxios(`${configs.teamBaseAPI}`);
+      await repackageTeamBase(data);
       // const teamBase = await getTeamBase($_teamBase);
       return resolve();
     } catch (err) {
@@ -52,16 +37,29 @@ function crawlerTeamBase(season) {
   });
 }
 
-// function getTeamBase($) {
-//   return new Promise(async function(resolve, reject) {
-//     try {
-//       $('*').each(function(i) {
-//         console.log($(this).text());
-//       });
-//       return resolve();
-//     } catch (err) {
-//       return reject(new AppErrors.CrawlersError(err.stack));
-//     }
-//   });
-// }
+function repackageTeamBase(data) {
+  return new Promise(async function(resolve, reject) {
+    try {
+      // Go through each region
+      for (let i = 0; i < data.records.length; i++) {
+        for (let j = 0; j < data.records[i].teamRecords.length; j++) {
+          const teamData = data.records[i].teamRecords[j];
+          const teamName = teamData.team.name;
+          const teamId = MLB_teamName2id(teamName).id;
+          const Win = String(teamData.leagueRecord.wins);
+          const Loss = String(teamData.leagueRecord.losses);
+          const G = String(Number(Win) + Number(Loss));
+          const PCT = String(`0${teamData.leagueRecord.pct}`);
+          // const GB = teamData.leagueGamesBack; // not correct
+          const R = String(teamData.runsScored);
+          const allow_R = String(teamData.runsAllowed);
+          const STRK = teamData.streak.streakCode;
+          console.log(teamName, teamId, G, PCT, R, allow_R, STRK);
+        }
+      }
+    } catch (err) {
+      return reject(new AppErrors.RepackageError());
+    }
+  });
+}
 module.exports = prematch_MLB;
