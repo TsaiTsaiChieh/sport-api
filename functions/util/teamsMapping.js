ecefde80867e49292de50979bfaad5162b452e07
d0177381@gmail.com <>
2020-08-02 20:41:16 +0800

    [Add] MLB code book

--- functions/util/teamsMapping.js
+++ functions/util/teamsMapping.js
@@ -36,4 +36,112 @@ function KBO_teamName2id(name) {
   }
 }
 
-module.exports = { KBO_teamName2id };
+function MLB_teamName2id(name) {
+  name = name.toLowerCase().trim();
+  switch (name) {
+    case 'houston astros':
+    case 'hou':
+      return { statId: '117', id: '1217' };
+    case 'oakland athletics':
+    case 'oak':
+      return { statId: '113', id: '1222' };
+    case 'seattle mariners':
+    case 'sea':
+      return { statId: '136', id: '1202' };
+    case 'los angeles angels':
+    case 'ana':
+    case 'laa':
+      return { statId: '108', id: '1090' };
+    case 'texas rangers':
+    case 'tex':
+      return { statId: '140', id: '1311' };
+    case 'New York Yankees':
+    case 'nya':
+      return { statId: '147', id: '1121' };
+    case 'baltimore orioles':
+    case 'bal':
+      return { statId: '110', id: '1120' };
+    case 'tampa bay rays':
+    case 'tba':
+      return { statId: '139', id: '1216' };
+    case 'tronto blue jays':
+    case 'tor':
+      return { statId: '141', id: '1089' };
+    case 'boston red sox':
+    case 'bos':
+      return { statId: '111', id: '1479' };
+    case 'minnesota twins':
+    case 'min':
+      return { statId: '142', id: '1088' };
+    case 'detroit tigers':
+    case 'det':
+      return { statId: '116', id: '1091' };
+    case 'cleveland indians':
+    case 'cle':
+      return { statId: '114', id: '1310' };
+    case 'Chicago White Sox':
+    case 'cha':
+      return { statId: '145', id: '1091' };
+    case 'kansas city royals':
+    case 'kca':
+    case 'kc':
+      return { statId: '118', id: '1478' };
+    case 'chicago cubs':
+    case 'chn':
+    case 'chc':
+      return { statId: '112', id: '1368' };
+    case 'milwaukee brewers':
+    case 'mil':
+      return { statId: '158', id: '1187' };
+    case 'st. louis cardinals':
+    case 'sln':
+    case 'stl':
+      return { statId: '138', id: '1223' };
+    case 'cincinnati reds':
+    case 'cin':
+      return { statId: '113', id: '1364' };
+    case 'pittsburgh pirates':
+    case 'pit':
+      return { statId: '134', id: '1186' };
+    case 'colorado rockies':
+    case 'col':
+      return { statId: '115', id: '1146' };
+    case 'los angeles dodgers':
+    case 'lan':
+      return { statId: '119', id: '1369' };
+    case 'san diego padres':
+    case 'sdn':
+    case 'sd':
+      return { statId: '135', id: '1108' };
+    case 'san francisco giants':
+    case 'sfn':
+    case 'sf':
+      return { statId: '137', id: '1353' };
+    case 'arizona diamondbacks':
+    case 'ari':
+      return { statId: '109', id: '1365' };
+    case 'atlanta braves':
+    case 'atl':
+      return { statId: '144', id: '1352' };
+    case 'miami marlins':
+    case 'mia':
+    case 'fla':
+      return { statId: '146', id: '1109' };
+    case 'washington nationals':
+    case 'was':
+    case 'wah':
+      return { statId: '120', id: '1147' };
+    case 'philadelphia phillies':
+    case 'phi':
+      return { statId: '143', id: '1112' };
+    case 'new york mets':
+    case 'nyn':
+    case 'nym':
+      return { statId: '121', id: '1113' };
+    default:
+
+      return 'Unknown team name in MLB';
+  }
+}
+
+module.exports = { KBO_teamName2id, MLB_teamName2id };
