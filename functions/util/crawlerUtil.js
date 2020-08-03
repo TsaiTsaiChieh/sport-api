108ef2d5b8ed6060566e710631a589f2ae27e059
d0177381@gmail.com <>
2020-08-03 13:45:12 +0800

    [Add] setDataToFirestore, insertTeamNameToFirestore, debigLogger function

--- functions/util/crawlerUtil.js
+++ functions/util/crawlerUtil.js
@@ -1,6 +1,9 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 const AppErrors = require('./AppErrors');
+const firebaseAdmin = require('./firebaseUtil');
+const firestore = firebaseAdmin().firestore();
+const logger = require('firebase-functions/lib/logger');
 
 function crawler(URL) {
   return new Promise(async function(resolve, reject) {
@@ -25,4 +28,42 @@ function getDataByAxios(URL) {
   });
 }
 
-module.exports = { crawler, getDataByAxios };
+function setDataToFirestore(data, configs) {
+  return new Promise(async function(resolve, reject) {
+    try {
+      const { season, fieldName, collectionName, teamId } = configs;
+      const temp = {};
+      temp[`season_${season}`] = {};
+      temp[`season_${season}`][fieldName] = data;
+      await firestore.collection(collectionName).doc(teamId).set(temp, { merge: true });
+      return resolve();
+    } catch (err) {
+      return reject(new AppErrors.FirebaseCollectError(err.stack));
+    }
+  });
+}
+
+function insertTeamNameToFirestore(data, configs) {
+  return new Promise(async function(resolve, reject) {
+    try {
+      const { collectionName, teamId } = configs;
+      await firestore.collection(collectionName).doc(teamId).set({ alias: data }, { merge: true });
+      return resolve();
+    } catch (err) {
+      return reject(new AppErrors.FirebaseCollectError(err.stack));
+    }
+  });
+}
+
+function debugLogger(configs) {
+  const { league, teamId, teamName, fieldName } = configs;
+  logger.debug(`棒球 ${league} 更新 ${fieldName}：隊伍 ${teamName}(${teamId}) 完成`);
+}
+
+module.exports = {
+  crawler,
+  getDataByAxios,
+  setDataToFirestore,
+  insertTeamNameToFirestore,
+  debugLogger
+};
