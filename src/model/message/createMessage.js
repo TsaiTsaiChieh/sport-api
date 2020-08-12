4a85f52671764265dc7c8f2af8d68ea94b2550a6
d0177381@gmail.com <>
2020-08-12 21:25:26 +0800

    [Add] refactor and fix the response did not contain default title field

--- src/model/message/createMessage.js
+++ src/model/message/createMessage.js
@@ -2,129 +2,173 @@ const firebaseAdmin = require('../../util/firebaseUtil');
 const firestore = firebaseAdmin().firestore();
 const database = firebaseAdmin().database();
 const db = require('../../util/dbUtil');
-const messageModule = require('../../util/messageModule');
-async function getUserInfo(uid) {
-  return new Promise(async function(resolve, reject) {
-    try {
-      const result = await db.sequelize.models.user.findOne({
-        attributes: [
-          'uid',
-          'status',
-          'avatar',
-          'display_name',
-          'signature',
-          'default_title',
-          'block_message'
-        ],
-        where: {
-          uid: uid
-        },
-        raw: true
-      });
-      resolve(result);
-    } catch (error) {
-      console.error(error);
-      reject(error);
-    }
-  });
+const AppErrors = require('../../util/AppErrors');
+const { getTitlesPeriod } = require('../../util/modules');
+const { findUser } = require('../../util/databaseEngine');
+const { leagueDecoder, league2Sport, USER_STATUS } = require('../../util/leagueUtil');
+const DELETION = {
+  RETRACT: 0, // 使用者自行刪除，所以全部人皆看不到
+  DELETE: 1 // 使用者不想看到自己或別人的訊息，只有該使用者看不到，其他人看得到
 };
+
 function createMessage(args) {
   return new Promise(async function(resolve, reject) {
     try {
-      const insertData = {};
-      insertData.createTime = firebaseAdmin().firestore.Timestamp.now();
-
-      /* get user according token */
-      // user部分讀mysql 訊息仍然放在firebase rtdb
-      const mysql_user = await getUserInfo(args.token.uid);
-      /* step1: check if user exists */
-      if (!mysql_user) {
-        reject({ code: 404, error: 'user not found' });
-        return;
+      args.createTime = firebaseAdmin().firestore.Timestamp.now();
+      let defaultTitle = null;
+      // Check if user exists and get default title
+      const userData = await findUser(args.token.uid);
+      // If user status is 1 (normal user), do not get the default title
+      if (userData.status === USER_STATUS.GOD) {
+        const { period } = getTitlesPeriod(args.now);
+        defaultTitle = await getUserDefaultTitle(args.token.uid, period);
       }
-      /* step2: check user block message time */
-      if (mysql_user.block_message && mysql_user.block_message !== null && mysql_user.block_message !== '') {
-        const block_date = new Date(mysql_user.block_message);
-        const block_ts = block_date.getTime();
-        if (Date.now() < block_ts) {
-          reject({ code: 403, error: 'user had been muted' });
-          return;
+      // Check user block message time
+      await checkUserMuted(userData, args.now);
+      // Get reply message info
+      args.replyMessageObj = await replyMessage(args);
+      // add message data to firestore & realtime and response
+      return resolve(await insert2Firebase(args, { userData, defaultTitle }));
+    } catch (err) {
+      return reject(new AppErrors.CreateMessageError(err.stack));
+    }
+  });
+}
+
+function getUserDefaultTitle(uid, period) {
+  return new Promise(async function(resolve, reject) {
+    try {
+      const result = await db.sequelize.query(
+        `SELECT title.league_id, title.rank_id
+           FROM users AS user
+      INNER JOIN titles AS title 
+             ON user.default_god_league_rank = title.league_id
+            AND user.uid = title.uid
+          WHERE user.uid = :uid
+            AND title.period = :period`,
+        {
+          type: db.sequelize.QueryTypes.SELECT,
+          replacements: { uid, period }
+        });
+      // If null, the result[0] is undefined
+      return resolve(result[0]);
+    } catch (err) {
+      return reject(new AppErrors.MysqlError(err.stack));
+    }
+  });
+};
+
+function checkUserMuted(userData, now) {
+  return new Promise(async function(resolve, reject) {
+    try {
+      const { block_message } = userData;
+      if (block_message) {
+        const blockDate = new Date(block_message);
+        if (now <= blockDate) {
+          return reject(new AppErrors.UserHadBeenMuted());
         }
       }
-      // if (userSnapshot.data().blockMessage._seconds * 1000 > new Date()) {
-      //   reject({ code: 403, error: 'user had been muted' });
-      //   return;
-      // }
-      /* step3: get reply message info (future can be written as function) */
+      return resolve();
+    } catch (err) {
+      return resolve(err.stack);
+    }
+  });
+}
+
+function replyMessage(args) {
+  return new Promise(async function(resolve, reject) {
+    try {
       if (args.reply) {
         const messageSnapshot = await firestore.collection(`chat_${args.message.channelId}`).doc(args.reply.messageId).get();
-        /* messageId did not exist return 400 error */
-        /* second condition is soft delete logic: if -1 (admin delete) or 0 (user delete) return error */
-        if (
-          !messageSnapshot.data() ||
-          messageSnapshot.data().message.softDelete <= 0
-        ) {
-          reject({ code: 404, error: 'message/file not found' });
-          return;
-        }
-        /* deny user reply the message which deleted by user himself/herself */
-        if (
-          args.token.uid === messageSnapshot.data().user.uid &&
-          messageSnapshot.data().message.softDelete === 1
-        ) {
-          reject({
-            code: 403,
-            error: 'can not reply message which deleted by user himself/herself'
-          });
-          return;
-        }
-        const reply = messageModule.repackageMessageData(
-          messageSnapshot.data()
-        );
-        insertData.reply = reply;
-      }
-      /* step3: get insert doc id */
+        const messageData = messageSnapshot.data();
+        // message object did not exist return error
+        // second condition is soft delete logic: if -1 (admin delete) or 0 (user retract) return error
+        if (!messageData || messageData.message.softDelete <= DELETION.RETRACT) return reject(new AppErrors.MessageNotFound());
+
+        // deny user reply the message which deleted by user himself/herself
+        if (args.token.uid === messageData.user.uid && messageData.message.softDelete === DELETION.DELETE) return reject(new AppErrors.MessageNotFound());
+        const reply = repackageReplyMessageData(messageData);
+        return resolve(reply);
+      } else return resolve();
+    } catch (err) {
+      return reject(new AppErrors.FirebaseCollectError(err.stack));
+    }
+  });
+}
+
+function repackageReplyMessageData(messageData) {
+  return new Promise(async function(resolve, reject) {
+    try {
+      const { message } = messageData;
+      const data = {
+        channelId: message.channelId,
+        message: message.message,
+        type: message.type,
+        messageId: message.messageId,
+        user: messageData.user
+      };
+      if (message.thumbURL) data.thumbURL = message.thumbURL;
+      return resolve(data);
+    } catch (err) {
+      return reject(new AppErrors.RepackageError(err.stack));
+    }
+  });
+}
+
+function insert2Firebase(args, user) {
+  return new Promise(async function(resolve, reject) {
+    try {
       const messageDoc = firestore
         .collection(`chat_${args.message.channelId}`)
         .doc();
       const messageId = messageDoc.id;
-      // const user = messageModule.repackageUserData(userSnapshot.data()); //舊的
-      const user = { // 建立要放在firebase rtdb的結構
-        uid: mysql_user.uid,
-        displayName: mysql_user.display_name,
-        avatar: mysql_user.avatar,
-        status: mysql_user.status,
-        defaultTitle: {}
-      };
+      const data = await repackageMessageObject(args, messageId, user);
 
-      insertData.message = {
-        channelId: args.message.channelId,
-        message: args.message.message,
-        type: args.message.type,
-        tempHash: args.message.tempHash,
-        messageId: messageId,
-        softDelete: 2 /* 2 is default and normal */
-      };
-      if (args.message.thumbURL) {
-        insertData.message.thumbURL = args.message.thumbURL;
-      }
-      insertData.user = user;
-      /* add message data to firestore & realtime */
-      const result = await messageDoc.set(insertData);
+      const result = await messageDoc.set(data);
       if (result) {
-        database
-          .ref(`chat_${args.message.channelId}`)
-          .child(messageId)
-          .set(insertData);
-        resolve(insertData);
-      } else {
-        reject({ code: 500, error: 'insert firestore failed' });
-        return;
-      }
+        await database.ref(`chat_${args.message.channelId}`).child(messageId).set(data);
+        return resolve(data);
+      } else return reject(new AppErrors.FirebaseCollectError());
     } catch (err) {
-      console.error('error happened...', err);
-      reject({ code: 500, error: err });
+      return reject(new AppErrors.FirebaseCollectError(err.stack));
     }
   });
 }
+
+function repackageMessageObject(args, messageId, user) {
+  return new Promise(async function(resolve, reject) {
+    try {
+      const data = {
+        message: {
+          channelId: args.message.channelId,
+          message: args.message.message,
+          type: args.message.type,
+          messageId: messageId,
+          softDelete: 2 // 2 is default and normal
+        },
+        user: {
+          uid: user.userData.uid,
+          displayName: user.userData.display_name,
+          avatar: user.userData.avatar,
+          status: user.userData.status,
+          defaultTitle: null
+        },
+        createTime: args.createTime
+      };
+      if (user.defaultTitle) {
+        const league = leagueDecoder(user.defaultTitle.league_id);
+        const sport = league2Sport(league).sport_id;
+        data.user.defaultTitle = {
+          rank: user.defaultTitle.rank_id, league, sport
+        };
+      }
+      if (args.message.thumbURL) data.message.thumbURL = args.message.thumbURL;
+      if (args.replyMessageObj) data.reply = args.replyMessageObj;
+      return resolve(data);
+    } catch (err) {
+      return reject(new AppErrors.RepackageError(err.stack));
+    }
+  });
+}
+
 module.exports = createMessage;
