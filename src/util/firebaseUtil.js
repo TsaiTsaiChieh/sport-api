e83743e5711224a32ffedcc4e6acc1d496f4dad5
Rex <rex@gets-info.com>
2020-08-14 18:37:35 +0800

    Update firebaseUtil.js

--- src/util/firebaseUtil.js
+++ src/util/firebaseUtil.js
@@ -1,9 +1,9 @@
 const envValues = require('../config/env_values');
 const admin = require('firebase-admin');
-const cert = require(process.env.certPath);
 module.exports = function() {
   if (admin.apps.length === 0) {
     console.log('initializing firebase database');
+    const cert = require(process.env.certPath);
     return admin.initializeApp({
       credential: admin.credential.cert(cert),
       databaseURL: envValues.firebaseConfig.databaseURL,
