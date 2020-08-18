21ae211b03e0b81c8f59693cf323b9f6ca8f3d19
page40316 <page@gets-info.com>
2020-08-18 11:25:58 +0800

    merge develop

--- src/config/acceptValues.js
+++ src/config/acceptValues.js
@@ -1,9 +1,5 @@
 const acceptNumberAndLetter = '^[a-zA-Z0-9_.-]*$';
-<<<<<<< HEAD
-const acceptLeague = ['MLB', 'CPBL', 'KBO', 'NPB', 'ABL', 'LMB', 'NBA', 'SBL', 'WNBA', 'NBL', 'KBL', 'CBA', 'BJL', 'NHL', 'Soccer', 'eSoccer'];
-=======
 // const acceptLeague = ['NBA', 'CBA', 'MLB', 'KBO', 'NPB', 'CPBL', 'eSoccer'];
->>>>>>> develop
 
 const acceptLeague = ['NBA', 'MLB', 'KBO', 'NPB', 'CPBL', 'eSoccer'];
 module.exports = {
