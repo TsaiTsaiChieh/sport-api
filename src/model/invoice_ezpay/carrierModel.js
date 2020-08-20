2d08d7d9693042be1e372ca2e3308964df92365d
henry <henry@gets-info.com>
2020-08-20 17:05:30 +0800

    [Fix] adjust invoice carrier api

--- src/model/invoice_ezpay/carrierModel.js
+++ src/model/invoice_ezpay/carrierModel.js
@@ -2,56 +2,28 @@ const db = require('../../util/dbUtil');
 async function carrierModel(req) {
   return new Promise(async function(resolve, reject) {
     try {
-      console.log(req);
       const uid = req.token.uid;
-      console.log('222');
       const invoice_carrier = req.body.invoice_carrier;
-      console.log('333');
       const carrier = {
         uid: uid,
         invoice_carrier: invoice_carrier
       };
-      console.log('444');
-      /* 新增載具 */
-      if(req.method=== "GET" ){
-        try{
-          const carrier_status = await db.User.findOne({
-            where: {
-              invoice_carrier:invoice_carrier
-            },
-            attributes: ['uid','invoice_carrier'],
-            raw: true
-          })
-          if(carrier_status){
-            resolve({'status':'success'});
-          }
-        } catch (e) {
-          reject({'status':'failed'});
-        }
-      } else if (req.method === 'POST') {
-        try{
-          const carrier_status = await db.User.create(carrier);
-          if(carrier_status){
-            resolve({'status':'success'});
-          }
-        } catch (e) {
-          reject({'status':'failed'});
-        }
-      /* 更新載具 */
-      } else if (req.method === 'PUT') {
-        try{
-          const carrier_type = db.User.update({
+
+      /* 新增/更新載具 */
+      if (req.method === 'POST') {
+        try {
+          const carrier_status = db.User.update({
             invoice_carrier: invoice_carrier
           }, {
             where: {
               uid: uid
             }
           });
-          if(carrier_status){
-            resolve({'status':'success'});
+          if (carrier_status) {
+            resolve({ status: 'success' });
           }
         } catch (e) {
-          reject({'status':'failed'});
+          reject({ status: 'failed' });
         }
       }
     } catch (err) {
--- src/routers/invoice_ezpay.js
+++ src/routers/invoice_ezpay.js
@@ -14,23 +14,12 @@ router.post(
   require('../controller/invoice_ezpay/mpgController')
 );
 
-/*判斷使用者是否有發票載具*/
-router.get(
-  '/carrier',
-  verification.token,
-  require('../controller/invoice_ezpay/carrierController')
-);
-/*使用者 新增 發票載具*/
+/* 使用者 新增/更新 發票載具 */
 router.post(
   '/carrier',
   verification.token,
   require('../controller/invoice_ezpay/carrierController')
 );
-/*使用者 更新 發票載具*/
-router.put(
-  '/carrier',
-  verification.token,
-  require('../controller/invoice_ezpay/carrierController')
-);
+
 
 module.exports = router;
