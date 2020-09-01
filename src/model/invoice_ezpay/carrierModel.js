28162793e6f2b1ae7f3f0ac686eb4e8c6a52f3e7
henry <henry@gets-info.com>
2020-09-01 17:37:34 +0800

    [Add] add the default carrier and carrier status

--- src/model/invoice_ezpay/carrierModel.js
+++ src/model/invoice_ezpay/carrierModel.js
@@ -3,11 +3,14 @@ async function carrierModel(req) {
   return new Promise(async function(resolve, reject) {
     try {
       const uid = req.token.uid;
-      const invoice_carrier = req.body.invoice_carrier;
+      let invoice_carrier = req.body.invoice_carrier;
 
       /* 新增/更新載具 */
       if (req.method === 'POST') {
         try {
+          if(req.body.carrier_status===0){
+            invoice_carrier = '/ABCDEFG';
+          }
           const carrier_status = db.User.update({
             invoice_carrier: invoice_carrier
           }, {
