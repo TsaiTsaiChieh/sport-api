82205cc00e8085bb158a546ee2678c3f6e2c5609
henry <henry@gets-info.com>
2020-09-15 10:40:07 +0800

    [Fix] news separate to two api

--- src/model/user/newsModel.js
+++ src/model/user/newsModel.js
@@ -1,49 +1,46 @@
 const modules = require('../../util/modules');
 const db = require('../../util/dbUtil');
-function newsModel(method, args, uid) {
+
+function newsModel(req) {
   return new Promise(async function(resolve, reject) {
     try {
-      if (method === 'POST') {
-        const newsList = {
-          system: [],
-          user: []
-        };
+      if (req.method === 'POST') {
         const end = await modules.moment(new Date()).format('YYYY-MM-DD HH:MM:SS');
         /* 取前一個月時間 */
         const begin_month = await modules.moment(new Date()).subtract(1, 'months').format('YYYY-MM-DD HH:MM:SS');
         /* 取前一周時間 */
         const begin_week = await modules.moment(new Date()).subtract(1, 'weeks').format('YYYY-MM-DD HH:MM:SS');
 
-        /* 系統訊息資料 */
-        newsList.system = await db.sequelize.query(
+        if (req.body.message_type === 'system') {
+          const system_list = await db.sequelize.query(
           `
-            SELECT * FROM user__news__sys WHERE (target = -1 OR target LIKE '%${uid}%') AND updatedAt BETWEEN '${begin_month}' AND '${end}'
+            SELECT * FROM user__news__sys WHERE (target = -1 OR target LIKE '${req.token.uid}') AND updatedAt BETWEEN '${begin_month}' AND '${end}'
           `,
           {
             type: db.sequelize.QueryTypes.SELECT,
             raw: true
           }
-        );
-
-        // console.log(user_filter);return;
+          );
+          resolve(system_list);
+        } else if (req.body.message_type === 'user') {
         /* 讀取最愛玩家資料 */
-        const favorite_user = await db.sequelize.query(
+          const favorite_user = await db.sequelize.query(
         `
           SELECT god_uid
             FROM user__favoriteplayers
-           WHERE uid = '${uid}'
-           GROUP BY god_uid
+          WHERE uid = '${req.token.uid}'
+          GROUP BY god_uid
         `,
         {
           type: db.sequelize.QueryTypes.SELECT,
           raw: true
         }
-        );
+          );
 
-        const uids = Object.values(favorite_user).map(item => item.god_uid);
+          const uids = Object.values(favorite_user).map(item => item.god_uid);
 
-        /* 使用者訊息資料 */
-        newsList.user = await db.sequelize.query(
+          /* 使用者訊息資料 */
+          const user_list = await db.sequelize.query(
             `
             SELECT un.news_id, un.uid, u.display_name, un.sort, un.sort_id, un.league, un.title, un.content, un.status, un.match_scheduled_tw, un.scheduled, un.createdAt, un.updatedAt
               FROM user__news un, users u
@@ -59,14 +56,15 @@ function newsModel(method, args, uid) {
               type: db.sequelize.QueryTypes.SELECT,
               raw: true
             }
-        );
-        console.log(newsList);
-        resolve(newsList);
-      } else if (method === 'PUT') {
+          );
+
+          resolve(user_list);
+        }
+      } else if (req.method === 'PUT') {
         const now = modules.moment(new Date()).format('YYYY-MM-DD HH:mm:ss');
         const now_timestamp = modules.moment(new Date()).unix();
-        const title = args.title;
-        const content = args.content;
+        const title = req.body.title;
+        const content = req.body.content;
 
         const insert = db.sequelize.query(
           `
@@ -74,21 +72,21 @@ function newsModel(method, args, uid) {
             VALUES ($uid, $title, $content, 0, 1, $now_timestamp, $now, $now);
           `,
           {
-            bind: { uid: uid, title: title, content: content, now_timestamp: now_timestamp, now: now },
+            bind: { uid: req.token.uid, title: title, content: content, now_timestamp: now_timestamp, now: now },
             type: db.sequelize.QueryTypes.INSERT
           }
         );
         resolve(insert);
-      } else if (method === 'DELETE') {
-        const is_system = args.is_system;
-        const items = args.items;
+      } else if (req.method === 'DELETE') {
+        const is_system = req.body.is_system;
+        const items = req.body.items;
         const del_join = items.join(',');
         let del_res = [];
         if (is_system) {
           items.forEach(function(item) {
             del_res = db.News_System.upsert({
               system_id: item,
-              uid: uid,
+              uid: req.token.uid,
               active: 0
             });
           });
