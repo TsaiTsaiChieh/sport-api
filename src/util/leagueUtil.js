ba23ebbb4496e8373730bb75159f889493e421a8
Rex <rex@gets-info.com>
2020-09-15 15:55:38 +0800

    [Fix] rank conditions by each league ratio

--- src/util/leagueUtil.js
+++ src/util/leagueUtil.js
@@ -72,103 +72,120 @@ function leagueCodebook(league) {
       return {
         id: '2274',
         match: db.basketball_NBA,
-        name_ch: '美國國家籃球協會'
+        name_ch: '美國國家籃球協會',
+        predicts_perDay: 1
       };
     case 'SBL':
       return {
         id: '8251',
         match: db.basketball_SBL,
-        name_ch: '超級籃球聯賽'
+        name_ch: '超級籃球聯賽',
+        predicts_perDay: 0.5
       };
     case 'WNBA':
       return {
         id: '244',
         match: db.basketball_WNBA,
-        name_ch: '美國國家女子籃球協會'
+        name_ch: '美國國家女子籃球協會',
+        predicts_perDay: 0.8
       };
     case 'NBL':
       return {
         id: '1714',
         match: db.basketball_NBL,
-        name_ch: '澳洲職籃'
+        name_ch: '澳洲職籃',
+        predicts_perDay: 2
       };
     case 'CBA':
       return {
         id: '2319',
         match: db.basketball_CBA,
-        name_ch: '中國職籃'
+        name_ch: '中國職籃',
+        predicts_perDay: 2
       };
     case 'KBL':
       return {
         id: '2148',
         match: db.basketball_KBL,
-        name_ch: '韓國職籃'
+        name_ch: '韓國職籃',
+        predicts_perDay: 0.8
       };
     case 'BJL':
       return {
         id: '1298',
         match: db.basketball_BJL,
-        name_ch: '日本職籃'
+        name_ch: '日本職籃',
+        predicts_perDay: 2
       };
     case 'MLB':
       return {
         id: '3939',
         match: db.baseball_MLB,
-        name_ch: '美國職棒大聯盟'
+        name_ch: '美國職棒大聯盟',
+        predicts_perDay: 2
       };
     case 'NPB':
       return {
         id: '347',
         match: db.baseball_NPB,
-        name_ch: '日本職棒'
+        name_ch: '日本職棒',
+        predicts_perDay: 2
       };
     case 'CPBL':
       return {
         id: '11235',
         match: db.baseball_CPBL,
-        name_ch: '中華職棒'
+        name_ch: '中華職棒',
+        predicts_perDay: 0.8
       };
     case 'KBO':
       return {
         id: '349',
         match: db.baseball_KBO,
-        name_ch: '韓國職棒'
+        name_ch: '韓國職棒',
+        predicts_perDay: 0.8
       };
     case 'ABL':
       return {
         id: '2759',
         match: db.baseball_ABL,
-        name_ch: '澳洲職棒'
+        name_ch: '澳洲職棒',
+        predicts_perDay: 1.2
       };
     case 'LMB':
       return {
         id: '4412',
         match: db.baseball_LMB,
-        name_ch: '墨西哥職棒'
+        name_ch: '墨西哥職棒',
+        predicts_perDay: 1.2
       };
     case 'NHL':
       return {
         id: '1926',
         match: db.icehockey_NHL,
-        name_ch: '國家冰球聯盟'
+        name_ch: '國家冰球聯盟',
+        predicts_perDay: 2
       };
     case 'Soccer':
       return {
         id: '8',
         match: db.Soccer,
-        name_ch: '足球'
+        name_ch: '足球',
+        predicts_perDay: 2
       };
     case 'eSoccer':
       return {
         id: '22000',
         match: db.eSoccer,
-        name_ch: '足球電競'
+        name_ch: '足球電競',
+        predicts_perDay: 2
       };
     case 'eGame':
       return {
         id: '23000',
         match: db.eGame,
-        name_ch: '電競遊戲'
+        name_ch: '電競遊戲',
+        predicts_perDay: 2
       };
     default:
       throw new AppErrors.UnknownLeague();
