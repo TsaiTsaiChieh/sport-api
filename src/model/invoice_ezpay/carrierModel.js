c80239f56f435b230fd71eb455817a142054cc4d
henry <henry@gets-info.com>
2020-09-24 11:42:35 +0800

    [Fix] add carrier status of invoice

--- src/model/invoice_ezpay/carrierModel.js
+++ src/model/invoice_ezpay/carrierModel.js
@@ -6,20 +6,31 @@ async function carrierModel(req) {
       const uid = req.token.uid;
       let invoice_carrier = req.body.invoice_carrier;
       /* 新增/更新載具 */
+
       if (req.method === 'POST') {
         try {
           if (req.body.carrier_status === 0) {
             invoice_carrier = ezpay_config.default_invoice_carrier;
           }
-          const carrier_status = await db.User.update({
-            invoice_carrier: invoice_carrier
+          await db.User.update({
+            invoice_carrier: invoice_carrier,
+            carrier_status: req.body.carrier_status
           }, {
             where: {
               uid: uid
             }
           });
-          if (carrier_status) {
-            resolve({ status: 'success' });
+          const getCarrier = await db.sequelize.query(`
+            SELECT invoice_carrier, carrier_status FROM users WHERE uid='${req.token.uid}'
+          `,
+          {
+            raw: true,
+            type: db.sequelize.QueryTypes.SELECT
+          });
+          if (getCarrier) {
+            resolve(getCarrier);
+          } else {
+            resolve({ status: 'failed' });
           }
         } catch (e) {
           reject({ status: 'failed' });
