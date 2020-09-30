cdc840f941c67fa3e819024d335d8ff7960de9e8
henry <henry@gets-info.com>
2020-09-30 15:37:58 +0800

    [Fix] transfer/buy api adjust

--- src/model/user/buyModel.js
+++ src/model/user/buyModel.js
@@ -9,12 +9,14 @@ async function buyModel(args, uid) {
   const end = args.end;
 
   const [err, buy] = await to(getGodSellPredictionDatesWinBetsInfo(uid, begin, end));
+  console.log(buy);
   if (err) {
     console.error('[Error][buyModel][getGodSellPredictionDatesWinBetsInfo] ', err);
     throw errs.dbErrsMsg('404', '50111', { addMsg: err.parent.code });
   }
   if (!buy) return buyList;
 
+ 
   for (const ele of buy) {
     buyList.push({
       date: ele.matches_date,
